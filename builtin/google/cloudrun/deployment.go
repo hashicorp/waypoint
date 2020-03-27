42693f9cff2126bb57b7b0d76a3e06cfedc6c297
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2020-03-27 16:19:47 -0700

    builtin/google: plugins don't need to implement protomarshaler anymore

--- builtin/google/cloudrun/deployment.go
+++ builtin/google/cloudrun/deployment.go
@@ -1,11 +1,7 @@
 package cloudrun
 
 import (
-	"github.com/golang/protobuf/proto"
-
 	"github.com/mitchellh/devflow/sdk/component"
 )
-
-func (d *Deployment) Proto() proto.Message { return d }
 
 var _ component.Deployment = (*Deployment)(nil)
