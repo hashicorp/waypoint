61663f21bf9946b70525c3cd59029980fc241502
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2020-05-30 17:27:39 -0700

    internal/factory: extract mapper's factory to a separate package
    I don't think this made a lot of sense to live in mapper anyways and we
    have now extracted that to argmapper.

---
+++ internal/factory/factory.go
@@ -0,0 +1,53 @@
+// Package factory contains a "factory" pattern based on argmapper.
+//
+// A Factory can be used to register factory methods to create some predefined
+// type or interface implementation. These functions are argmapper functions so
+// converters and so on can be used as part of instantiation.
+package factory
+
+import (
+	"fmt"
+	"reflect"
+
+	"github.com/mitchellh/go-argmapper"
+)
+
+// Factory keeps track of named dependency-injected factory functions to
+// create an implementation of an interface.
+type Factory struct {
+	iface reflect.Type
+	funcs map[string]*argmapper.Func
+}
+
+// NewFactory creates a Factory for the interface iface. The parameter
+// iface should be a nil pointer to the interface type. Example: (*iface)(nil).
+func NewFactory(iface interface{}) (*Factory, error) {
+	// Get the interface type
+	it := reflect.TypeOf(iface)
+	if k := it.Kind(); k != reflect.Ptr {
+		return nil, fmt.Errorf("iface must be a pointer to an interface, got %s", k)
+	}
+	it = it.Elem()
+	if k := it.Kind(); k != reflect.Interface {
+		return nil, fmt.Errorf("iface must be a pointer to an interface, got %s", k)
+	}
+
+	return &Factory{iface: it, funcs: make(map[string]*argmapper.Func)}, nil
+}
+
+// Register registers a factory function named name for the interface.
+func (f *Factory) Register(name string, fn interface{}) error {
+	ff, err := argmapper.NewFunc(fn)
+	if err != nil {
+		return err
+	}
+
+	f.funcs[name] = ff
+	return nil
+}
+
+// Func returns the factory function named name. This can then be used to
+// call and instantiate the factory interface type.
+func (f *Factory) Func(name string) *argmapper.Func {
+	return f.funcs[name]
+}
---
+++ internal/factory/factory_test.go
@@ -0,0 +1,61 @@
+package factory
+
+import (
+	"testing"
+
+	"github.com/mitchellh/go-argmapper"
+	"github.com/stretchr/testify/require"
+)
+
+func TestFactory(t *testing.T) {
+	require := require.New(t)
+
+	factory, err := NewFactory((*adder)(nil))
+	require.NoError(err)
+	require.NoError(factory.Register("two", func(a int) *adderTwo {
+		return &adderTwo{From: a}
+	}))
+
+	// Get a valid mapper with satisfied types
+	{
+		fn := factory.Func("two")
+		require.NotNil(fn)
+		result := fn.Call(argmapper.Typed("two", 42))
+		require.NoError(result.Err())
+		adder := result.Out(0).(adder)
+		require.Equal(adder.Add(), 44)
+	}
+
+	// Unregistered
+	{
+		fn := factory.Func("three")
+		require.Nil(fn)
+	}
+}
+
+// Test that our function can return an interface{} type and still implement
+// the factory interface.
+func TestFactory_interface(t *testing.T) {
+	require := require.New(t)
+
+	factory, err := NewFactory((*adder)(nil))
+	require.NoError(err)
+	require.NoError(factory.Register("two", func(a int) interface{} {
+		return &adderTwo{From: a}
+	}))
+
+	fn := factory.Func("two")
+	require.NotNil(fn)
+	result := fn.Call(argmapper.Typed("two", 42))
+	require.NoError(result.Err())
+	adder := result.Out(0).(adder)
+	require.Equal(adder.Add(), 44)
+}
+
+type adder interface {
+	Add() int
+}
+
+type adderTwo struct{ From int }
+
+func (a *adderTwo) Add() int { return a.From + 2 }
