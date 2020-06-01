e129110c2c5814804ea55c13d11b4f9dc935c46f
Evan Phoenix <evan@phx.io>
2020-06-01 10:04:37 -0700

    Move default URL hookups to DefaultEnv

--- cmd/waypoint-entrypoint/main.go
+++ cmd/waypoint-entrypoint/main.go
@@ -5,7 +5,6 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"strconv"
 	"strings"
 	"unicode"
 
@@ -18,11 +17,6 @@ import (
 func main() {
 	os.Exit(realMain())
 }
-
-const (
-	DefaultPort                = 5000
-	DefaultWaypointControlAddr = "control.alpha.waypoint.run"
-)
 
 func realMain() int {
 	flag.Usage = usage
@@ -42,45 +36,10 @@ func realMain() int {
 	ctx, closer := signalcontext.WithInterrupt(context.Background(), log)
 	defer closer()
 
-	options := []ceb.Option{
-		ceb.WithEnvDefaults(),
-		ceb.WithExec(args),
-	}
-
-	labels := os.Getenv("WAYPOINT_URL_LABELS")
-	if labels != "" {
-		var port int
-
-		portStr := os.Getenv("PORT")
-		if portStr == "" {
-			port = DefaultPort
-			os.Setenv("PORT", strconv.Itoa(DefaultPort))
-		} else {
-			i, err := strconv.Atoi(portStr)
-			if err != nil {
-				fmt.Fprintf(flag.CommandLine.Output(), "Invalid value of PORT: %s\n", err)
-				return 1
-			}
-
-			port = i
-		}
-
-		controlAddr := os.Getenv("WAYPOINT_CONTROL_ADDR")
-		if controlAddr == "" {
-			controlAddr = DefaultWaypointControlAddr
-		}
-
-		token := os.Getenv("WAYPOINT_TOKEN")
-		if token == "" {
-			fmt.Fprintf(flag.CommandLine.Output(), "No token provided via WAYPOINT_TOKEN.\n")
-			return 1
-		}
-
-		options = append(options, ceb.WithURLService(controlAddr, token, port, labels))
-	}
-
 	// Run our core logic
-	err := ceb.Run(ctx, options...)
+	err := ceb.Run(ctx,
+		ceb.WithEnvDefaults(),
+		ceb.WithExec(args))
 	if err != nil {
 		fmt.Fprintf(flag.CommandLine.Output(),
 			"Error initializing Waypoint entrypoint: %s\n", err)
