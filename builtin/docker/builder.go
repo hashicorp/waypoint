9f25941f9988be0661b23e600bfff043a9dd80ef
Evan Phoenix <evan@phx.io>
2020-06-02 17:18:49 -0700

    Inject the CEB into pack and docker builders by default

--- builtin/docker/builder.go
+++ builtin/docker/builder.go
@@ -3,14 +3,21 @@ package docker
 import (
 	"context"
 	"fmt"
+	"io/ioutil"
+	"os"
 	"os/exec"
+	"path/filepath"
 
+	"github.com/hashicorp/waypoint/internal/assets"
+	"github.com/hashicorp/waypoint/internal/pkg/epinject"
 	"github.com/hashicorp/waypoint/sdk/component"
 	"github.com/hashicorp/waypoint/sdk/terminal"
 )
 
 // Builder uses `docker build` to build a Docker iamge.
-type Builder struct{}
+type Builder struct {
+	DisableCEB bool
+}
 
 // BuildFunc implements component.Builder
 func (b *Builder) BuildFunc() interface{} {
@@ -40,6 +47,35 @@ func (b *Builder) Build(
 	cmd.Stderr = stderr
 	if err := cmd.Run(); err != nil {
 		return nil, err
+	}
+
+	if !b.DisableCEB {
+		tmpdir, err := ioutil.TempDir("", "waypoint")
+		if err != nil {
+			return nil, err
+		}
+
+		defer os.RemoveAll(tmpdir)
+
+		err = assets.RestoreAsset(tmpdir, "ceb/ceb")
+		if err != nil {
+			return nil, err
+		}
+
+		err = epinject.AlterEntrypoint(ctx, result.Name(), func(cur []string) (*epinject.NewEntrypoint, error) {
+			ep := &epinject.NewEntrypoint{
+				Entrypoint: append([]string{"/bin/wpceb"}, cur...),
+				InjectFiles: map[string]string{
+					filepath.Join(tmpdir, "ceb/ceb"): "/bin/wpceb",
+				},
+			}
+
+			return ep, nil
+		})
+
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	return result, nil
