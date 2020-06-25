e0a839600af64981537ce0d6ceef8369608f4c35
Jack Pearkes <jackpearkes@gmail.com>
2020-06-25 10:01:33 -0700

    sdk/release_manager: support authenticator

--- sdk/proto/release.pb.go
+++ sdk/proto/release.pb.go
@@ -124,25 +124,29 @@ func init() {
 }
 
 var fileDescriptor_e241864f384c76b6 = []byte{
-	// 273 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x74, 0x90, 0x4f, 0x4b, 0x03, 0x31,
-	0x10, 0xc5, 0x59, 0xff, 0xb4, 0x74, 0xba, 0x56, 0x89, 0x45, 0xd6, 0x08, 0x22, 0x9e, 0x7a, 0x28,
-	0x29, 0x54, 0x3f, 0x80, 0xa5, 0xe8, 0xcd, 0x4b, 0x7a, 0x17, 0xb6, 0xcb, 0x34, 0x14, 0x62, 0x12,
-	0xf3, 0xe7, 0x50, 0xfc, 0xe8, 0x5e, 0x64, 0x93, 0xac, 0xd2, 0x8a, 0xa7, 0xdd, 0x99, 0xf7, 0xe3,
-	0xbd, 0x97, 0x81, 0x33, 0x8b, 0x12, 0x6b, 0x87, 0xcc, 0x58, 0xed, 0x35, 0x39, 0x8d, 0x1f, 0x7a,
-	0x2d, 0xb4, 0x16, 0x12, 0x67, 0x71, 0x5a, 0x87, 0xcd, 0xac, 0x56, 0xbb, 0x44, 0xd0, 0x9b, 0x43,
-	0x09, 0xdf, 0x8d, 0xef, 0xc4, 0xd2, 0xc8, 0x20, 0xb6, 0x2a, 0x4d, 0xf7, 0x9f, 0xd0, 0xe7, 0xc9,
-	0x9d, 0x5c, 0xc0, 0x71, 0xb0, 0xb2, 0x2a, 0xee, 0x8a, 0xc9, 0x80, 0xb7, 0xbf, 0xf4, 0x0d, 0x4e,
-	0x38, 0x3a, 0x43, 0xa6, 0xd0, 0xb3, 0xe8, 0x82, 0xf4, 0x51, 0x1c, 0xce, 0xc7, 0x2c, 0x05, 0xb0,
-	0x2e, 0x80, 0x2d, 0xd4, 0x8e, 0x67, 0x86, 0x4c, 0xa0, 0x9f, 0x0b, 0x57, 0x47, 0x11, 0x1f, 0x25,
-	0x8e, 0xe5, 0x20, 0xde, 0xc9, 0xf3, 0xaf, 0x02, 0x46, 0x79, 0xf9, 0x5a, 0xab, 0x5a, 0xa0, 0x25,
-	0x4f, 0x50, 0x2e, 0xb5, 0xda, 0x6c, 0xc5, 0xca, 0xdb, 0xd0, 0x78, 0x72, 0xf5, 0x27, 0xea, 0xb9,
-	0x7d, 0x0b, 0xad, 0xb2, 0x67, 0x82, 0x59, 0xa2, 0x63, 0xd9, 0x25, 0x0c, 0xd2, 0x32, 0x58, 0x24,
-	0xb7, 0xfb, 0xd8, 0x8f, 0xc0, 0xf1, 0x23, 0xa0, 0xf3, 0xf4, 0x1f, 0x7b, 0x32, 0x85, 0x61, 0x2e,
-	0xb6, 0x32, 0xd8, 0x90, 0x32, 0xdb, 0xa4, 0xec, 0xf3, 0x3c, 0xbd, 0x04, 0xd5, 0x44, 0xf9, 0xf1,
-	0xf7, 0x88, 0xe3, 0x03, 0x8d, 0x2d, 0xac, 0x70, 0xf4, 0x72, 0xff, 0x02, 0xac, 0x2d, 0xba, 0xee,
-	0xc5, 0xdd, 0xc3, 0x77, 0x00, 0x00, 0x00, 0xff, 0xff, 0x26, 0xc9, 0xb8, 0xaf, 0xdf, 0x01, 0x00,
-	0x00,
+	// 343 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x9c, 0x52, 0xcb, 0x4f, 0xe2, 0x40,
+	0x18, 0x0f, 0xbb, 0x2c, 0x2c, 0x1f, 0x5d, 0x20, 0xb3, 0x68, 0xb0, 0x26, 0xc6, 0x78, 0xe2, 0x40,
+	0x86, 0x04, 0xb8, 0xe9, 0x01, 0x42, 0x34, 0xe1, 0xe0, 0xa5, 0x24, 0x1e, 0x4d, 0x86, 0xfa, 0x51,
+	0x9b, 0x0c, 0xd3, 0x3a, 0x8f, 0x03, 0xf1, 0xcf, 0xf1, 0x1f, 0x35, 0x9d, 0x19, 0x54, 0xd0, 0x1e,
+	0xf4, 0xd4, 0xce, 0xf7, 0x7b, 0xce, 0x03, 0xfe, 0x49, 0xe4, 0xc8, 0x14, 0xd2, 0x5c, 0x66, 0x3a,
+	0x23, 0x7f, 0xec, 0x27, 0x3c, 0x49, 0xb2, 0x2c, 0xe1, 0x38, 0xb4, 0xab, 0x95, 0x59, 0x0f, 0x99,
+	0xd8, 0x3a, 0x46, 0x78, 0x7a, 0x08, 0xe1, 0x26, 0xd7, 0x3b, 0x30, 0xc8, 0xb9, 0x49, 0x52, 0xe1,
+	0x56, 0x17, 0xcf, 0x50, 0x8f, 0x9c, 0x3b, 0xe9, 0xc0, 0x6f, 0x23, 0x79, 0xaf, 0x72, 0x5e, 0xe9,
+	0x37, 0xa2, 0xe2, 0x37, 0xbc, 0x87, 0x6a, 0x84, 0x2a, 0x27, 0x03, 0xa8, 0x49, 0x54, 0x86, 0x6b,
+	0x0b, 0x36, 0x47, 0x5d, 0xea, 0x02, 0xe8, 0x2e, 0x80, 0xce, 0xc4, 0x36, 0xf2, 0x1c, 0xd2, 0x87,
+	0xba, 0x2f, 0xdc, 0xfb, 0x65, 0xe9, 0x2d, 0xc7, 0xa3, 0x3e, 0x28, 0xda, 0xc1, 0xa3, 0x97, 0x2a,
+	0xb4, 0xfc, 0xf0, 0x96, 0x09, 0x96, 0xa0, 0x24, 0x53, 0x68, 0x2f, 0xd4, 0xcc, 0xe8, 0x47, 0x14,
+	0x3a, 0x8d, 0x99, 0xce, 0x24, 0x39, 0xfe, 0x94, 0x76, 0x5d, 0x6c, 0x27, 0x3c, 0xf2, 0xb6, 0x8b,
+	0x4d, 0xce, 0x71, 0x83, 0x42, 0x2b, 0x5b, 0x76, 0x02, 0xd5, 0x42, 0x4f, 0xba, 0x1e, 0xbe, 0x31,
+	0x22, 0x5e, 0xe6, 0x18, 0xd3, 0x99, 0x4c, 0x54, 0x58, 0x62, 0x46, 0xc6, 0xf0, 0xb7, 0x50, 0x15,
+	0xc4, 0xd2, 0xc0, 0xf6, 0x81, 0x23, 0xb9, 0x82, 0xe0, 0x8e, 0xf1, 0xf4, 0x81, 0x69, 0xfc, 0x41,
+	0xe4, 0x25, 0x74, 0x3e, 0xaa, 0xbf, 0x17, 0x3d, 0x85, 0x60, 0x9e, 0x89, 0x75, 0x9a, 0x2c, 0xb5,
+	0x34, 0xb1, 0x2e, 0x15, 0xf6, 0xbc, 0xd0, 0x91, 0xa9, 0x63, 0xdb, 0x73, 0x9a, 0x43, 0xc3, 0x0d,
+	0x8d, 0x44, 0x72, 0xb6, 0x4f, 0x7b, 0x03, 0x22, 0x7c, 0x32, 0xa8, 0x74, 0xe9, 0x1e, 0x06, 0xd0,
+	0xf4, 0x17, 0x68, 0x5b, 0x05, 0xde, 0xa6, 0xa4, 0xf4, 0xe4, 0xfd, 0xb1, 0x7d, 0x7d, 0x54, 0xff,
+	0xf7, 0x5f, 0x0a, 0x2d, 0x8a, 0xae, 0x6a, 0x76, 0x36, 0x7e, 0x0d, 0x00, 0x00, 0xff, 0xff, 0x38,
+	0xb7, 0x3b, 0x9b, 0x07, 0x03, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -157,6 +161,11 @@ const _ = grpc.SupportPackageIsVersion6
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type ReleaseManagerClient interface {
+	IsAuthenticator(ctx context.Context, in *empty.Empty, opts ...grpc.CallOption) (*ImplementsResp, error)
+	Auth(ctx context.Context, in *FuncSpec_Args, opts ...grpc.CallOption) (*empty.Empty, error)
+	AuthSpec(ctx context.Context, in *empty.Empty, opts ...grpc.CallOption) (*FuncSpec, error)
+	ValidateAuth(ctx context.Context, in *FuncSpec_Args, opts ...grpc.CallOption) (*empty.Empty, error)
+	ValidateAuthSpec(ctx context.Context, in *empty.Empty, opts ...grpc.CallOption) (*FuncSpec, error)
 	ConfigStruct(ctx context.Context, in *empty.Empty, opts ...grpc.CallOption) (*Config_StructResp, error)
 	Configure(ctx context.Context, in *Config_ConfigureRequest, opts ...grpc.CallOption) (*empty.Empty, error)
 	ReleaseSpec(ctx context.Context, in *Empty, opts ...grpc.CallOption) (*FuncSpec, error)
@@ -169,6 +178,51 @@ type releaseManagerClient struct {
 
 func NewReleaseManagerClient(cc grpc.ClientConnInterface) ReleaseManagerClient {
 	return &releaseManagerClient{cc}
+}
+
+func (c *releaseManagerClient) IsAuthenticator(ctx context.Context, in *empty.Empty, opts ...grpc.CallOption) (*ImplementsResp, error) {
+	out := new(ImplementsResp)
+	err := c.cc.Invoke(ctx, "/proto.ReleaseManager/IsAuthenticator", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *releaseManagerClient) Auth(ctx context.Context, in *FuncSpec_Args, opts ...grpc.CallOption) (*empty.Empty, error) {
+	out := new(empty.Empty)
+	err := c.cc.Invoke(ctx, "/proto.ReleaseManager/Auth", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *releaseManagerClient) AuthSpec(ctx context.Context, in *empty.Empty, opts ...grpc.CallOption) (*FuncSpec, error) {
+	out := new(FuncSpec)
+	err := c.cc.Invoke(ctx, "/proto.ReleaseManager/AuthSpec", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *releaseManagerClient) ValidateAuth(ctx context.Context, in *FuncSpec_Args, opts ...grpc.CallOption) (*empty.Empty, error) {
+	out := new(empty.Empty)
+	err := c.cc.Invoke(ctx, "/proto.ReleaseManager/ValidateAuth", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *releaseManagerClient) ValidateAuthSpec(ctx context.Context, in *empty.Empty, opts ...grpc.CallOption) (*FuncSpec, error) {
+	out := new(FuncSpec)
+	err := c.cc.Invoke(ctx, "/proto.ReleaseManager/ValidateAuthSpec", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
 }
 
 func (c *releaseManagerClient) ConfigStruct(ctx context.Context, in *empty.Empty, opts ...grpc.CallOption) (*Config_StructResp, error) {
@@ -209,6 +263,11 @@ func (c *releaseManagerClient) Release(ctx context.Context, in *FuncSpec_Args, o
 
 // ReleaseManagerServer is the server API for ReleaseManager service.
 type ReleaseManagerServer interface {
+	IsAuthenticator(context.Context, *empty.Empty) (*ImplementsResp, error)
+	Auth(context.Context, *FuncSpec_Args) (*empty.Empty, error)
+	AuthSpec(context.Context, *empty.Empty) (*FuncSpec, error)
+	ValidateAuth(context.Context, *FuncSpec_Args) (*empty.Empty, error)
+	ValidateAuthSpec(context.Context, *empty.Empty) (*FuncSpec, error)
 	ConfigStruct(context.Context, *empty.Empty) (*Config_StructResp, error)
 	Configure(context.Context, *Config_ConfigureRequest) (*empty.Empty, error)
 	ReleaseSpec(context.Context, *Empty) (*FuncSpec, error)
@@ -219,6 +278,21 @@ type ReleaseManagerServer interface {
 type UnimplementedReleaseManagerServer struct {
 }
 
+func (*UnimplementedReleaseManagerServer) IsAuthenticator(ctx context.Context, req *empty.Empty) (*ImplementsResp, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method IsAuthenticator not implemented")
+}
+func (*UnimplementedReleaseManagerServer) Auth(ctx context.Context, req *FuncSpec_Args) (*empty.Empty, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Auth not implemented")
+}
+func (*UnimplementedReleaseManagerServer) AuthSpec(ctx context.Context, req *empty.Empty) (*FuncSpec, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method AuthSpec not implemented")
+}
+func (*UnimplementedReleaseManagerServer) ValidateAuth(ctx context.Context, req *FuncSpec_Args) (*empty.Empty, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method ValidateAuth not implemented")
+}
+func (*UnimplementedReleaseManagerServer) ValidateAuthSpec(ctx context.Context, req *empty.Empty) (*FuncSpec, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method ValidateAuthSpec not implemented")
+}
 func (*UnimplementedReleaseManagerServer) ConfigStruct(ctx context.Context, req *empty.Empty) (*Config_StructResp, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method ConfigStruct not implemented")
 }
@@ -234,6 +308,96 @@ func (*UnimplementedReleaseManagerServer) Release(ctx context.Context, req *Func
 
 func RegisterReleaseManagerServer(s *grpc.Server, srv ReleaseManagerServer) {
 	s.RegisterService(&_ReleaseManager_serviceDesc, srv)
+}
+
+func _ReleaseManager_IsAuthenticator_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(empty.Empty)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ReleaseManagerServer).IsAuthenticator(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/proto.ReleaseManager/IsAuthenticator",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ReleaseManagerServer).IsAuthenticator(ctx, req.(*empty.Empty))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _ReleaseManager_Auth_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(FuncSpec_Args)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ReleaseManagerServer).Auth(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/proto.ReleaseManager/Auth",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ReleaseManagerServer).Auth(ctx, req.(*FuncSpec_Args))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _ReleaseManager_AuthSpec_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(empty.Empty)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ReleaseManagerServer).AuthSpec(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/proto.ReleaseManager/AuthSpec",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ReleaseManagerServer).AuthSpec(ctx, req.(*empty.Empty))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _ReleaseManager_ValidateAuth_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(FuncSpec_Args)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ReleaseManagerServer).ValidateAuth(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/proto.ReleaseManager/ValidateAuth",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ReleaseManagerServer).ValidateAuth(ctx, req.(*FuncSpec_Args))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _ReleaseManager_ValidateAuthSpec_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(empty.Empty)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ReleaseManagerServer).ValidateAuthSpec(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/proto.ReleaseManager/ValidateAuthSpec",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ReleaseManagerServer).ValidateAuthSpec(ctx, req.(*empty.Empty))
+	}
+	return interceptor(ctx, in, info, handler)
 }
 
 func _ReleaseManager_ConfigStruct_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
@@ -312,6 +476,26 @@ var _ReleaseManager_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "proto.ReleaseManager",
 	HandlerType: (*ReleaseManagerServer)(nil),
 	Methods: []grpc.MethodDesc{
+		{
+			MethodName: "IsAuthenticator",
+			Handler:    _ReleaseManager_IsAuthenticator_Handler,
+		},
+		{
+			MethodName: "Auth",
+			Handler:    _ReleaseManager_Auth_Handler,
+		},
+		{
+			MethodName: "AuthSpec",
+			Handler:    _ReleaseManager_AuthSpec_Handler,
+		},
+		{
+			MethodName: "ValidateAuth",
+			Handler:    _ReleaseManager_ValidateAuth_Handler,
+		},
+		{
+			MethodName: "ValidateAuthSpec",
+			Handler:    _ReleaseManager_ValidateAuthSpec_Handler,
+		},
 		{
 			MethodName: "ConfigStruct",
 			Handler:    _ReleaseManager_ConfigStruct_Handler,
