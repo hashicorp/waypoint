ba0ca50cf69d6e80ab7e7f6fd6183ce3a06a9389
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2020-07-01 17:43:08 -0700

    internal/clicontext: basic context structure and storage

---
+++ internal/clicontext/testing.go
@@ -0,0 +1,22 @@
+package clicontext
+
+import (
+	"io/ioutil"
+	"os"
+
+	"github.com/mitchellh/go-testing-interface"
+	"github.com/stretchr/testify/require"
+)
+
+// TestStorage returns a *Storage pointed at a temporary directory. This
+// will cleanup automatically by using t.Cleanup.
+func TestStorage(t testing.T) *Storage {
+	td, err := ioutil.TempDir("", "waypoint-test")
+	require.NoError(t, err)
+	t.Cleanup(func() { os.RemoveAll(td) })
+
+	st, err := NewStorage(WithDir(td))
+	require.NoError(t, err)
+
+	return st
+}
