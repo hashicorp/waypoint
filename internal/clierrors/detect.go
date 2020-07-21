1b45066021608a14c81aedac9461bf02845b3d31
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2020-07-20 17:11:42 -0700

    internal/clierrors: tests

--- internal/clierrors/detect.go
+++ internal/clierrors/detect.go
@@ -7,6 +7,8 @@ import (
 	"google.golang.org/grpc/status"
 )
 
+// IsCanceled is true if the error represents a cancellation. This detects
+// context cancellation as well as gRPC cancellation codes.
 func IsCanceled(err error) bool {
 	if err == context.Canceled {
 		return true
