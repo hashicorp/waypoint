0776949254e50aa2b5854e24d63910c6e3e04172
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2020-08-07 13:20:45 -0700

    internal/cli: messy default TLS implementation

--- internal/config/server.go
+++ internal/config/server.go
@@ -5,17 +5,21 @@ type ServerConfig struct {
 	// DBPath is the path to the database file, including the filename.
 	DBPath string `hcl:"db_path,attr"`
 
-	// Listeners sets up the listeners
-	Listeners Listeners `hcl:"listeners,block"`
+	// GRPC is the grpc service listening configuration. This is required.
+	GRPC Listener `hcl:"grpc,block"`
+
+	// HTTP is the listening configuration for the HTTP service for grpc-web.
+	HTTP Listener `hcl:"http,block"`
 
 	// URL configures a server to use a URL service.
 	URL *URL `hcl:"url,block"`
 }
 
-// Listeners is the configuration for the listeners.
-type Listeners struct {
-	GRPC string `hcl:"grpc,attr"`
-	HTTP string `hcl:"http,attr"`
+type Listener struct {
+	Addr        string `hcl:"address,attr"`
+	TLSDisable  bool   `hcl:"tls_disable,optional"`
+	TLSCertFile string `hcl:"tls_cert_file,optional"`
+	TLSKeyFile  string `hcl:"tls_key_file,optional"`
 }
 
 // URL is the configuration for the URL service.
