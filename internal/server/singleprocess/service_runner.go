bb7a5765f9946b59cacb28306e85f91b37086959
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2020-08-12 17:04:27 -0700

    internal/server: fix potential panic with nil access

--- internal/server/singleprocess/service_runner.go
+++ internal/server/singleprocess/service_runner.go
@@ -349,7 +349,8 @@ func (s *service) RunnerJobStream(
 			// If the job is canceled, send that event. We send this each time
 			// the cancel time changes. The cancel time only changes if multiple
 			// cancel requests are made.
-			if job.CancelTime != nil && lastJob.CancelTime != job.CancelTime {
+			if job.CancelTime != nil &&
+				(lastJob == nil || lastJob.CancelTime != job.CancelTime) {
 				// The job is forced if we're in an error state. This must be true
 				// because we would've already exited the loop if we naturally
 				// got a terminal event.
