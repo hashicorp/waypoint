b685cac192f65cc4835d9a1c7e1678cdfc95625f
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2020-08-22 15:08:42 -0700

    internal/server: track workspaces on operation index creation

--- internal/server/singleprocess/state/workspace.go
+++ internal/server/singleprocess/state/workspace.go
@@ -1,47 +1,106 @@
 package state
 
 import (
-	"strings"
-
-	"github.com/boltdb/bolt"
+	"github.com/hashicorp/go-memdb"
 
 	pb "github.com/hashicorp/waypoint/internal/server/gen"
 )
 
-var workspaceBucket = []byte("workspace")
-
 func init() {
-	dbBuckets = append(dbBuckets, workspaceBucket)
+	schemas = append(schemas, workspaceIndexSchema)
 }
 
-// workspaceDefaultForRef returns a default pb.Workspace for a ref. This
-// can be used in tandem with workspaceCreateIfNotExist to create defaults.
-func (s *State) workspaceDefaultForRef(ref *pb.Ref_Workspace) *pb.Workspace {
-	return &pb.Workspace{
-		Name: ref.Workspace,
-	}
-}
-
-func (s *State) workspaceCreateIfNotExist(tx *bolt.Tx, p *pb.Workspace) error {
-	b, err := s.workspaceBucket(tx, p)
+// workspaceInit creates an initial record for the given workspace or
+// returns one if it already exists.
+func (s *State) workspaceInit(
+	memTxn *memdb.Txn,
+	ref *pb.Ref_Workspace,
+	app *pb.Ref_Application,
+) (*workspaceIndexRecord, error) {
+	rec, err := s.workspaceGet(memTxn, ref, app)
 	if err != nil {
-		return nil
+		return nil, err
+	}
+	if rec != nil {
+		return rec, nil
 	}
 
-	id := []byte("value")
-	if b.Get(id) != nil {
-		// Workspace already exists
-		return nil
+	rec = &workspaceIndexRecord{
+		Name:    ref.Workspace,
+		Project: app.Project,
+		App:     app.Application,
+	}
+	return rec, s.workspacePut(memTxn, rec)
+}
+
+// workspacePut updates the workspace record.
+func (s *State) workspacePut(
+	memTxn *memdb.Txn,
+	rec *workspaceIndexRecord,
+) error {
+	return memTxn.Insert(workspaceIndexTableName, rec)
+}
+
+func (s *State) workspaceGet(
+	memTxn *memdb.Txn,
+	ref *pb.Ref_Workspace,
+	app *pb.Ref_Application,
+) (*workspaceIndexRecord, error) {
+	raw, err := memTxn.First(
+		workspaceIndexTableName,
+		workspaceIndexIdIndexName,
+		ref.Workspace,
+		app.Project,
+		app.Application,
+	)
+	if err != nil {
+		return nil, err
+	}
+	if raw == nil {
+		return nil, nil
 	}
 
-	// Create the workspace
-	return dbPut(b, id, p)
+	return raw.(*workspaceIndexRecord), nil
 }
 
-func (s *State) workspaceBucket(tx *bolt.Tx, p *pb.Workspace) (*bolt.Bucket, error) {
-	return tx.Bucket(workspaceBucket).CreateBucketIfNotExists(s.workspaceId(p))
+func workspaceIndexSchema() *memdb.TableSchema {
+	return &memdb.TableSchema{
+		Name: workspaceIndexTableName,
+		Indexes: map[string]*memdb.IndexSchema{
+			workspaceIndexIdIndexName: &memdb.IndexSchema{
+				Name:         workspaceIndexIdIndexName,
+				AllowMissing: false,
+				Unique:       true,
+				Indexer: &memdb.CompoundIndex{
+					Indexes: []memdb.Indexer{
+						&memdb.StringFieldIndex{
+							Field:     "Name",
+							Lowercase: true,
+						},
+
+						&memdb.StringFieldIndex{
+							Field:     "Project",
+							Lowercase: true,
+						},
+
+						&memdb.StringFieldIndex{
+							Field:     "App",
+							Lowercase: true,
+						},
+					},
+				},
+			},
+		},
+	}
 }
 
-func (s *State) workspaceId(p *pb.Workspace) []byte {
-	return []byte(strings.ToLower(p.Name))
+const (
+	workspaceIndexTableName   = "workspace-index"
+	workspaceIndexIdIndexName = "id"
+)
+
+type workspaceIndexRecord struct {
+	Name    string
+	Project string
+	App     string
 }
