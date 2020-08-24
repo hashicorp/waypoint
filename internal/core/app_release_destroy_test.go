24f5db6a75dd59742410012f772ea8232b98789b
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2020-08-23 21:20:47 -0700

    internal/core: app.DestroyRelease

---
+++ internal/core/app_release_destroy_test.go
@@ -0,0 +1,154 @@
+package core
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/golang/protobuf/ptypes"
+	"github.com/golang/protobuf/ptypes/any"
+	"github.com/golang/protobuf/ptypes/empty"
+	mockpkg "github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+
+	"github.com/hashicorp/waypoint/internal/config"
+	pb "github.com/hashicorp/waypoint/internal/server/gen"
+	"github.com/hashicorp/waypoint/sdk/component"
+	componentmocks "github.com/hashicorp/waypoint/sdk/component/mocks"
+)
+
+func TestAppCanDestroyRelease(t *testing.T) {
+	t.Run("can", func(t *testing.T) {
+		require := require.New(t)
+
+		// Our mock platform, which must also implement Destroyer
+		mock := struct {
+			*componentmocks.ReleaseManager
+			*componentmocks.Destroyer
+		}{
+			&componentmocks.ReleaseManager{},
+			&componentmocks.Destroyer{},
+		}
+		mock.Destroyer.On("DestroyFunc").Return(42)
+
+		// Make our factory for platforms
+		factory := TestFactory(t, component.ReleaseManagerType)
+		TestFactoryRegister(t, factory, "test", mock)
+
+		// Make our app
+		app := TestApp(t, TestProject(t,
+			WithConfig(config.TestConfig(t, testReleaseManagerConfig)),
+			WithFactory(component.ReleaseManagerType, factory),
+		), "test")
+
+		require.True(app.CanDestroyRelease())
+	})
+
+	t.Run("cannot", func(t *testing.T) {
+		require := require.New(t)
+
+		// Our mock platform, which must also implement Destroyer
+		mock := &componentmocks.ReleaseManager{}
+
+		// Make our factory for platforms
+		factory := TestFactory(t, component.ReleaseManagerType)
+		TestFactoryRegister(t, factory, "test", mock)
+
+		// Make our app
+		app := TestApp(t, TestProject(t,
+			WithConfig(config.TestConfig(t, testReleaseManagerConfig)),
+			WithFactory(component.ReleaseManagerType, factory),
+		), "test")
+
+		require.False(app.CanDestroyRelease())
+	})
+}
+
+func TestAppDestroyRelease_happy(t *testing.T) {
+	require := require.New(t)
+
+	// Our mock platform, which must also implement Destroyer
+	mock := struct {
+		*componentmocks.ReleaseManager
+		*componentmocks.Destroyer
+	}{
+		&componentmocks.ReleaseManager{},
+		&componentmocks.Destroyer{},
+	}
+
+	// Make our factory for platforms
+	factory := TestFactory(t, component.ReleaseManagerType)
+	TestFactoryRegister(t, factory, "test", mock)
+
+	// Make our app
+	app := TestApp(t, TestProject(t,
+		WithConfig(config.TestConfig(t, testReleaseManagerConfig)),
+		WithFactory(component.ReleaseManagerType, factory),
+	), "test")
+
+	// Expect to have the destroy function called
+	deployment, err := ptypes.MarshalAny(&empty.Empty{})
+	require.NoError(err)
+	mock.Destroyer.On("DestroyFunc").Return(func(v *any.Any) error {
+		if v == nil || v != deployment {
+			return fmt.Errorf("value didn't match")
+		}
+
+		return nil
+	})
+
+	{
+		// Destroy
+		require.NoError(app.DestroyRelease(context.Background(), &pb.Release{
+			Application: app.ref,
+			Workspace:   app.workspace,
+			Release:     deployment,
+		}))
+
+		// Verify that we set the status properly
+		resp, err := app.client.ListReleases(context.Background(), &pb.ListReleasesRequest{
+			Application: app.ref,
+			Workspace:   app.workspace,
+		})
+		require.NoError(err)
+		require.Equal(pb.Operation_DESTROYED, resp.Releases[0].State)
+	}
+
+	{
+		// Try with an error
+		mock.Destroyer.Mock = mockpkg.Mock{}
+		mock.Destroyer.On("DestroyFunc").Return(func() error {
+			return fmt.Errorf("error!")
+		})
+
+		err := app.DestroyRelease(context.Background(), &pb.Release{
+			Application: app.ref,
+			Workspace:   app.workspace,
+			Release:     deployment,
+		})
+		require.Error(err)
+		require.Contains(err.Error(), "error")
+
+		// Verify that we set the status properly
+		resp, err := app.client.ListReleases(context.Background(), &pb.ListReleasesRequest{
+			Application: app.ref,
+			Workspace:   app.workspace,
+			Order: &pb.OperationOrder{
+				Order: pb.OperationOrder_COMPLETE_TIME,
+				Desc:  true,
+			},
+		})
+		require.NoError(err)
+		require.Equal(pb.Operation_DESTROYED, resp.Releases[0].State)
+	}
+}
+
+const testReleaseManagerConfig = `
+project = "test"
+
+app "test" {
+	release {
+		use "test" {}
+	}
+}
+`
