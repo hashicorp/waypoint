093151956e2c23923041abd34a5f20a8e5ef87ff
Jack Pearkes <jackpearkes@gmail.com>
2020-08-28 14:22:19 -0700

    ui: fill in deployment pages

--- ui/app/routes/workspace/projects/project/app/deployment.ts
+++ ui/app/routes/workspace/projects/project/app/deployment.ts
@@ -1,7 +1,7 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 import ApiService from 'waypoint/services/api';
-import { GetDeploymentRequest, Deployment } from 'waypoint-pb';
+import { GetDeploymentRequest, Deployment, Ref } from 'waypoint-pb';
 import CurrentApplicationService from 'waypoint/services/current-application';
 import CurrentWorkspaceService from 'waypoint/services/current-workspace';
 
@@ -15,8 +15,10 @@ export default class DeploymentDetail extends Route {
   @service currentWorkspace!: CurrentWorkspaceService;
 
   async model(params: DeploymentModelParams) {
+    var ref = new Ref.Operation();
+    ref.setId(params.deployment_id);
     var req = new GetDeploymentRequest();
-    req.setDeploymentId(params.deployment_id);
+    req.setRef(ref);
 
     var resp = await this.api.client.getDeployment(req, {});
     let deploy: Deployment = resp;
