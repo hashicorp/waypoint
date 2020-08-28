9c07679549ca72c9dab591e0afd31f194ed8e3c1
Jack Pearkes <jackpearkes@gmail.com>
2020-08-28 14:21:50 -0700

    ui: add external-link component

---
+++ ui/app/components/external-link.ts
@@ -0,0 +1,19 @@
+import Component from '@glimmer/component';
+
+/**
+ *
+ * `ExternalLink` renders an anchor tag with the proper `rel` attribute and the
+ *    external link "exit" icon.
+ *
+ *
+ * ```
+ * <ExternalLink href="https://www.hashicorp.com">
+ *   Some Link Text
+ * </ExternalLink>
+ * ```
+ *
+ * @class ExternalLink
+ *
+ */
+
+export default class ExternalLinkComponent extends Component {}
