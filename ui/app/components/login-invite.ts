499b76a729d69db48017cbd00e18928b167544bc
Jack Pearkes <jackpearkes@gmail.com>
2020-09-04 13:07:01 -0700

    ui: thread through a cli query param, automatic login

--- ui/app/components/login-invite.ts
+++ ui/app/components/login-invite.ts
@@ -1,5 +1,5 @@
 import { inject as service } from '@ember/service';
-import Component from '@ember/component';
+import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import SessionService from 'waypoint/services/session';
 import RouterService from '@ember/routing/router-service';
@@ -7,12 +7,29 @@ import ApiService from 'waypoint/services/api';
 import { ConvertInviteTokenRequest } from 'waypoint-pb';
 import { tracked } from '@glimmer/tracking';
 
-export default class InviteLoginForm extends Component {
+interface InviteLoginFormArgs {
+  inviteToken: string;
+  cli: boolean;
+}
+
+export default class InviteLoginForm extends Component<InviteLoginFormArgs> {
   @service session!: SessionService;
   @service router!: RouterService;
   @service api!: ApiService;
 
   @tracked inviteToken = '';
+  @tracked cli = null;
+
+  constructor(owner: any, args: any) {
+    super(owner, args);
+
+    let { cli } = this.args;
+
+    // If this is a CLI invite login, do it automatically when the component loads
+    if (cli) {
+      this.login();
+    }
+  }
 
   @action
   async login() {
@@ -20,6 +37,14 @@ export default class InviteLoginForm extends Component {
     req.setToken(this.inviteToken);
     var resp = await this.api.client.convertInviteToken(req, this.api.WithMeta());
     await this.session.setToken(resp.getToken());
-    return this.router.transitionTo('onboarding.install');
+
+    // If this is an invite for a new user, take them to on-boarding, otherwise, take
+    // them to the workspaces page with a query parameter to specify it came
+    // from the CLI
+    if (this.cli) {
+      return this.router.transitionTo('workspaces', { queryParams: { cli: this.cli } });
+    } else {
+      return this.router.transitionTo('onboarding.install');
+    }
   }
 }
