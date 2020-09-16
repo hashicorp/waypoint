f09d8359c63e5de4a426fa7840afcff9e2d83998
Jack Pearkes <jackpearkes@gmail.com>
2020-09-15 22:08:47 -0700

    ui: support streaming app logs

--- ui/app/routes/workspace/projects/project/app/logs.ts
+++ ui/app/routes/workspace/projects/project/app/logs.ts
@@ -1,11 +1,19 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 import ApiService from 'waypoint/services/api';
+import { GetLogStreamRequest } from 'waypoint-pb';
+import { AppRouteModel } from '../app';
 
 export default class Logs extends Route {
   @service api!: ApiService;
 
   async model() {
-    // todo(pearkes): construct GetLogStreamRequest
+    const app = this.modelFor('workspace.projects.project.app') as AppRouteModel;
+    const latestDeployment = (await app.deployments).firstObject;
+    const req = new GetLogStreamRequest();
+
+    req.setDeploymentId(latestDeployment?.id!);
+
+    return req;
   }
 }
