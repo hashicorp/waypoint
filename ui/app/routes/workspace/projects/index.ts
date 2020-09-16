a7dbd40a164bdf5651e1899a0c2bc8defadc94dd
Jack Pearkes <jackpearkes@gmail.com>
2020-09-15 20:02:50 -0700

    ui: projects polling

--- ui/app/routes/workspace/projects/index.ts
+++ ui/app/routes/workspace/projects/index.ts
@@ -2,14 +2,20 @@ import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 import ApiService from 'waypoint/services/api';
 import { Empty } from 'google-protobuf/google/protobuf/empty_pb';
+import PollModelService from 'waypoint/services/poll-model';
 
 export default class Index extends Route {
   @service api!: ApiService;
+  @service pollModel!: PollModelService;
 
   async model() {
     let resp = await this.api.client.listProjects(new Empty(), this.api.WithMeta());
     let projects = resp.getProjectsList().map((p) => p.toObject());
 
     return projects;
+  }
+
+  afterModel() {
+    this.pollModel.setup(this);
   }
 }
