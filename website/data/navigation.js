cab4394fbe2255e8898b813bbadf78282d9e27a4
Jenn Briden <jenn.allen@gmail.com>
2020-09-17 12:23:26 -0400

    Removed Community from nav

--- website/data/navigation.js
+++ website/data/navigation.js
@@ -9,9 +9,10 @@ export default [
     url: '/commands',
     type: 'inbound',
   },
-  {
+  // Commenting out the Community section until release
+  /*   {
     text: 'Community',
     url: '/community',
     type: 'inbound',
-  },
+  }, */
 ]
