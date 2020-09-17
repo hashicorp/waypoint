d7ddbc313aad429f2aa75e70bddd5fa5243b8c34
dizzyup <hector@me.com>
2020-09-17 13:49:00 +0100

    Update installation instructions

--- ui/app/routes/onboarding/install/index.ts
+++ ui/app/routes/onboarding/install/index.ts
@@ -1,24 +1,24 @@
 import Route from '@ember/routing/route';
-import { UAParser } from 'ua-parser-js';
+// import { UAParser } from 'ua-parser-js';
 
 export default class OnboardingInstallIndex extends Route {
-  redirect() {
-    const parser = new UAParser();
-
-    switch (parser.getResult().os.name) {
-      case 'Mac OS':
-        return this.transitionTo('onboarding.install.homebrew');
-      case 'Windows':
-        return this.transitionTo('onboarding.install.chocolatey');
-      case 'Debian':
-      case 'Ubuntu':
-        return this.transitionTo('onboarding.install.linux.ubuntu');
-      case 'CentOS':
-        return this.transitionTo('onboarding.install.linux.centos');
-      case 'Fedora':
-        return this.transitionTo('onboarding.install.linux.fedora');
-      default:
-        return this.transitionTo('onboarding.install.manual');
-    }
-  }
+  //   redirect() {
+  //     const parser = new UAParser();
+  //
+  //     switch (parser.getResult().os.name) {
+  //       case 'Mac OS':
+  //         return this.transitionTo('onboarding.install.homebrew');
+  //       case 'Windows':
+  //         return this.transitionTo('onboarding.install.chocolatey');
+  //       case 'Debian':
+  //       case 'Ubuntu':
+  //         return this.transitionTo('onboarding.install.linux.ubuntu');
+  //       case 'CentOS':
+  //         return this.transitionTo('onboarding.install.linux.centos');
+  //       case 'Fedora':
+  //         return this.transitionTo('onboarding.install.linux.fedora');
+  //       default:
+  //         return this.transitionTo('onboarding.install.manual');
+  //     }
+  //   }
 }
