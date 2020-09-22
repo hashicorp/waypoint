9c0cb7fe6d793414dc3323e7efadb6bec3295c7b
Jeff Escalante <jescalan@users.noreply.github.com>
2020-09-22 18:24:30 -0400

    lock code, remove unneeded head loads

--- website/components/temporary_docs-page/index.jsx
+++ website/components/temporary_docs-page/index.jsx
@@ -1,3 +1,14 @@
+// -----------------------------------------------------
+//                This code is LOCKED
+//
+// If any changes are needed to this code, or if this code
+// is needed in any other projects, instead of changing or
+// using it, instead we must complete this task as a prerequisite
+//
+// https://app.asana.com/0/1100423001970639/1195001770724993
+//
+// ------------------------------------------------------
+
 import DocsPageComponent from '@hashicorp/react-docs-page'
 import Head from 'next/head'
 import Link from 'next/link'
--- website/pages/_app.js
+++ website/pages/_app.js
@@ -30,26 +30,8 @@ function App({ Component, pageProps }) {
           title={`${productName} by HashiCorp`}
           siteName={`${productName} by HashiCorp`}
           description="Waypoint allows developers to define their application build, deploy, and release lifecycle as code, with a consistent 'waypoint up' workflow."
-          image="https://www.example.com/img/og-image.png"
-          stylesheet={[
-            {
-              href:
-                'https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700&display=swap',
-            },
-          ]}
+          image="https://www.waypointproject.io/img/og-image.png"
           icon={[{ href: '/favicon.svg' }]}
-          preload={[
-            { href: '/fonts/klavika/medium.woff2', as: 'font' },
-            { href: '/fonts/gilmer/light.woff2', as: 'font' },
-            { href: '/fonts/gilmer/regular.woff2', as: 'font' },
-            { href: '/fonts/gilmer/medium.woff2', as: 'font' },
-            { href: '/fonts/gilmer/bold.woff2', as: 'font' },
-            { href: '/fonts/metro-sans/book.woff2', as: 'font' },
-            { href: '/fonts/metro-sans/regular.woff2', as: 'font' },
-            { href: '/fonts/metro-sans/semi-bold.woff2', as: 'font' },
-            { href: '/fonts/metro-sans/bold.woff2', as: 'font' },
-            { href: '/fonts/dejavu/mono.woff2', as: 'font' },
-          ]}
         />
         <ProductSubnav />
         <div className="content">
