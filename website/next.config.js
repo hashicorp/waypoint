aededfcf976fa3b6ca5c6469ef7d654415ff176c
Jimmy Merritello <7191639+jmfury@users.noreply.github.com>
2020-09-22 17:48:42 -0500

    Add auth0 as an authProvider w/ abstractions

--- website/next.config.js
+++ website/next.config.js
@@ -4,7 +4,9 @@ const redirects = require('./redirects')
 // log out our primary environment variables for clarity in build logs
 console.log(`HASHI_ENV: ${process.env.HASHI_ENV}`)
 console.log(`NODE_ENV: ${process.env.NODE_ENV}`)
+console.log(`NEXTAUTH_URL: ${process.env.NEXTAUTH_URL}`)
 console.log(`OKTA_DOMAIN: ${process.env.OKTA_DOMAIN}`)
+console.log(`AUTH0_DOMAIN: ${process.env.AUTH0_DOMAIN}`)
 
 module.exports = withHashicorp({
   defaultLayout: true,
@@ -18,7 +20,7 @@ module.exports = withHashicorp({
     SEGMENT_WRITE_KEY: '9mlIVayJbNtJW2EOdAFKHNKcdLAgEDlV',
     BUGSNAG_CLIENT_KEY: '98922c3298fff145a2d154ad2e6d4e6a',
     BUGSNAG_SERVER_KEY: '45f0129bdbe991d7fdcd0338a1a4f1d7',
-    NEXT_PUBLIC_OKTA_CLIENT_ID: process.env.OKTA_CLIENT_ID,
     NEXT_PUBLIC_OKTA_DOMAIN: process.env.OKTA_DOMAIN,
+    NEXT_PUBLIC_AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
   },
 })
--- website/pages/api/auth/[...nextauth].js
+++ website/pages/api/auth/[...nextauth].js
@@ -1,18 +1,12 @@
-import NextAuth from 'next-auth'
-import Providers from 'next-auth/providers'
+import nextAuthApiRoute from 'lib/next-auth-utils/config'
 
-const options = {
-  // Configure one or more authentication providers
-  providers: [
-    Providers.Okta({
-      clientId: process.env.OKTA_CLIENT_ID,
-      clientSecret: process.env.OKTA_CLIENT_SECRET,
-      domain: process.env.OKTA_DOMAIN,
-    }),
-  ],
-  pages: {
-    error: '/signin-error', // Error code passed in query string as ?error=
-  },
-}
-
-export default (req, res) => NextAuth(req, res, options)
+export default (req, res) =>
+  nextAuthApiRoute(
+    req,
+    res
+  )({
+    environments: { production: ['Okta'], preview: ['Auth0', 'Okta'] },
+    pages: {
+      error: '/signin-error', // Error code passed in query string as ?error=
+    },
+  })
