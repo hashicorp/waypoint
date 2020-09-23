aad745dab216ada18d55c3ac2b7d5d735e7835aa
Jimmy Merritello <7191639+jmfury@users.noreply.github.com>
2020-09-23 10:32:53 -0500

    Invert control to auth api route

--- website/lib/next-auth-utils/config.js
+++ website/lib/next-auth-utils/config.js
@@ -1,33 +1,6 @@
 import NextAuth from 'next-auth'
 import NextAuthProviders from 'next-auth/providers'
 
-function getSiteProvidersForEnvironment() {
-  let providers = []
-  switch (process.env.HASHI_ENV) {
-    case 'production':
-      providers = ['Okta']
-      break
-    case 'preview':
-      providers = ['Auth0', 'Okta']
-      break
-    case 'development':
-      break
-    default:
-      break
-  }
-  return providers
-}
-
-export const customAuthPages = {
-  pages: {
-    error: '/signin-error', // Error code passed in query string as ?error=
-  },
-}
-
-export const siteAuthProviders = {
-  providers: getSiteProvidersForEnvironment().map(formatProviderConfig),
-}
-
 function formatProviderConfig(ap) {
   const apName = ap.toUpperCase()
   const config = {
@@ -38,5 +11,9 @@ function formatProviderConfig(ap) {
   return NextAuthProviders[ap](config)
 }
 
-export default (req, res) =>
-  NextAuth(req, res, { ...siteAuthProviders, ...customAuthPages })
+export default (req, res) => ({ environments, pages }) =>
+  NextAuth(req, res, {
+    providers:
+      environments[process.env.HASHI_ENV]?.map(formatProviderConfig) || [],
+    pages,
+  })
--- website/pages/api/auth/[...nextauth].js
+++ website/pages/api/auth/[...nextauth].js
@@ -1,3 +1,12 @@
 import nextAuthApiRoute from 'lib/next-auth-utils/config'
 
-export default nextAuthApiRoute
+export default (req, res) =>
+  nextAuthApiRoute(
+    req,
+    res
+  )({
+    environments: { production: ['Okta'], preview: ['Auth0', 'Okta'] },
+    pages: {
+      error: '/signin-error', // Error code passed in query string as ?error=
+    },
+  })
