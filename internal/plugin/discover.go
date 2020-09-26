e387f997e312f236eca007b6ab7d6337351f7596
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2020-09-25 20:42:37 -0700

    internal/plugin: discovery funcs

---
+++ internal/plugin/discover.go
@@ -0,0 +1,66 @@
+package plugin
+
+import (
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+
+	"github.com/adrg/xdg"
+
+	"github.com/hashicorp/waypoint/internal/config"
+)
+
+// Discover finds the given plugin and returns the command for it. The command
+// can subsequently be used with Factory to build a factory for a specific
+// plugin type. If the plugin is not found `(nil, nil)` is returned.
+//
+// The plugin binary must have the form "waypoint-plugin-<name>" (with a
+// ".exe" extension on Windows).
+//
+// This will search the paths given. You can use DefaultPaths() to get
+// the default set of paths.
+//
+func Discover(cfg *config.Plugin, paths []string) (*exec.Cmd, error) {
+	// Expected filename
+	expected := "waypoint-plugin-" + cfg.Name
+	if runtime.GOOS == "windows " {
+		expected += ".exe"
+	}
+
+	// Search our paths
+	for _, path := range paths {
+		_, err := os.Stat(filepath.Join(path, expected))
+		if err == nil {
+			cmd := exec.Command(path)
+			return cmd, nil
+		}
+
+		if os.IsNotExist(err) {
+			continue
+		}
+
+		return nil, err
+	}
+
+	return nil, nil
+}
+
+// DefaultPaths returns the default search paths for plugins. These are:
+//
+//   * pwd given
+//   * "$pwd/.waypoint/plugins"
+//   * "$XDG_CONFIG_DIR/waypoint/plugins"
+//
+func DefaultPaths(pwd string) ([]string, error) {
+	xdgPath, err := xdg.ConfigFile("waypoint/plugins/.ignore")
+	if err != nil {
+		return nil, err
+	}
+
+	return []string{
+		pwd,
+		filepath.Join(pwd, ".waypoint", "plugins"),
+		filepath.Dir(xdgPath),
+	}, nil
+}
--- internal/plugin/plugin.go
+++ internal/plugin/plugin.go
@@ -21,11 +21,6 @@ import (
 )
 
 var (
-	Builders   = mustFactory(factory.New((*component.Builder)(nil)))
-	Registries = mustFactory(factory.New((*component.Registry)(nil)))
-	Platforms  = mustFactory(factory.New((*component.Platform)(nil)))
-	Releasers  = mustFactory(factory.New((*component.ReleaseManager)(nil)))
-
 	// Builtins is the map of all available builtin plugins and their
 	// options for launching them.
 	Builtins = map[string][]sdk.Option{
@@ -43,30 +38,44 @@ var (
 		"aws-ec2":                  ec2.Options,
 		"aws-alb":                  alb.Options,
 	}
+
+	// BaseFactories is the set of base plugin factories. This will include any
+	// built-in or well-known plugins by default. This should be used as the base
+	// for building any set of factories.
+	BaseFactories = map[component.Type]*factory.Factory{
+		component.BuilderType:        mustFactory(factory.New(component.TypeMap[component.BuilderType])),
+		component.RegistryType:       mustFactory(factory.New(component.TypeMap[component.RegistryType])),
+		component.PlatformType:       mustFactory(factory.New(component.TypeMap[component.PlatformType])),
+		component.ReleaseManagerType: mustFactory(factory.New(component.TypeMap[component.ReleaseManagerType])),
+	}
 )
 
 func init() {
-	Builders.Register("docker", BuiltinFactory("docker", component.BuilderType))
-	Builders.Register("files", BuiltinFactory("files", component.BuilderType))
-	Builders.Register("pack", BuiltinFactory("pack", component.BuilderType))
-	Builders.Register("aws-ami", BuiltinFactory("aws-ami", component.BuilderType))
+	b := BaseFactories[component.BuilderType]
+	b.Register("docker", BuiltinFactory("docker", component.BuilderType))
+	b.Register("files", BuiltinFactory("files", component.BuilderType))
+	b.Register("pack", BuiltinFactory("pack", component.BuilderType))
+	b.Register("aws-ami", BuiltinFactory("aws-ami", component.BuilderType))
 
-	Registries.Register("docker", BuiltinFactory("docker", component.RegistryType))
-	Registries.Register("files", BuiltinFactory("files", component.RegistryType))
-	Registries.Register("aws-ecr", BuiltinFactory("aws-ecr", component.RegistryType))
+	b = BaseFactories[component.RegistryType]
+	b.Register("docker", BuiltinFactory("docker", component.RegistryType))
+	b.Register("files", BuiltinFactory("files", component.RegistryType))
+	b.Register("aws-ecr", BuiltinFactory("aws-ecr", component.RegistryType))
 
-	Platforms.Register("google-cloud-run", BuiltinFactory("google-cloud-run", component.PlatformType))
-	Platforms.Register("kubernetes", BuiltinFactory("kubernetes", component.PlatformType))
-	Platforms.Register("azure-container-instance", BuiltinFactory("azure-container-instance", component.PlatformType))
-	Platforms.Register("netlify", BuiltinFactory("netlify", component.PlatformType))
-	Platforms.Register("docker", BuiltinFactory("docker", component.PlatformType))
-	Platforms.Register("aws-ecs", BuiltinFactory("aws-ecs", component.PlatformType))
-	Platforms.Register("nomad", BuiltinFactory("nomad", component.PlatformType))
-	Platforms.Register("aws-ec2", BuiltinFactory("aws-ec2", component.PlatformType))
+	b = BaseFactories[component.PlatformType]
+	b.Register("google-cloud-run", BuiltinFactory("google-cloud-run", component.PlatformType))
+	b.Register("kubernetes", BuiltinFactory("kubernetes", component.PlatformType))
+	b.Register("azure-container-instance", BuiltinFactory("azure-container-instance", component.PlatformType))
+	b.Register("netlify", BuiltinFactory("netlify", component.PlatformType))
+	b.Register("docker", BuiltinFactory("docker", component.PlatformType))
+	b.Register("aws-ecs", BuiltinFactory("aws-ecs", component.PlatformType))
+	b.Register("nomad", BuiltinFactory("nomad", component.PlatformType))
+	b.Register("aws-ec2", BuiltinFactory("aws-ec2", component.PlatformType))
 
-	Releasers.Register("google-cloud-run", BuiltinFactory("google-cloud-run", component.ReleaseManagerType))
-	Releasers.Register("kubernetes", BuiltinFactory("kubernetes", component.ReleaseManagerType))
-	Releasers.Register("aws-alb", BuiltinFactory("aws-alb", component.ReleaseManagerType))
+	b = BaseFactories[component.ReleaseManagerType]
+	b.Register("google-cloud-run", BuiltinFactory("google-cloud-run", component.ReleaseManagerType))
+	b.Register("kubernetes", BuiltinFactory("kubernetes", component.ReleaseManagerType))
+	b.Register("aws-alb", BuiltinFactory("aws-alb", component.ReleaseManagerType))
 }
 
 func must(err error) {
