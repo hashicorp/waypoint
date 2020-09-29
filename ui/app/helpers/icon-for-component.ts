155e97d398ca0280ae8f37e7d13927963b76dbdd
dizzyup <hector@me.com>
2020-09-29 23:20:13 +0100

    Add additional service icons, show for builds

--- ui/app/helpers/icon-for-component.ts
+++ ui/app/helpers/icon-for-component.ts
@@ -3,14 +3,26 @@ import { helper } from '@ember/component/helper';
 // iconForComponent
 export function iconForComponent([component]: [string]): string {
   switch (component) {
-    case 'kubernetes':
-      return 'logo-kubernetes-color';
+    case 'aws-ec2':
+      return 'logo-aws-color';
+    case 'aws-ecs':
+      return 'logo-aws-color';
+    case 'azure-container-instances':
+      return 'logo-azure-color';
     case 'docker':
       return 'logo-docker-color';
     case 'google-cloud-run':
       return 'logo-gcp-color';
+    case 'kubernetes':
+      return 'logo-kubernetes-color';
+    case 'netlify':
+      return 'logo-netlify-color';
+    case 'nomad':
+      return 'logo-nomad-color';
+    case 'pack':
+      return 'logo-pack-color';
     default:
-      return 'help-circle-outline';
+      return 'more-horizontal';
   }
 }
 
