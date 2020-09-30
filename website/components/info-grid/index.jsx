e45ebe8effabc36adeb721600b749055a14038e1
Brandon Romano <BrandonRRomano@gmail.com>
2020-09-30 13:53:35 -0700

    Homepage 'Why Waypoint' Section (#297)
    * Implement HomepageSection
    
    * Stub the sections
    
    * Implements why waypoint InfoGrid
    
    * Adds diagram to why waypoint section

---
+++ website/components/info-grid/index.jsx
@@ -0,0 +1,17 @@
+import styles from './InfoGrid.module.css'
+
+export default function InfoGrid({ items }) {
+  return (
+    <div className={styles.infoGrid}>
+      <ul>
+        {items.map((item) => (
+          <li key={item.title}>
+            <img src={item.icon} />
+            <h4>{item.title}</h4>
+            <p>{item.description}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
