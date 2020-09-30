d166c2b63f7c2d82a9303e00b7dbeda47ccf6b4b
Brandon Romano <BrandonRRomano@gmail.com>
2020-09-30 13:53:35 -0700

    Homepage 'Why Waypoint' Section (#297)
    * Implement HomepageSection
    
    * Stub the sections
    
    * Implements why waypoint InfoGrid
    
    * Adds diagram to why waypoint section

---
+++ website/components/homepage-section/index.jsx
@@ -0,0 +1,19 @@
+import styles from './HomepageSection.module.css'
+import classNames from 'classnames'
+
+export default function HomepageSection({ title, theme, children }) {
+  return (
+    <section
+      className={classNames(styles.homepageSection, {
+        [styles.light]: theme === 'light',
+        [styles.gray]: theme === 'gray',
+        [styles.dark]: theme === 'dark',
+      })}
+    >
+      <div className={styles.gridContainer}>
+        <h2>{title}</h2>
+        {children}
+      </div>
+    </section>
+  )
+}
