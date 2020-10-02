9006abbc4bb87fc9d1177914db87b2c5d62e353c
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2020-10-02 15:42:32 -0700

    builtin/docker: use term output capture for push

--- builtin/docker/registry.go
+++ builtin/docker/registry.go
@@ -1,6 +1,7 @@
 package docker
 
 import (
+	"bytes"
 	"context"
 	"encoding/base64"
 	"encoding/json"
@@ -12,9 +13,9 @@ import (
 	"github.com/docker/docker/client"
 	"github.com/docker/docker/pkg/jsonmessage"
 	"github.com/docker/docker/registry"
+	"github.com/hashicorp/go-hclog"
 	"github.com/hashicorp/waypoint/sdk/docs"
 	"github.com/hashicorp/waypoint/sdk/terminal"
-	"github.com/mattn/go-isatty"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -39,27 +40,32 @@ func (r *Registry) Push(
 	ctx context.Context,
 	img *Image,
 	ui terminal.UI,
+	log hclog.Logger,
 ) (*Image, error) {
-	ui.Output("Tagging Docker image: %s => %s:%s", img.Name(), r.config.Image, r.config.Tag)
-
-	stdout, stderr, err := ui.OutputWriters()
+	stdout, _, err := ui.OutputWriters()
 	if err != nil {
 		return nil, status.Errorf(codes.FailedPrecondition, "unable to create output for logs:%s", err)
 	}
 
-	target := &Image{Image: r.config.Image, Tag: r.config.Tag}
+	sg := ui.StepGroup()
+	step := sg.Add("Initializing Docker client...")
+	defer func() { step.Abort() }()
 
 	cli, err := client.NewClientWithOpts(client.FromEnv)
 	if err != nil {
 		return nil, status.Errorf(codes.FailedPrecondition, "unable to create Docker client:%s", err)
 	}
-
 	cli.NegotiateAPIVersion(ctx)
 
+	step.Update("Tagging Docker image: %s => %s:%s", img.Name(), r.config.Image, r.config.Tag)
+
+	target := &Image{Image: r.config.Image, Tag: r.config.Tag}
 	err = cli.ImageTag(ctx, img.Name(), target.Name())
 	if err != nil {
 		return nil, status.Errorf(codes.Internal, "unable to tag image:%s", err)
 	}
+
+	step.Done()
 
 	if r.config.Local {
 		return target, nil
@@ -71,7 +77,6 @@ func (r *Registry) Push(
 	}
 
 	encodedAuth := r.config.EncodedAuth
-
 	if encodedAuth == "" {
 		// Resolve the Repository name from fqn to RepositoryInfo
 		repoInfo, err := registry.ParseRepositoryInfo(ref)
@@ -92,7 +97,12 @@ func (r *Registry) Push(
 			server = repoInfo.Index.Name
 		}
 
-		cf := config.LoadDefaultConfigFile(stderr)
+		var errBuf bytes.Buffer
+		cf := config.LoadDefaultConfigFile(&errBuf)
+		if errBuf.Len() > 0 {
+			// NOTE(mitchellh): I don't know why we ignore this, but we always have.
+			log.Warn("error loading Docker config file", "err", err)
+		}
 
 		authConfig, _ := cf.GetAuthConfig(server)
 		buf, err := json.Marshal(authConfig)
@@ -101,6 +111,8 @@ func (r *Registry) Push(
 		}
 		encodedAuth = base64.URLEncoding.EncodeToString(buf)
 	}
+
+	step = sg.Add("Pushing Docker image...")
 
 	options := types.ImagePushOptions{
 		RegistryAuth: encodedAuth,
@@ -113,22 +125,20 @@ func (r *Registry) Push(
 
 	defer responseBody.Close()
 
-	var (
-		termFd uintptr
-		isTerm bool
-	)
-
+	var termFd uintptr
 	if f, ok := stdout.(*os.File); ok {
 		termFd = f.Fd()
-		isTerm = isatty.IsTerminal(termFd)
 	}
 
-	err = jsonmessage.DisplayJSONMessagesStream(responseBody, stdout, termFd, isTerm, nil)
+	err = jsonmessage.DisplayJSONMessagesStream(responseBody, step.TermOutput(), termFd, true, nil)
 	if err != nil {
 		return nil, status.Errorf(codes.Internal, "unable to stream Docker logs to terminal: %s", err)
 	}
 
-	ui.Output("Docker image pushed: %s:%s", r.config.Image, r.config.Tag)
+	step.Done()
+
+	step = sg.Add("Docker image pushed: %s:%s", r.config.Image, r.config.Tag)
+	step.Done()
 
 	return target, nil
 }
