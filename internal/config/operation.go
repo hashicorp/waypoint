772e5f0b0889be551132503b07657187e0ae3ddd
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2020-10-06 18:06:15 -0700

    internal/config: require build/deploy blocks

--- internal/config/operation.go
+++ internal/config/operation.go
@@ -11,10 +11,13 @@ type Operation struct {
 	Labels map[string]string `hcl:"labels,optional"`
 	Hooks  []*Hook           `hcl:"hook,block"`
 	Use    *Use              `hcl:"use,block"`
+
+	// set internally to note an operation is required for validation
+	required bool
 }
 
 func (b *Build) Operation() *Operation {
-	return mapoperation(b)
+	return mapoperation(b, true)
 }
 
 func (b *Build) RegistryOperation() *Operation {
@@ -26,20 +29,20 @@ func (b *Build) RegistryOperation() *Operation {
 }
 
 func (b *Registry) Operation() *Operation {
-	return mapoperation(b)
+	return mapoperation(b, false)
 }
 
 func (b *Deploy) Operation() *Operation {
-	return mapoperation(b)
+	return mapoperation(b, true)
 }
 
 func (b *Release) Operation() *Operation {
-	return mapoperation(b)
+	return mapoperation(b, false)
 }
 
 // mapoperation takes a struct that is a superset of Operation and
 // maps it down to an Operation. This will panic if this fails.
-func mapoperation(input interface{}) *Operation {
+func mapoperation(input interface{}, req bool) *Operation {
 	if input == nil {
 		return nil
 	}
@@ -48,6 +51,7 @@ func mapoperation(input interface{}) *Operation {
 	if err := mapstructure.Decode(input, &op); err != nil {
 		panic(err)
 	}
+	op.required = req
 
 	return &op
 }
