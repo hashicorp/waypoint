467a3981a7634d3651b6fc2d0acae2704340e207
Brandon Romano <BrandonRRomano@gmail.com>
2020-10-07 02:05:45 -0700

    Couple animation progress to scrolling

--- website/components/animated-steps-list/index.jsx
+++ website/components/animated-steps-list/index.jsx
@@ -2,15 +2,50 @@ import styles from './AnimatedStepsList.module.css'
 import StepsList from './steps-list'
 import StepsIndicator from './steps-indicator'
 import useScrollPosition from 'lib/hooks/useScrollPosition'
-import SteppedAnimatedTerminal from './stepped-animated-terminal'
-
+import AnimatedTerminal from 'components/animated-terminal'
+import FramedTerminal from 'components/animated-terminal/framed-terminal'
 import { useState } from 'react'
+
+// The breakpoints where the next step of each animation triggers
+const breakpoints = [0, 350, 1258, 2309, 2880]
+
+// The number of pixels before the next breakpoint that the animation should complete
+const animationBottomPadding = 100
+
+function calculateCurrentFrame(terminalSteps, currentIndex, scrollPosition) {
+  const percentage = Math.min(
+    (scrollPosition - breakpoints[currentIndex]) /
+      (breakpoints[currentIndex + 1] -
+        breakpoints[currentIndex] -
+        animationBottomPadding),
+    1
+  )
+  const currentLines = terminalSteps[currentIndex].lines
+  let totalFrames = 0
+  currentLines.forEach((line) => {
+    let frames = line.frames ? line.frames : 1
+    if (Array.isArray(line.code)) {
+      totalFrames += line.code.length * frames
+    } else {
+      totalFrames += frames
+    }
+  })
+  return Math.max(0, percentage * (totalFrames - 1))
+}
 
 export default function AnimatedStepsList({ terminalHeroState, steps }) {
   const scrollPosition = useScrollPosition()
   const [indicatorIndex, setIndicatorIndex] = useState(0)
   const activeTerminalStateIndex =
     scrollPosition <= 350 ? 0 : indicatorIndex + 1
+  const terminalSteps = [terminalHeroState].concat(
+    steps.map((step) => step.terminal)
+  )
+  const currentFrame = calculateCurrentFrame(
+    terminalSteps,
+    activeTerminalStateIndex,
+    scrollPosition
+  )
 
   return (
     <div className={styles.animatedStepsList}>
@@ -27,10 +62,18 @@ export default function AnimatedStepsList({ terminalHeroState, steps }) {
       />
 
       <div className={styles.terminalWrapper}>
-        <SteppedAnimatedTerminal
-          activeIndex={activeTerminalStateIndex}
-          steps={[terminalHeroState].concat(steps.map((step) => step.terminal))}
-        />
+        {activeTerminalStateIndex === 0 ? (
+          <AnimatedTerminal
+            frameLength={terminalSteps[activeTerminalStateIndex].frameLength}
+            loop={terminalSteps[activeTerminalStateIndex].loop}
+            lines={terminalSteps[activeTerminalStateIndex].lines}
+          />
+        ) : (
+          <FramedTerminal
+            frame={currentFrame}
+            lines={terminalSteps[activeTerminalStateIndex].lines}
+          />
+        )}
       </div>
     </div>
   )
