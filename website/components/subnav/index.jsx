45c01a43fd9cb613c9ca8847174998bc07b152e9
Brandon Romano <BrandonRRomano@gmail.com>
2020-10-07 00:24:56 -0700

    Fixes a nav bug
    Fix a nav bug where the active underline was not applying to docs / cli
    / plugins pages.

--- website/components/subnav/index.jsx
+++ website/components/subnav/index.jsx
@@ -4,6 +4,11 @@ import Link from 'next/link'
 import subnavItems from 'data/navigation'
 import { productSlug } from 'data/metadata'
 
+// A regex to match the pathname that comes out of router.pathname
+// when the path is a content page (e.g. /docs/[[...page]]) to strip
+// out the part that we're not interested in.
+const multipathRegex = /\/\[\[.*\]\]/g
+
 export default function ProductSubnav() {
   const router = useRouter()
   return (
@@ -22,7 +27,7 @@ export default function ProductSubnav() {
           url: '/downloads',
         },
       ]}
-      currentPath={router.pathname}
+      currentPath={router.pathname.replace(multipathRegex, '')}
       menuItemsAlign="right"
       menuItems={subnavItems}
       constrainWidth
