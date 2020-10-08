c1d9bf7bdd5717a7465ec2505bdf60cd5638ed6a
Brandon Romano <BrandonRRomano@gmail.com>
2020-10-08 12:04:46 -0700

    Adds note for beta testers to download the binary

--- website/data/alert-banner.js
+++ website/data/alert-banner.js
@@ -1,7 +1,9 @@
-export const ALERT_BANNER_ACTIVE = false
+export const ALERT_BANNER_ACTIVE = true
 
 export default {
-  tag: 'ANNOUNCING',
-  text: 'Some details about an announcement here',
-  url: 'https://hashicorp.com',
+  tag: 'Beta Testers',
+  text:
+    'The Download page is still in progress.  You can download the Beta binary by clicking this banner.',
+  url:
+    'http://ihngtake2gyn8nbyfgtgvu449dnsbrgopvukjdbntyndmlv7tb.s3-website-us-east-1.amazonaws.com/waypoint/',
 }
