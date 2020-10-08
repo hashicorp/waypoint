aa395bdfd3008ebe2450e15c18c1a526a186322a
Jack Pearkes <jackpearkes@gmail.com>
2020-10-08 16:34:32 -0700

    ui: support streaming operation logs

---
+++ ui/app/helpers/strip-ansi.ts
@@ -0,0 +1,16 @@
+import { helper } from '@ember/component/helper';
+
+// stripAnsi
+export function stripAnsi([text]: [string]): string {
+  if (!text) return '';
+
+  const pattern = [
+    '[\\u001B\\u009B][[\\]()#;?]*(?:(?:(?:[a-zA-Z\\d]*(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]*)*)?\\u0007)',
+    '(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PR-TZcf-ntqry=><~]))',
+  ].join('|');
+
+  const regex = new RegExp(pattern, 'g');
+  return text.replace(regex, '');
+}
+
+export default helper(stripAnsi);
