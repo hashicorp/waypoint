a0fa5409c80ee55cca2f725aaa17747c38d504ae
Brandon Romano <BrandonRRomano@gmail.com>
2020-10-13 00:35:02 -0700

    Update logos to be independent for alt text support (#429)

--- website/components/animated-steps-list/steps-list/step/index.jsx
+++ website/components/animated-steps-list/steps-list/step/index.jsx
@@ -1,12 +1,12 @@
 import styles from './Step.module.css'
 import { useState, useEffect } from 'react'
 import { useInView } from 'react-intersection-observer'
+import LogoList from 'components/logo-list'
 
 export default function Step({
   name,
   description,
   logos,
-  logosAlt,
   onInViewStatusChanged,
 }) {
   const [ref, inView] = useInView({ threshold: 0.4 })
@@ -23,7 +23,7 @@ export default function Step({
     <li className={styles.step} ref={ref}>
       <h4>{name}</h4>
       <div className={styles.description}>{description}</div>
-      <img src={logos} alt={logosAlt} />
+      <LogoList className={styles.logoList} logos={logos} />
     </li>
   )
 }
--- website/components/logo-list/index.jsx
+++ website/components/logo-list/index.jsx
@@ -3,12 +3,14 @@ import classNames from 'classnames'
 
 export default function LogoList({ logos, className }) {
   return (
-    <ul className={classNames(styles.logoList, className)}>
-      {logos.map((logo) => (
-        <li key={logo.url}>
-          <img src={logo.url} alt={logo.alt} />
-        </li>
-      ))}
-    </ul>
+    <div className={classNames(styles.logoList, className)}>
+      <ul>
+        {logos.map((logo) => (
+          <li key={logo.url}>
+            <img src={logo.url} alt={logo.alt} />
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
