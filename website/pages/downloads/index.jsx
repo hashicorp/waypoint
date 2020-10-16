981aa1c6cb20ee8ce3ea6c4c13dc0c5b349fca65
Brandon Romano <BrandonRRomano@gmail.com>
2020-10-16 14:19:02 -0700

    Switch all releases to build time fetch (#559)
    * Switch all releases to build time fetch
    
    * Adds no-cache to the fetchAll request
    
    * Remove superfluous map

--- website/pages/downloads/index.jsx
+++ website/pages/downloads/index.jsx
@@ -2,18 +2,25 @@ import { useMemo, useState, useEffect } from 'react'
 import VERSION from 'data/version.js'
 import Head from 'next/head'
 import HashiHead from '@hashicorp/react-head'
-
 import { productName, productSlug } from 'data/metadata'
 import { packageManagersByOs, getStartedLinks } from 'data/downloads'
 import ReleaseInformation from 'components/downloader/release-information'
-import { sortPlatforms, detectOs } from 'components/downloader/utils/downloader'
+import {
+  sortPlatforms,
+  detectOs,
+  sortAndFilterReleases,
+} from 'components/downloader/utils/downloader'
 import DownloadCards from 'components/downloader/cards'
 import styles from './style.module.css'
 
-export default function DownloadsPage({ releaseData, previousVersions }) {
-  const sortedDownloads = useMemo(() => sortPlatforms(releaseData), [
-    releaseData,
-  ])
+export default function DownloadsPage({
+  currentVersionReleaseData,
+  allVersionsReleaseData,
+}) {
+  const sortedDownloads = useMemo(
+    () => sortPlatforms(currentVersionReleaseData),
+    [currentVersionReleaseData]
+  )
   const osKeys = Object.keys(sortedDownloads)
   const [osIndex, setSelectedOsIndex] = useState()
 
@@ -41,6 +48,7 @@ export default function DownloadsPage({ releaseData, previousVersions }) {
         logo={
           <img
             className={styles.logo}
+            alt="Waypoint"
             src={require('./img/waypoint-logo.svg')}
           />
         }
@@ -70,24 +78,51 @@ export default function DownloadsPage({ releaseData, previousVersions }) {
         brand="blue"
         productId="waypoint"
         productName={productName}
-        releases={previousVersions}
-        latestVersion={releaseData.version}
+        releases={allVersionsReleaseData}
+        latestVersion={currentVersionReleaseData.version}
       />
     </div>
   )
 }
 
-export async function getStaticProps() {
+function fetchVersionRelease(version) {
   return fetch(
-    `https://releases.hashicorp.com/waypoint/${VERSION}/index.json`,
+    `https://releases.hashicorp.com/waypoint/${version}/index.json`,
     {
       headers: {
         'Cache-Control': 'no-cache',
       },
     }
-  )
-    .then((r) => r.json())
-    .then((releaseData) => ({ props: { releaseData } }))
+  ).then((r) => r.json())
+}
+
+function fetchAllReleases() {
+  return fetch(`https://releases.hashicorp.com/waypoint/index.json`, {
+    headers: {
+      'Cache-Control': 'no-cache',
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      const latestReleases = sortAndFilterReleases(Object.keys(data.versions))
+      const releases = latestReleases.map((releaseVersion) => ({
+        ...sortPlatforms(data.versions[releaseVersion]),
+        version: releaseVersion,
+      }))
+      return releases
+    })
+}
+
+export async function getStaticProps() {
+  return Promise.all([fetchVersionRelease(VERSION), fetchAllReleases()])
+    .then((result) => {
+      return {
+        props: {
+          currentVersionReleaseData: result[0],
+          allVersionsReleaseData: result[1],
+        },
+      }
+    })
     .catch(() => {
       throw new Error(
         `--------------------------------------------------------
