8972cd409c90fe86aba2e250bc936df76c91c196
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2020-11-01 13:59:44 -0800

    internal/pkg/ctystructure: package to convert maps to cty objects

---
+++ internal/pkg/ctystructure/doc.go
@@ -0,0 +1,5 @@
+// Package crystructure helps convert map[string]interface{} values to cty.Values.
+//
+// This is useful for dynamically creating variables that may be available
+// to cty-powered environments such as HCL.
+package ctystructure
---
+++ internal/pkg/ctystructure/object_test.go
@@ -0,0 +1,65 @@
+package ctystructure
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/zclconf/go-cty/cty"
+)
+
+func TestObject(t *testing.T) {
+	cases := []struct {
+		Name   string
+		Input  map[string]interface{}
+		Output cty.Value
+	}{
+		{
+			"primitives",
+			map[string]interface{}{
+				"string": "hello!",
+			},
+			cty.ObjectVal(map[string]cty.Value{
+				"string": cty.StringVal("hello!"),
+			}),
+		},
+
+		{
+			"list of same types",
+			map[string]interface{}{
+				"ports": []interface{}{80, 100},
+			},
+			cty.ObjectVal(map[string]cty.Value{
+				"ports": cty.ListVal([]cty.Value{
+					cty.NumberIntVal(80),
+					cty.NumberIntVal(100),
+				}),
+			}),
+		},
+
+		{
+			"nested map",
+			map[string]interface{}{
+				"env": map[string]interface{}{
+					"key":  "value",
+					"port": 8080,
+				},
+			},
+			cty.ObjectVal(map[string]cty.Value{
+				"env": cty.ObjectVal(map[string]cty.Value{
+					"key":  cty.StringVal("value"),
+					"port": cty.NumberIntVal(8080),
+				}),
+			}),
+		},
+	}
+
+	for _, tt := range cases {
+		t.Run(tt.Name, func(t *testing.T) {
+			require := require.New(t)
+
+			val, err := Object(tt.Input)
+			require.NoError(err)
+			require.Equal(tt.Output, val)
+		})
+	}
+}
