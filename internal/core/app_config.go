c8df0e7efdc27b2177199422a2acff58b8ecb1fa
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2020-11-09 20:04:56 -0800

    internal/core: log variables that are synced

--- internal/core/app_config.go
+++ internal/core/app_config.go
@@ -2,6 +2,7 @@ package core
 
 import (
 	"context"
+	"fmt"
 
 	pb "github.com/hashicorp/waypoint/internal/server/gen"
 )
@@ -9,6 +10,7 @@ import (
 // ConfigSync writes all the app configuration in the waypoint.hcl file to
 // the server.
 func (a *App) ConfigSync(ctx context.Context) error {
+	a.logger.Debug("evaluating config vars for syncing")
 	vars, err := a.config.ConfigVars()
 	if err != nil {
 		return err
@@ -16,7 +18,17 @@ func (a *App) ConfigSync(ctx context.Context) error {
 
 	// If we have no vars then we don't want to round trip to the server.
 	if len(vars) == 0 {
+		a.logger.Debug("no file-based config vars, not syncing config")
 		return nil
+	}
+
+	a.logger.Info("syncing config variables", "len", len(vars))
+	if a.logger.IsDebug() {
+		for _, v := range vars {
+			a.logger.Debug("variable",
+				"name", v.Name,
+				"type", fmt.Sprintf("%T", v.Value))
+		}
 	}
 
 	_, err = a.client.SetConfig(ctx, &pb.ConfigSetRequest{
