ceba5c657442549e029a6d8ed9e83ccce9a650be
dizzyup <hector@me.com>
2020-11-16 19:02:48 +0000

    backport of commit c1da0f9be9095697987a182eb72b67acf114e035

--- website/components/footer/index.jsx
+++ website/components/footer/index.jsx
@@ -6,39 +6,49 @@ export default function Footer({ openConsentManager }) {
   const waypointServiceOK = useWaypointServiceStatus()
 
   return (
-    <footer className={s.root}>
-      <div className={s.container}>
-        <div className={s.left}>
-          <Link href="/docs">
-            <a>Docs</a>
-          </Link>
-          <Link href="/commands">
-            <a>CLI</a>
-          </Link>
-          <a href="https://learn.hashicorp.com/waypoint">Learn</a>
-          <a href="https://hashicorp.com/privacy">Privacy</a>
-          <Link href="/security">
-            <a>Security</a>
-          </Link>
-          <Link href="/terms">
-            <a>Terms of Use</a>
-          </Link>
-          <Link href="/copyright-policy">
-            <a>Copyright Policy</a>
-          </Link>
-          <a href="/files/press-kit.zip">Press Kit</a>
-          <a onClick={openConsentManager}>Consent Manager</a>
+    <>
+      <footer className={s.root}>
+        <div className={s.container}>
+          <div className={s.left}>
+            <Link href="/docs">
+              <a>Docs</a>
+            </Link>
+            <Link href="/commands">
+              <a>CLI</a>
+            </Link>
+            <a href="https://learn.hashicorp.com/waypoint">Learn</a>
+            <a href="https://hashicorp.com/privacy">Privacy</a>
+            <Link href="/security">
+              <a>Security</a>
+            </Link>
+            <Link href="/terms">
+              <a>Terms of Use</a>
+            </Link>
+            <Link href="/copyright-policy">
+              <a>Copyright Policy</a>
+            </Link>
+            <a href="/files/press-kit.zip">Press Kit</a>
+            <a onClick={openConsentManager}>Consent Manager</a>
+          </div>
+          {waypointServiceOK && (
+            <div className={s.status}>
+              <Link href="https://status.hashicorp.com">
+                <a className={s.normal}>All systems normal</a>
+              </Link>
+            </div>
+          )}
         </div>
-        <div className={s.status}>
-          <Link href="https://status.hashicorp.com">
-            {waypointServiceOK ? (
-              <a className={s.normal}>All systems normal</a>
-            ) : (
-              <a className={s.degraded}>Degraded URL service</a>
-            )}
-          </Link>
-        </div>
-      </div>
-    </footer>
+      </footer>
+      {!waypointServiceOK && (
+        <Link href="https://status.hashicorp.com">
+          <a className={s.statusBanner}>
+            <span>
+              The Waypoint URL service is currently experiencing an issue. View
+              information on <b>status.hashicorp.com</b>
+            </span>
+          </a>
+        </Link>
+      )}
+    </>
   )
 }
