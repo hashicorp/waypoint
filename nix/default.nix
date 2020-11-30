885a9d58ea19ed2f7cb30fe058152a29b8a88040
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2020-11-30 14:50:12 -0800

    nix: update to Go 1.15.5

--- nix/default.nix
+++ nix/default.nix
@@ -7,10 +7,10 @@ import sources.nixpkgs {
       # niv = (import sources.niv { pkgs = self; }).niv;
       go = super.go_1_15.overrideAttrs (
         old: rec {
-          version = "1.15.3";
+          version = "1.15.5";
           src = super.fetchurl {
             url = "https://dl.google.com/go/go${version}.src.tar.gz";
-            sha256 = "1228nv4vyzbqv768dl0bimsic47x9yyqld61qbgqqk75f0jn0sl9";
+            sha256 = "1wc43h3pmi92r6ypmh58vq13vm44rl1di09assz3xdwlry86n1y1";
           };
         }
       );
