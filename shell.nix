5ae85e2c42155ed7dc12f6fbdf6b4520cd31c40a
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2020-11-30 14:59:38 -0800

    nix: set ldflags for mockery to get proper version

--- shell.nix
+++ shell.nix
@@ -64,6 +64,11 @@ in with pkgs; let
       sha256 = "16yqhr92n5s0svk31yy3k42764fas694mnqqcny633yi0wqb876a";
     };
 
+    buildFlagsArray = ''
+      -ldflags=
+      -s -w -X github.com/vektra/mockery/mockery.SemVer=${version}
+    '';
+
     modSha256 = "0wyzfmhk7plazadbi26rzq3w9cmvqz2dd5jsl6kamw53ps5yh536";
     vendorSha256 = "0fai4hs3q822dg36a2zrxb191f71xdpafapn6ymi1w9dx68navcb";
 
