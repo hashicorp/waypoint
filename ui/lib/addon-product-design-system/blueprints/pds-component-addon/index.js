bcdf21a4dd6a9b1d68a10a9dbfc9c82582872cfc
Greg Hoin <gregwhat@gmail.com>
2020-12-02 14:34:20 +0100

    remove lib/pds

--- ui/lib/addon-product-design-system/blueprints/pds-component-addon/index.js
+++
@@ -1,1 +0,0 @@
-module.exports = require('../pds-component');
--- ui/lib/addon-product-design-system/tests/dummy/app/components/docs/field-name/index.js
+++
@@ -1,7 +0,0 @@
-import Component from '@glimmer/component';
-
-/**
- * @class DocsFieldName
- */
-
-export default class DocsFieldName extends Component {}
