14633a34666fe0d476f398020646b5f41467ab02
Greg Hoin <gregwhat@gmail.com>
2020-12-02 17:25:08 +0100

    query application version info

--- ui/app/routes/application.ts
+++ ui/app/routes/application.ts
@@ -3,17 +3,27 @@ import { inject as service } from '@ember/service';
 import SessionService from 'waypoint/services/session';
 import Transition from '@ember/routing/-private/transition';
 import { action } from '@ember/object';
+import ApiService from 'waypoint/services/api';
+import { Empty } from 'google-protobuf/google/protobuf/empty_pb';
+
 
 const ErrInvalidToken = 'invalid authentication token';
 
 export default class Application extends Route {
   @service session!: SessionService;
+  @service api!: ApiService;
 
   async beforeModel(transition: Transition) {
     await super.beforeModel(transition);
     if (!this.session.authConfigured && !transition.to.name.startsWith('auth')) {
       this.transitionTo('auth');
     }
+  }
+
+  async model() {
+    let resp = await this.api.client.getVersionInfo(new Empty(), this.api.WithMeta());
+    let versionInfo = resp?.getInfo();
+    return versionInfo?.toObject();
   }
 
   @action
