2ef2f01eb8503864e462f8ef56e178b299961f34
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2020-12-09 14:24:25 -0800

    wip

--- internal/serverinstall/serverinstall.go
+++ internal/serverinstall/serverinstall.go
@@ -13,8 +13,36 @@ import (
 // Installer is implemented by the server platforms and is responsible for managing
 // the installation of the Waypoint server.
 type Installer interface {
-	Install(ctx context.Context, ui terminal.UI, log hclog.Logger) (*clicontext.Config, *pb.ServerConfig_AdvertiseAddr, string, error)
+	// Install expects the Waypoint server to be installed.
+	Install(context.Context, *InstallOpts) (*InstallResults, error)
+
+	// InstallFlags is called prior to Install and allows the installer to
+	// specify flags for the install CLI. The flags should be prefixed with
+	// the platform name to avoid conflicts with other flags.
 	InstallFlags(*flag.Set)
+}
+
+// InstallOpts are the options sent to Installer.Install.
+type InstallOpts struct {
+	Log hclog.Logger
+	UI  terminal.UI
+}
+
+// InstallResults are the results expected for a successful Installer.Install.
+type InstallResults struct {
+	// Context is the connection context that can be used to connect from
+	// the CLI to the server. This will be used to establish an API client.
+	Context *clicontext.Config
+
+	// AdvertiseAddr is the configuration for the advertised address
+	// that entrypoints (deployed workloads) will use to communicate back
+	// to the server. This may be different from the context info because this
+	// may be a private address.
+	AdvertiseAddr *pb.ServerConfig_AdvertiseAddr
+
+	// HTTPAddr is the address to the HTTP listener on the server. This generally
+	// is reachable from the CLI immediately and not a private address.
+	HTTPAddr string
 }
 
 var Platforms = map[string]Installer{
