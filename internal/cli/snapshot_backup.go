46ec52017cc8c2690683452445d6913293a3c7fd
Rae Krantz <8461333+krantzinator@users.noreply.github.com>
2020-12-15 13:48:35 -0500

    remove superfluous test

--- internal/cli/snapshot_backup.go
+++ internal/cli/snapshot_backup.go
@@ -8,8 +8,8 @@ import (
 	"github.com/posener/complete"
 	sshterm "golang.org/x/crypto/ssh/terminal"
 
-	"github.com/hashicorp/waypoint/internal/pkg/flag"
 	"github.com/hashicorp/waypoint/internal/clisnapshot"
+	"github.com/hashicorp/waypoint/internal/pkg/flag"
 )
 
 type SnapshotBackupCommand struct {
