203e6b1f3d08e281f9773ffee659f1e45a6916c3
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2021-01-07 21:29:43 +0000

    backport of commit b46d3fa70f2782409f40d34465e1fecfaea53b70

--- builtin/azure/aci/platform.go
+++ builtin/azure/aci/platform.go
@@ -13,7 +13,6 @@ import (
 	"google.golang.org/grpc/status"
 
 	"github.com/hashicorp/waypoint-plugin-sdk/component"
-	"github.com/hashicorp/waypoint-plugin-sdk/datadir"
 	"github.com/hashicorp/waypoint-plugin-sdk/docs"
 	"github.com/hashicorp/waypoint-plugin-sdk/terminal"
 	"github.com/hashicorp/waypoint/builtin/docker"
@@ -55,7 +54,6 @@ func (p *Platform) Deploy(
 	log hclog.Logger,
 	src *component.Source,
 	img *docker.Image,
-	dir *datadir.Component,
 	deployConfig *component.DeploymentConfig,
 	ui terminal.UI,
 ) (*Deployment, error) {
