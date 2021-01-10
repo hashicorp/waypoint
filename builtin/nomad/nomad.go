176b46e0bb27c1292eb76ec73e38ab7a23fc409b
Michael Lange <dingoeatingfuzz@gmail.com>
2021-01-10 12:24:39 -0800

    Mappers from Files and Docker to NomadSpec

--- builtin/nomad/nomad.go
+++ builtin/nomad/nomad.go
@@ -1,6 +1,12 @@
 package nomad
 
-import "github.com/hashicorp/waypoint-plugin-sdk"
+import (
+	"strings"
+
+	"github.com/hashicorp/waypoint-plugin-sdk"
+	"github.com/hashicorp/waypoint/builtin/docker"
+	"github.com/hashicorp/waypoint/builtin/files"
+)
 
 //go:generate protoc -I ../../.. --go_opt=plugins=grpc --go_out=../../.. waypoint/builtin/nomad/plugin.proto
 
@@ -8,4 +14,23 @@ import "github.com/hashicorp/waypoint-plugin-sdk"
 // the Nomad plugin.
 var Options = []sdk.Option{
 	sdk.WithComponents(&Platform{}),
+	sdk.WithMappers(DockerMapper, FilesMapper),
+}
+
+func FilesMapper(src *files.Files) *NomadSpec {
+	parts := strings.Split(src.Path, "/")
+
+	return &NomadSpec{
+		Driver:   "exec",
+		Command:  parts[len(parts)-1],
+		Artifact: src.Path,
+	}
+}
+
+func DockerMapper(src *docker.Image) *NomadSpec {
+	return &NomadSpec{
+		Driver: "docker",
+		Image:  src.Image,
+		Tag:    src.Tag,
+	}
 }
