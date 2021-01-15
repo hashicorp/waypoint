a54d468e900624ec19a4b1d40f4798c5b6b6580c
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2021-01-15 09:54:17 -0800

    builtin/docker: support img for docker-pull

--- builtin/docker/pull/builder.go
+++ builtin/docker/pull/builder.go
@@ -6,6 +6,7 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"os"
+	"os/exec"
 
 	"github.com/docker/cli/cli/config"
 	"github.com/docker/distribution/reference"
@@ -13,6 +14,7 @@ import (
 	"github.com/docker/docker/client"
 	"github.com/docker/docker/pkg/jsonmessage"
 	"github.com/docker/docker/registry"
+	"github.com/golang/protobuf/ptypes/empty"
 	"github.com/hashicorp/go-hclog"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -64,6 +66,9 @@ can disable this with the "disable_entrypoint" configuration.
 If you wish to rename or retag an image, use this along with the
 "docker" registry option which will rename/retag the image and then
 push it to the specified registry.
+
+If Docker isn't available (the Docker daemon isn't running or a DOCKER_HOST
+isn't set), a daemonless solution will be used instead.
 `)
 
 	doc.Example(`
@@ -127,18 +132,19 @@ func (b *Builder) Build(
 	src *component.Source,
 	log hclog.Logger,
 ) (*wpdocker.Image, error) {
-	stdout, _, err := ui.OutputWriters()
-	if err != nil {
-		return nil, err
-	}
-
 	sg := ui.StepGroup()
+	defer sg.Wait()
 	step := sg.Add("Initializing Docker client...")
-	defer step.Abort()
+	defer func() {
+		if step != nil {
+			step.Abort()
+		}
+	}()
 
 	result := &wpdocker.Image{
-		Image: b.config.Image,
-		Tag:   b.config.Tag,
+		Image:    b.config.Image,
+		Tag:      b.config.Tag,
+		Location: &wpdocker.Image_Docker{Docker: &empty.Empty{}},
 	}
 
 	cli, err := wpdockerclient.NewClientWithOpts(client.FromEnv)
@@ -147,11 +153,97 @@ func (b *Builder) Build(
 	}
 	cli.NegotiateAPIVersion(ctx)
 
-	step.Done()
+	// We now test if Docker is actually functional. We do this here because we
+	// need all of the above to complete the actual build.
+	log.Debug("testing if we should use a Docker fallback")
+	useImg := false
+	if fallback, err := wpdockerclient.Fallback(ctx, log, cli); err != nil {
+		log.Warn("error during check if we should use Docker fallback", "err", err)
+		return nil, status.Errorf(codes.Internal,
+			"error validating Docker connection: %s", err)
+	} else if fallback && wpdocker.HasImg() {
+		// If we're falling back and have "img" available, use that. If we
+		// don't have "img" available, we continue to try to use Docker. We'll
+		// fail but that error message should help the user.
+		step.Update("Docker isn't available. Falling back to daemonless image pull...")
+		step.Done()
+		step = nil
+		if err := b.buildWithImg(
+			ctx, log, sg, result,
+		); err != nil {
+			return nil, err
+		}
 
+		// Our image is in the img registry now. We set this so that
+		// future users of this result type know where to look.
+		result.Location = &wpdocker.Image_Img{Img: &empty.Empty{}}
+
+		// We set this to true so we use the img-based injector later
+		useImg = true
+	} else {
+		// No fallback, build with Docker
+		step.Done()
+		step = nil
+		if err := b.buildWithDocker(
+			ctx, log, ui, sg, cli, result,
+		); err != nil {
+			return nil, err
+		}
+	}
+
+	if !b.config.DisableCEB {
+		step = sg.Add("Injecting Waypoint Entrypoint...")
+
+		asset, err := assets.Asset("ceb/ceb")
+		if err != nil {
+			return nil, status.Errorf(codes.Internal, "unable to restore custom entry point binary: %s", err)
+		}
+
+		assetInfo, err := assets.AssetInfo("ceb/ceb")
+		if err != nil {
+			return nil, status.Errorf(codes.Internal, "unable to restore custom entry point binary: %s", err)
+		}
+
+		callback := func(cur []string) (*epinject.NewEntrypoint, error) {
+			ep := &epinject.NewEntrypoint{
+				Entrypoint: append([]string{"/waypoint-entrypoint"}, cur...),
+				InjectFiles: map[string]epinject.InjectFile{
+					"/waypoint-entrypoint": {
+						Reader: bytes.NewReader(asset),
+						Info:   assetInfo,
+					},
+				},
+			}
+
+			return ep, nil
+		}
+
+		if !useImg {
+			_, err = epinject.AlterEntrypoint(ctx, result.Name(), callback)
+		} else {
+			_, err = epinject.AlterEntrypointImg(ctx, result.Name(), callback)
+		}
+		if err != nil {
+			return nil, status.Errorf(codes.Internal, "unable to set modify Docker entrypoint: %s", err)
+		}
+
+		step.Done()
+	}
+
+	return result, nil
+}
+
+func (b *Builder) buildWithDocker(
+	ctx context.Context,
+	log hclog.Logger,
+	ui terminal.UI,
+	sg terminal.StepGroup,
+	cli *client.Client,
+	result *wpdocker.Image,
+) error {
 	ref, err := reference.ParseNormalizedNamed(result.Name())
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, "unable to parse image name: %s", err)
+		return status.Errorf(codes.Internal, "unable to parse image name: %s", err)
 	}
 
 	encodedAuth := b.config.EncodedAuth
@@ -159,7 +251,7 @@ func (b *Builder) Build(
 		// Resolve the Repository name from fqn to RepositoryInfo
 		repoInfo, err := registry.ParseRepositoryInfo(ref)
 		if err != nil {
-			return nil, status.Errorf(codes.Internal, "unable to parse repository info from image name: %s", err)
+			return status.Errorf(codes.Internal, "unable to parse repository info from image name: %s", err)
 		}
 
 		var server string
@@ -185,20 +277,26 @@ func (b *Builder) Build(
 		authConfig, _ := cf.GetAuthConfig(server)
 		buf, err := json.Marshal(authConfig)
 		if err != nil {
-			return nil, status.Errorf(codes.Internal, "unable to generate authentication info for registry: %s", err)
+			return status.Errorf(codes.Internal, "unable to generate authentication info for registry: %s", err)
 		}
 		encodedAuth = base64.URLEncoding.EncodeToString(buf)
 	}
 
-	step = sg.Add("Pulling image...")
+	step := sg.Add("Pulling image...")
+	defer step.Abort()
 
 	resp, err := cli.ImagePull(ctx, reference.FamiliarString(ref), types.ImagePullOptions{
 		RegistryAuth: encodedAuth,
 	})
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, "error pulling image: %s", err)
+		return status.Errorf(codes.Internal, "error pulling image: %s", err)
 	}
 	defer resp.Close()
+
+	stdout, _, err := ui.OutputWriters()
+	if err != nil {
+		return err
+	}
 
 	var termFd uintptr
 	if f, ok := stdout.(*os.File); ok {
@@ -207,44 +305,51 @@ func (b *Builder) Build(
 
 	err = jsonmessage.DisplayJSONMessagesStream(resp, step.TermOutput(), termFd, true, nil)
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, "unable to stream pull logs to the terminal: %s", err)
+		return status.Errorf(codes.Internal, "unable to stream pull logs to the terminal: %s", err)
 	}
 
 	step.Done()
+	return nil
+}
 
-	if !b.config.DisableCEB {
-		step = sg.Add("Injecting Waypoint Entrypoint...")
-
-		asset, err := assets.Asset("ceb/ceb")
-		if err != nil {
-			return nil, status.Errorf(codes.Internal, "unable to restore custom entry point binary: %s", err)
+func (b *Builder) buildWithImg(
+	ctx context.Context,
+	log hclog.Logger,
+	sg terminal.StepGroup,
+	target *wpdocker.Image,
+) error {
+	var step terminal.Step
+	defer func() {
+		if step != nil {
+			step.Abort()
 		}
+	}()
 
-		assetInfo, err := assets.AssetInfo("ceb/ceb")
-		if err != nil {
-			return nil, status.Errorf(codes.Internal, "unable to restore custom entry point binary: %s", err)
-		}
-
-		_, err = epinject.AlterEntrypoint(ctx, result.Name(), func(cur []string) (*epinject.NewEntrypoint, error) {
-			ep := &epinject.NewEntrypoint{
-				Entrypoint: append([]string{"/waypoint-entrypoint"}, cur...),
-				InjectFiles: map[string]epinject.InjectFile{
-					"/waypoint-entrypoint": {
-						Reader: bytes.NewReader(asset),
-						Info:   assetInfo,
-					},
-				},
-			}
-
-			return ep, nil
-		})
-
-		if err != nil {
-			return nil, status.Errorf(codes.Internal, "unable to modify Docker entrypoint: %s", err)
-		}
-
-		step.Done()
+	step = sg.Add("Preparing Docker configuration...")
+	env := os.Environ()
+	if path, err := wpdocker.TempDockerConfig(log, target, b.config.EncodedAuth); err != nil {
+		return err
+	} else if path != "" {
+		defer os.RemoveAll(path)
+		env = append(env, "DOCKER_CONFIG="+path)
 	}
 
-	return result, nil
+	step.Done()
+	step = sg.Add("Pulling Docker image with img...")
+
+	// NOTE(mitchellh): we can probably use the img Go pkg directly one day.
+	cmd := exec.CommandContext(ctx,
+		"img",
+		"pull",
+		target.Name(),
+	)
+	cmd.Env = env
+	cmd.Stdout = step.TermOutput()
+	cmd.Stderr = cmd.Stdout
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+
+	step.Done()
+	return nil
 }
