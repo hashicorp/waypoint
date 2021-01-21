6d7204996f6b7b8ce74d0a513ee1d27dfb978c3c
Evan Phoenix <evan@phx.io>
2021-01-21 15:13:53 -0800

    internal/runner: Add exec operation that delegates to app.Exec

---
+++ internal/runner/operation_exec.go
@@ -0,0 +1,30 @@
+package runner
+
+import (
+	"context"
+
+	"github.com/hashicorp/waypoint/internal/core"
+	pb "github.com/hashicorp/waypoint/internal/server/gen"
+)
+
+func (r *Runner) executeExecOp(
+	ctx context.Context,
+	job *pb.Job,
+	project *core.Project,
+) (*pb.Job_Result, error) {
+	r.logger.Debug("exec operation starting")
+	app, err := project.App(job.Application.Application)
+	if err != nil {
+		return nil, err
+	}
+
+	op, ok := job.Operation.(*pb.Job_Exec)
+	if !ok {
+		// this shouldn't happen since the call to this function is gated
+		// on the above type match.
+		panic("operation not expected type")
+	}
+
+	err = app.Exec(ctx, op.Exec.InstanceId, op.Exec.Deployment)
+	return nil, err
+}
