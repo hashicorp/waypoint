23b922568e791d2ada36cdb49df9a71eaa7f9c4f
Brian Cain <bcain@hashicorp.com>
2021-01-26 16:05:27 -0800

    Add more comments to plan out shell runner

--- test-e2e/run-test.sh
+++ test-e2e/run-test.sh
@@ -1,21 +1,42 @@
 #!/bin/bash
 
-#TODO: make this a Makefile
+#TODO: make this a Makefile probably?
 
-GOOS="$(go env GOOS)"
-GOARCH="$(go env GOARCH)"
-GOEXE="$(go env GOEXE)"
-OUTDIR="build/${GOOS}_${GOARCH}"
+echo "Installing dependencies..."
+echo
+
+# install packages for building waypoint and running supported platforms:
+# - git, curl, (probably more)
+# - golang
+# - docker
+# - k8s (potentially external Digital Ocean service?)
+# - nomad (use the nomad dev mode scripts from waypoint-flightlist)
+
+# Build env vars
+export GOOS="$(go env GOOS)"
+export GOARCH="$(go env GOARCH)"
+export GOEXE="$(go env GOEXE)"
+export OUTDIR="build/${GOOS}_${GOARCH}"
+
+echo "Building waypoint binary..."
+echo
+
+# build waypoint OR download a package, add a switch for this
+#   - add param for installing a certain waypoint server
+
+# make tools
+# git submodule update for grpc status from api common
+# make
+
+echo "Pulling in waypoint-examples for test..."
+echo
+# Bring in test apps
+# clone waypoint-examples
 
 # Test env vars
 export WP_BINARY="waypoint"
 
 echo "Starting Waypoint end-to-end tests..."
 echo
-
-# install packages
-# clone waypoint-examples
-# build waypoint OR download a package, add a switch for this
-#   - add param for installing a certain waypoint server
 
 go test .
