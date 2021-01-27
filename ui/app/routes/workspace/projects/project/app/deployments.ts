9ca8ae391901d7d8f31afc6202a96cf988b5e215
Greg Hoin <gregwhat@gmail.com>
2021-01-27 19:15:31 +0100

    Restyle destroyed deployments toggle

--- ui/app/routes/workspace/projects/project/app/deployments.ts
+++ ui/app/routes/workspace/projects/project/app/deployments.ts
@@ -1,12 +1,8 @@
 import Route from '@ember/routing/route';
 import { AppRouteModel } from '../app';
 
-
-interface DeploymentsModelParams {
-  destroyed: boolean;
-}
 export default class Deployments extends Route {
-  async model(params: DeploymentsModelParams) {
+  async model() {
     let app = this.modelFor('workspace.projects.project.app') as AppRouteModel;
     return app.deployments;
   }
