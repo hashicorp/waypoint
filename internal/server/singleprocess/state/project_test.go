556df5e8d5c2eaaeee766ca79771cb76ee39088b
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2021-01-27 10:26:27 -0800

    internal/server/state: project put should not modify applications
    We document that project-related APIs cannot be used for applications,
    but previously we didn't preserve existing applications. If a user
    called UpsertProject to modify an existing project it'd blow away all
    their applications.
    
    This couldn't be hit in the real world unless the user was using the API
    directly, since `waypoint init -update` is the only thing that calls
    this API and it always registers applications again.
    
    But with future changes such as introducing server side waypoint.hcl and
    future UI changes to modify projects, this would've become a big issue.

--- internal/server/singleprocess/state/project_test.go
+++ internal/server/singleprocess/state/project_test.go
@@ -64,6 +64,51 @@ func TestProject(t *testing.T) {
 		}
 	})
 
+	t.Run("Put does not modify applications", func(t *testing.T) {
+		require := require.New(t)
+
+		const name = "AbCdE"
+		ref := &pb.Ref_Project{Project: name}
+
+		s := TestState(t)
+		defer s.Close()
+
+		// Set
+		proj := serverptypes.TestProject(t, &pb.Project{Name: name})
+		err := s.ProjectPut(proj)
+		require.NoError(err)
+		_, err = s.AppPut(serverptypes.TestApplication(t, &pb.Application{
+			Project: ref,
+		}))
+		require.NoError(err)
+
+		// Get exact
+		{
+			resp, err := s.ProjectGet(&pb.Ref_Project{
+				Project: "AbCdE",
+			})
+			require.NoError(err)
+			require.NotNil(resp)
+			require.False(resp.RemoteEnabled)
+			require.Len(resp.Applications, 1)
+		}
+
+		// Update the project
+		proj.RemoteEnabled = true
+		require.NoError(s.ProjectPut(proj))
+
+		// Get exact
+		{
+			resp, err := s.ProjectGet(&pb.Ref_Project{
+				Project: "AbCdE",
+			})
+			require.NoError(err)
+			require.NotNil(resp)
+			require.True(resp.RemoteEnabled)
+			require.Len(resp.Applications, 1)
+		}
+	})
+
 	t.Run("Delete", func(t *testing.T) {
 		require := require.New(t)
 
