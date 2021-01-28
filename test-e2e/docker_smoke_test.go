7dfa133416b7c4daa622dabb8cfe01301d734c58
Brian Cain <bcain@hashicorp.com>
2021-01-28 15:00:46 -0800

    Remove duplicate cli test

--- test-e2e/docker_smoke_test.go
+++ test-e2e/docker_smoke_test.go
@@ -10,23 +10,6 @@ var (
 	dockerTestDir = fmt.Sprintf("%s/docker/go", examplesRootDir)
 )
 
-func TestWaypointInstall(t *testing.T) {
-	t.Logf("Testing waypoint is available...")
-	wp := NewBinary(wpBinary, ".")
-	stdout, stderr, err := wp.Run("version")
-	if err != nil {
-		t.Errorf("unexpected error getting version: %s", err)
-	}
-
-	if stderr != "" {
-		t.Errorf("unexpected stderr output getting version: %s", err)
-	}
-
-	if !strings.Contains(stdout, "Waypoint v") {
-		t.Errorf("No version output detected:\n%s", stdout)
-	}
-}
-
 func TestWaypointDockerInstall(t *testing.T) {
 	wp := NewBinary(wpBinary, dockerTestDir)
 	stdout, stderr, err := wp.Run("install", "-platform=docker", "-accept-tos", fmt.Sprintf("-docker-server-image=%s", wpServerImage))
