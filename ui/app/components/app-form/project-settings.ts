21df2d1ae1a9579d66378e18c74e5d0cc904c353
Greg Hoin <gregwhat@gmail.com>
2021-02-08 15:36:33 +0100

    Add authentication methods

--- ui/app/components/app-form/project-settings.ts
+++ ui/app/components/app-form/project-settings.ts
@@ -9,9 +9,19 @@ class ProjectModel {
   name: string;
   applicationsList: [];
   dataSource: {
-    git: any,
+    git: {
+      url: string,
+      path: string,
+      ref: string,
+      basic: {
+        username: string,
+        password: string
+      },
+      ssh: {
+        privateKeyPem: string
+      }
+    },
     local:any,
-    url: any,
   };
   remoteEnabled: boolean;
 }
@@ -24,38 +34,43 @@ export default class AppFormProjectSettings extends Component<ProjectSettingsArg
   // normal class body definition here
   @service api!: ApiService;
   @tracked project: ProjectModel;
+  @tracked authCase: number;
 
   constructor(owner: any, args: any) {
     super(owner, args);
     let { project } = this.args;
-    this.project = project;
+    this.project = Object.assign(new ProjectModel(), project);
+    this.authCase = 4;
   }
 
   get dataSource(){
     return this.project.dataSource;
   }
 
-  get authCase() {
-    return this.project.dataSource?.git?.authCase;
+  get authSSH() {
+    return this.authCase === 5;
   }
 
-  set authCase(val) {
-    this.project.dataSource['git']['authCase'] = val
+  get authBasic() {
+    return this.authCase === 4;
   }
 
   get git() {
-    return this.dataSource?.git || {};
+    return this.dataSource?.git;
   }
 
-  set git(args) {
-    this.project.dataSource.git = args
+  set git(args: any) {
+    this.project.dataSource.git = args;
   }
 
   setGitData(prop: string, value: any) {
-    if (!this.dataSource.git) {
-      this.dataSource.git = {};
+    if (!this.dataSource) {
+      this.project.dataSource = {};
     }
-    this.dataSource.git[prop] = value;
+    if (!this.dataSource.git) {
+      this.project.dataSource.git = {};
+    }
+    this.project.dataSource.git[prop] = value;
   }
 
   @action
@@ -79,16 +94,56 @@ export default class AppFormProjectSettings extends Component<ProjectSettingsArg
   }
 
   @action
+  setGitSSH(e: any) {
+    if (!this.git.ssh) {
+      this.git.ssh = {};
+    }
+    this.git.ssh['privateKeyPem'] = e.target.value;
+  }
+
+  @action
+  setGitPassword(e: any) {
+    if (!this.git.basic) {
+      this.git.basic = {};
+    }
+    this.git.basic['password'] = e.target.value;
+  }
+
+  @action
+  setGitUsername(e: any) {
+    if (!this.git.basic) {
+      this.git.basic = {};
+    }
+    this.git.basic['username'] = e.target.value
+  }
+
+  @action
   async saveSettings() {
     let project = this.project;
     project.dataSource = this.dataSource;
     let ref = new Project();
     ref.setName(project.name);
     let dataSource = new Job.DataSource();
+    // Git settings
     let git = new Job.Git();
     git.setUrl(project.dataSource.git.url);
     git.setPath(project.dataSource.git.path);
     git.setRef(project.dataSource.git.ref);
+
+    // Git Authentication settings
+    if (this.authBasic) {
+      let gitBasic = new Job.Git.Basic();
+      gitBasic.setUsername(this.git.basic.username);
+      gitBasic.setPassword(this.git.basic.password);
+      git.setBasic(gitBasic);
+    }
+
+    if (this.authSSH) {
+      let gitSSH = new Job.Git.SSH();
+      gitSSH.setPrivateKeyPem(this.git.ssh.privateKeyPem);
+      git.setSsh(gitSSH);
+    }
+
     dataSource.setGit(git);
     ref.setDataSource(dataSource);
     ref.setRemoteEnabled(project.remoteEnabled);
