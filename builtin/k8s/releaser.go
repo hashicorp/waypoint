aa9f1046c49c29bc3bbf0bcae7b1e2f32fa7a8ba
Brian Cain <bcain@hashicorp.com>
2021-02-12 19:04:19 +0000

    backport of commit 9abbcadf6065aaec4e4cabe31d6433bcdc984c40

--- builtin/k8s/releaser.go
+++ builtin/k8s/releaser.go
@@ -313,7 +313,7 @@ func (r *Releaser) Documentation() (*docs.Documentation, error) {
 	doc.SetField(
 		"port",
 		"the TCP port that the application is listening on",
-		docs.Default("80"),
+		docs.Default(DefaultPort),
 	)
 
 	doc.SetField(
