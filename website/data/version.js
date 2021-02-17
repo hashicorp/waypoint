1f8f6bf4ca15edeadfa0362a2e2b9aa35557ccf4
Brian Cain <bcain@hashicorp.com>
2021-02-17 09:46:47 -0800

    Update website version for software download

--- website/data/version.js
+++ website/data/version.js
@@ -1,5 +1,5 @@
 // This is current version of the software available for download
-export default '0.2.1'
+export default '0.2.2'
 
 // HashiCorp officially supported package managers
 // NOTE: This information will be moving into the releases API
