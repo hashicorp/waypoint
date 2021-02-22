125fb6ff5a3ac2f6e9e7cc9db03f780d18980a73
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2021-02-22 11:15:39 -0800

    internal/appconfig: tests

---
+++ internal/appconfig/option_test.go
@@ -0,0 +1,23 @@
+package appconfig
+
+import (
+	"github.com/hashicorp/waypoint-plugin-sdk/component"
+	"github.com/hashicorp/waypoint/internal/plugin"
+)
+
+// testWithConfigSourcer can be used by tests to set a specific
+// ConfigSourcer implementation. It may make sense to export this
+// in the future but we don't have a need for it yet.
+func testWithConfigSourcer(n string, cs component.ConfigSourcer) Option {
+	return func(w *Watcher) error {
+		if w.plugins == nil {
+			w.plugins = map[string]*plugin.Instance{}
+		}
+
+		w.plugins[n] = &plugin.Instance{
+			Component: cs,
+		}
+
+		return nil
+	}
+}
