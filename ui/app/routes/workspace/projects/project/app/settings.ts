baa769d454ba7514860bb88d6c35f7545a28c02d
Greg Hoin <gregwhat@gmail.com>
2021-02-23 18:08:39 +0100

    Add link to app settings route

--- ui/app/routes/workspace/projects/project/app/settings.ts
+++ ui/app/routes/workspace/projects/project/app/settings.ts
@@ -1,7 +1,19 @@
 import Route from '@ember/routing/route';
 
-export default class WorkspaceProjectsProjectAppSettings extends Route.extend({
-  // anything which *must* be merged to prototype here
-}) {
-  // normal class body definition here
+export default class WorkspaceProjectsProjectAppSettings extends Route {
+  breadcrumbs(model: AppRouteModel) {
+    if (!model) return [];
+    return [
+      {
+        label: model.application.application,
+        icon: 'git-repository',
+        args: ['workspace.projects.project.app'],
+      },
+      {
+        label: 'Settings',
+        icon: 'settings',
+        args: ['workspace.projects.project.app.settings'],
+      },
+    ];
+  }
 }
