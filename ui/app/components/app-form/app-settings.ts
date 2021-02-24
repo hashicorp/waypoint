d05f162be8b1d01ae64526c3510d83f2548d0c8d
Greg Hoin <gregwhat@gmail.com>
2021-02-24 19:26:53 +0100

    Enabled/disabled app git settings

--- ui/app/components/app-form/app-settings.ts
+++ ui/app/components/app-form/app-settings.ts
@@ -1,14 +1,27 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
-import {Project, Application } from 'waypoint-pb';
-
-
+import { Project, Application } from 'waypoint-pb';
+import { tracked } from '@glimmer/tracking';
 interface AppFormAppSettingsArgs {
-  app: Application,
-  project: Project
+  app: Application;
+  project: Project;
 }
 
 export default class AppFormAppSettings extends Component<AppFormAppSettingsArgs> {
+  @tracked project;
+  @tracked app;
+
+  constructor(owner: any, args: any) {
+    super(owner, args);
+    let { project, app } = this.args;
+    this.project = project;
+    this.app = app;
+  }
+  // disabled git settings if project-level git config is not set up
+  get gitDisabled() {
+    return !(this.project.dataSource?.git?.basic || this.project.dataSource?.git?.ssh)
+  }
+
   @action
   saveSettings() {
 
