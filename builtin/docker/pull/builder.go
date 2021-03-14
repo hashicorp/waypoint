8e55f0bc2a6afb21f8ea2f5845bca61af5ef35c8
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2021-03-14 11:58:42 -0700

    builtin/docker/pull: update docs

--- builtin/docker/pull/builder.go
+++ builtin/docker/pull/builder.go
@@ -58,7 +58,7 @@ func (b *Builder) Documentation() (*docs.Documentation, error) {
 	}
 
 	doc.Description(`
-Use an existing, pre-built Docker image
+Use an existing, pre-built Docker image.
 
 This builder will automatically inject the Waypoint entrypoint. You
 can disable this with the "disable_entrypoint" configuration.
@@ -69,6 +69,11 @@ push it to the specified registry.
 
 If Docker isn't available (the Docker daemon isn't running or a DOCKER_HOST
 isn't set), a daemonless solution will be used instead.
+
+If "disable_entrypoint" is set to true and the Waypoint configuration
+has no registry, this builder will not physically pull the image. This enables
+Waypoint to work in environments where the image is built outside of Waypoint
+(such as in a CI pipeline).
 `)
 
 	doc.Example(`
