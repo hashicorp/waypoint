6ae03f2a9fe0f28de677796fb12ea616b19e5f80
Greg Hoin <gregwhat@gmail.com>
2021-03-18 20:45:52 +0100

    Remove system notifications teardown for now

--- ui/app/services/flash-messages.ts
+++ ui/app/services/flash-messages.ts
@@ -1,10 +1,3 @@
 import FlashMessages from 'ember-cli-flash/services/flash-messages';
 
-export default class PdsFlashMessages extends FlashMessages {
-  remove(id) {
-    let message = this.queue.findBy('id', id);
-    if (message) {
-      message._teardown();
-    }
-  }
-}
+export default class PdsFlashMessages extends FlashMessages {}
