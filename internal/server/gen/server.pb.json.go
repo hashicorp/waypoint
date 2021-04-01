8e546d0d763e11f69a8e1fe6986dbba262a1f4fa
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2021-03-31 19:55:02 -0700

    internal/server: proto

--- internal/server/gen/server.pb.json.go
+++ internal/server/gen/server.pb.json.go
@@ -478,6 +478,24 @@ func (msg *OperationOrder) UnmarshalJSON(b []byte) error {
 }
 
 // MarshalJSON implements json.Marshaler
+func (msg *Generation) MarshalJSON() ([]byte, error) {
+	var buf bytes.Buffer
+	err := (&jsonpb.Marshaler{
+		EnumsAsInts:  false,
+		EmitDefaults: false,
+		OrigName:     false,
+	}).Marshal(&buf, msg)
+	return buf.Bytes(), err
+}
+
+// UnmarshalJSON implements json.Unmarshaler
+func (msg *Generation) UnmarshalJSON(b []byte) error {
+	return (&jsonpb.Unmarshaler{
+		AllowUnknownFields: false,
+	}).Unmarshal(bytes.NewReader(b), msg)
+}
+
+// MarshalJSON implements json.Marshaler
 func (msg *QueueJobRequest) MarshalJSON() ([]byte, error) {
 	var buf bytes.Buffer
 	err := (&jsonpb.Marshaler{
