debf645a9936f215e66c7c7ced465ccca437b0a9
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2021-04-19 14:03:28 -0700

    internal/config: jsonnetdir tests

--- internal/config/funcs/jsonnet_test.go
+++ internal/config/funcs/jsonnet_test.go
@@ -2,6 +2,7 @@ package funcs
 
 import (
 	"io/ioutil"
+	"os"
 	"path/filepath"
 	"testing"
 
@@ -104,6 +105,73 @@ func TestJsonnetFile(t *testing.T) {
 				goldie.WithNameSuffix(outSuffix),
 			)
 			g.Assert(t, filepath.Base(tt.Path.AsString()), data)
+		})
+	}
+}
+
+func TestJsonnetDir(t *testing.T) {
+	tests := []struct {
+		Path cty.Value
+		Opts cty.Value
+		Err  string
+	}{
+		{
+			cty.StringVal("testdata/jsonnet/dir"),
+			cty.ObjectVal(map[string]cty.Value{
+				"ext_vars": cty.MapVal(map[string]cty.Value{
+					"prefix": cty.StringVal("Happy Hour "),
+				}),
+
+				"ext_code": cty.MapVal(map[string]cty.Value{
+					"brunch": cty.StringVal("true"),
+				}),
+			}),
+			``,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.Path.AsString(), func(t *testing.T) {
+			require := require.New(t)
+
+			abs, err := filepath.Abs(tt.Path.AsString())
+			require.NoError(err)
+			tt.Path = cty.StringVal(abs)
+
+			result, err := JsonnetDirFunc.Call([]cty.Value{
+				tt.Path,
+				tt.Opts,
+			})
+			if tt.Err != "" {
+				require.Error(err)
+				require.Contains(err.Error(), tt.Err)
+				return
+			}
+			require.NoError(err)
+
+			// Go through each file and validate that we have a match.
+			path := result.AsString()
+			require.NoError(filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
+				if err != nil {
+					return err
+				}
+
+				if info.IsDir() {
+					return nil
+				}
+
+				data, err := ioutil.ReadFile(path)
+				require.NoError(err)
+
+				const outSuffix = ".out"
+				g := goldie.New(t,
+					goldie.WithFixtureDir(tt.Path.AsString()),
+					goldie.WithNameSuffix(outSuffix),
+				)
+				g.Assert(t, filepath.Base(path), data)
+
+				return nil
+			}))
 		})
 	}
 }
