b550e5cee58c3cf363dc5abe00c77b42a9a92a5a
hashicorp-ci <noreply@hashicorp.com>
2021-04-20 18:44:42 +0000

    Release v0.3.1

--- website/data/version.js
+++ website/data/version.js
@@ -1,5 +1,5 @@
 // This is current version of the software available for download
-export default '0.3.0'
+export default '0.3.1'
 
 // HashiCorp officially supported package managers
 // NOTE: This information will be moving into the releases API
