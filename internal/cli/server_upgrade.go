040c0124576a2be4e68eeb5150d248d815079fd6
Evan Phoenix <evan@phx.io>
2021-05-12 00:27:02 +0000

    backport of commit be15c89e9dd23913eea42184700203b9ab6489b6

--- internal/cli/server_upgrade.go
+++ internal/cli/server_upgrade.go
@@ -235,7 +235,8 @@ func (c *ServerUpgradeCommand) Run(args []string) int {
 	httpAddr := result.HTTPAddr
 
 	// We update the context config if the server addr has changed between upgrades
-	if originalCfg.Server.Address != contextConfig.Server.Address {
+	if originalCfg.Server.Address != contextConfig.Server.Address ||
+		originalCfg.Server.Platform != c.platform {
 		// Update the platform here, basically to upgrade an older context that didn't
 		// have platform set.
 		originalCfg.Server.Platform = c.platform
