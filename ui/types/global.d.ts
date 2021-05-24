5b7e85316bb75ae8872f3e8961ffc4a572e7bf35
Jamie White <jamie@jgwhite.co.uk>
2021-05-24 21:17:36 +0200

    Add TestContext.server to type declarations

--- ui/types/global.d.ts
+++ ui/types/global.d.ts
@@ -2,6 +2,12 @@ declare module 'ember-cli-mirage/test-support' {
   export function setupMirage(hooks: NestedHooks): void;
 }
 
+declare module 'ember-test-helpers' {
+  interface TestContext {
+    server: any;
+  }
+}
+
 declare module 'ember-a11y-testing/test-support/audit' {
   export default function a11yAudit(target?: string | Element, axeOptions?: object): Promise<void>;
 }
