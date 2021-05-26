784618ba6ca00503203139ce35f5bbc92d6e1a1d
Jamie White <jamie@jgwhite.co.uk>
2021-05-26 15:54:46 +0200

    ui/tests/a11y: exclude .empty-state (for now)

--- ui/tests/helpers/a11y.ts
+++ ui/tests/helpers/a11y.ts
@@ -12,7 +12,7 @@ type OptionsWithContext = RunOptions & ContextObject;
 // Selectors of elements to exclude from a11y auditing. See the following docs
 // for more:
 // https://github.com/dequelabs/axe-core/blob/develop/doc/API.md#include-exclude-object
-const exclude = [['.pds-logomark'], ['.pds-tabNav'], ['.card-header']];
+const exclude = [['.pds-logomark'], ['.pds-tabNav'], ['.card-header'], ['.empty-state']];
 
 export function setup(): void {
   setupGlobalA11yHooks(() => true);
