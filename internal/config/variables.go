15827296602f0529167df9c78e2a74e90d6a14b9
Rae Krantz <8461333+krantzinator@users.noreply.github.com>
2021-05-26 22:45:51 -0400

    more vars funcs for eventual refactoring

--- internal/config/variables.go
+++ internal/config/variables.go
@@ -2,11 +2,14 @@ package config
 
 import (
 	"fmt"
+	"os"
+	"strings"
 
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/ext/typeexpr"
 	"github.com/hashicorp/hcl/v2/gohcl"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
+	pb "github.com/hashicorp/waypoint/internal/server/gen"
 	"github.com/zclconf/go-cty/cty"
 	"github.com/zclconf/go-cty/cty/convert"
 )
@@ -23,7 +26,7 @@ type VariableAssignment struct {
 	Expr   hcl.Expression
 	// The location of the variable value if the value was provided
 	// from a file
-	Range hcl.Range
+	Range  hcl.Range
 }
 
 // TODO krantzinator: this goes somewhere else
@@ -184,4 +187,234 @@ func (variables *Variables) decodeVariableBlock(block *hcl.Block) hcl.Diagnostic
 	(*variables)[name] = v
 
 	return diags
+}
+
+// Prefix your environment variables with VarEnvPrefix so that Waypoint can see
+// them.
+const VarEnvPrefix = "WP_VAR_"
+
+// Collect values from flags, local var files, and env vars
+func CollectInputVars(vars map[string]string, files []string) ([]*pb.Variable, hcl.Diagnostics) {
+	var diags hcl.Diagnostics
+	ret := []*pb.Variable{}
+
+	// First we'll deal with environment variables, since they have the lowest
+	// precedence.
+	{
+		env := os.Environ()
+		for _, raw := range env {
+			if !strings.HasPrefix(raw, VarEnvPrefix) {
+				continue
+			}
+			raw = raw[len(VarEnvPrefix):] // trim the prefix
+
+			eq := strings.Index(raw, "=")
+			if eq == -1 {
+				// Seems invalid, so we'll ignore it.
+				continue
+			}
+
+			name := raw[:eq]
+			rawVal := raw[eq+1:]
+
+			ret = append(ret, &pb.Variable{
+				Name:   name,
+				Value:  &pb.Variable_Str{Str: rawVal},
+				Source: &pb.Variable_Env{},
+			})
+		}
+	}
+
+	// TODO var file parsing
+
+	// Then we process values given explicitly on the command line, either
+	// as individual literal settings or as files to read.
+	// We'll first check for a value already set and overwrite if there, or
+	// append a new value
+	for name, val := range vars {
+		ret = append(ret, &pb.Variable{
+			Name:   name,
+			Value:  &pb.Variable_Str{Str: val},
+			Source: &pb.Variable_Cli{},
+		})
+	}
+
+	return ret, diags
+}
+
+// Collect values from server and remote-stored wpvars file
+func (variables *Variables) CollectInputValRemote(files []*hcl.File, serverVars []*pb.Variable) hcl.Diagnostics {
+	var diags hcl.Diagnostics
+
+	// files will contain files found in the remote git source
+	for _, file := range files {
+		// Before we do our real decode, we'll probe to see if there are any
+		// blocks of type "variable" in this body, since it's a common mistake
+		// for new users to put variable declarations in wpvars rather than
+		// variable value definitions.
+		{
+			content, _, _ := file.Body.PartialContent(&hcl.BodySchema{
+				Blocks: []hcl.BlockHeaderSchema{
+					{
+						Type:       "variable",
+						LabelNames: []string{"name"},
+					},
+				},
+			})
+			for _, block := range content.Blocks {
+				name := block.Labels[0]
+				diags = append(diags, &hcl.Diagnostic{
+					Severity: hcl.DiagError,
+					Summary:  "Variable declaration in a .wpvars file",
+					Detail: fmt.Sprintf("A .wpvars file is used to assign "+
+						"values to variables that have already been declared "+
+						"in the waypoint.hcl, not to declare new variables. To "+
+						"declare variable %q, place this block in your "+
+						"waypoint.hcl file.\n\nTo set a value for this variable "+
+						"in %s, use the definition syntax instead:\n    %s = <value>",
+						name, block.TypeRange.Filename, name),
+					Subject: &block.TypeRange,
+				})
+			}
+			if diags.HasErrors() {
+				// If we already found problems then JustAttributes below will find
+				// the same problems with less-helpful messages, so we'll bail for
+				// now to let the user focus on the immediate problem.
+				return diags
+			}
+		}
+
+		attrs, moreDiags := file.Body.JustAttributes()
+		diags = append(diags, moreDiags...)
+
+		for name, attr := range attrs {
+			variable, found := (*variables)[name]
+			if !found {
+				sev := hcl.DiagWarning
+				// TODO krantzinator
+				// if cfg.ValidationOptions.Strict {
+				// 	sev = hcl.DiagError
+				// }
+				diags = append(diags, &hcl.Diagnostic{
+					Severity: sev,
+					Summary:  "Undefined variable",
+					Detail: fmt.Sprintf("A %q variable was set but was "+
+						"not found in known variables. To declare "+
+						"variable %q, place this block in your "+
+						"waypoint.hcl file.",
+						name, name),
+					Context: attr.Range.Ptr(),
+				})
+				continue
+			}
+
+			val, moreDiags := attr.Expr.Value(nil)
+			diags = append(diags, moreDiags...)
+
+			if variable.Type != cty.NilType {
+				var err error
+				val, err = convert.Convert(val, variable.Type)
+				if err != nil {
+					diags = append(diags, &hcl.Diagnostic{
+						Severity: hcl.DiagError,
+						Summary:  "Invalid value for variable",
+						Detail:   fmt.Sprintf("The value for %s is not compatible with the variable's type constraint: %s.", name, err),
+						Subject:  attr.Expr.Range().Ptr(),
+					})
+					val = cty.DynamicVal
+				}
+			}
+
+			variable.Values = append(variable.Values, VariableAssignment{
+				Source: "repofile",
+				Value:  val,
+				Expr:   attr.Expr,
+			})
+		}
+	}
+
+	// Finally we process values given explicitly on the command line.
+	for _, sv := range serverVars {
+		variable, found := (*variables)[sv.Name]
+		if !found {
+			diags = append(diags, &hcl.Diagnostic{
+				Severity: hcl.DiagError,
+				Summary:  "Undefined server variable",
+				Detail: fmt.Sprintf("A %q variable was set in the UI "+
+					"and stored on the server, but was not found in "+
+					"known variables. To declare variable %q, place "+
+					"this block in your waypoint.hcl file.",
+					sv.Name, sv.Name),
+			})
+			continue
+		}
+
+		var expr hclsyntax.Expression
+		switch sv.Value.(type) {
+
+		case *pb.Variable_Hcl:
+			value := sv.Value.(*pb.Variable_Hcl).Hcl
+			fakeFilename := fmt.Sprintf("<value for var.%s from server>", sv.Name)
+			expr, diags = hclsyntax.ParseExpression([]byte(value), fakeFilename, hcl.Pos{Line: 1, Column: 1})
+
+		case *pb.Variable_Str:
+			value := sv.Value.(*pb.Variable_Str).Str
+			expr = &hclsyntax.LiteralValueExpr{Val: cty.StringVal(value)}
+		}
+
+		val, valDiags := expr.Value(nil)
+		diags = append(diags, valDiags...)
+		if valDiags.HasErrors() {
+			return diags
+		}
+
+		if variable.Type != cty.NilType {
+			var err error
+			val, err = convert.Convert(val, variable.Type)
+			if err != nil {
+				diags = append(diags, &hcl.Diagnostic{
+					Severity: hcl.DiagError,
+					Summary:  "Invalid argument value for server-stored variable",
+					Detail:   fmt.Sprintf("The received arg value for %s is not compatible with the variable's type constraint: %s.", sv.Name, err),
+					Subject:  expr.Range().Ptr(),
+				})
+				val = cty.DynamicVal
+			}
+		}
+
+		variable.Values = append(variable.Values, VariableAssignment{
+			Source: "server",
+			Value:  val,
+			Expr:   expr,
+		})
+	}
+
+	return diags
+}
+
+func (variables *Variables) SortPrecedence() ([]*pb.Variable, error) {
+	var ret []*pb.Variable
+
+	for _, v := range *variables {
+		for _, vv := range v.Values {
+			switch st := vv.Source; st {
+			case "default":
+				pbv := &pb.Variable{
+					Name:   v.Name,
+					Value:  &pb.Variable_Str{Str: vv.Value.AsString()},
+					Source: nil,
+				}
+				ret = append(ret, pbv)
+			case "server":
+				pbv := &pb.Variable{
+					Name:   v.Name,
+					Value:  &pb.Variable_Str{Str: vv.Value.AsString()},
+					Source: &pb.Variable_Server{},
+				}
+				ret = append(ret, pbv)
+			}
+		}
+	}
+
+	return ret, nil
 }
--- internal/config/variables_test.go
+++ internal/config/variables_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/hashicorp/hcl/v2/gohcl"
+	pb "github.com/hashicorp/waypoint/internal/server/gen"
 	"github.com/stretchr/testify/require"
 )
 
@@ -98,6 +99,84 @@ func TestVariables_decode(t *testing.T) {
 
 			require.True(diag.HasErrors())
 			require.Contains(diag.Error(), tt.Err)
+		})
+	}
+}
+
+func TestVariables_collectValues(t *testing.T) {
+	cases := []struct {
+		File     string
+		Values   []*pb.Variable
+		Expected map[string][]string
+		Err      string
+	}{
+		{
+			"valid.hcl",
+			[]*pb.Variable{
+				{
+					Name:  "bees",
+					Value: &pb.Variable_Str{Str: "notbuzz"},
+				},
+			},
+			map[string][]string{"bees": {"notbuzz", "buzz"}, "dinosaur": {"longneck"}},
+			"",
+		},
+	}
+	for _, tt := range cases {
+		t.Run(tt.File, func(t *testing.T) {
+			require := require.New(t)
+
+			cfg, err := Load(filepath.Join("testdata", "variables", tt.File), &LoadOptions{
+				Workspace: "default",
+			})
+			require.NoError(err)
+
+			var vs Variables
+			diags := cfg.DecodeVariableBlocks(&vs)
+			require.False(diags.HasErrors())
+
+			// collect values
+			diags = vs.CollectInputValRemote(nil, tt.Values)
+			require.False(diags.HasErrors())
+
+			// check that default and set values are all in the
+			// created []VariableAssignments
+			for k, vs := range tt.Expected {
+				require.Equal(vs, tt.Expected[k])
+			}
+		})
+	}
+}
+
+func TestVariables_collectInputVars(t *testing.T) {
+	cases := []struct {
+		Name     string
+		File     []string
+		Values   map[string]string
+		Expected []*pb.Variable
+		Err      string
+	}{
+		{
+			"success",
+			[]string{""},
+			map[string]string{"foo": "bar"},
+			[]*pb.Variable{
+				{
+					Name:   "foo",
+					Value:  &pb.Variable_Str{Str: "bar"},
+					Source: &pb.Variable_Cli{},
+				},
+			},
+			"",
+		},
+	}
+	for _, tt := range cases {
+		t.Run(tt.Name, func(t *testing.T) {
+			require := require.New(t)
+			vars, diags := CollectInputVars(tt.Values, tt.File)
+			require.False(diags.HasErrors())
+
+			require.Equal(vars, tt.Expected)
 		})
 	}
 }
