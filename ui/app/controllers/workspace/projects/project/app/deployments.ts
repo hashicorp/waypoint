f1c1f3a26d3cd89cc859a22caba70ef6dd041f5b
Jamie White <jamie@jgwhite.co.uk>
2021-05-27 17:23:31 +0200

    ui/deployments: auto-fix controller linting issues

--- ui/app/controllers/workspace/projects/project/app/deployments.ts
+++ ui/app/controllers/workspace/projects/project/app/deployments.ts
@@ -3,25 +3,49 @@ import { tracked } from '@glimmer/tracking';
 import { Deployment } from 'waypoint-pb';
 import { action } from '@ember/object';
 export default class WorkspaceProjectsProjectAppDeployments extends Controller {
-  queryParams = ['destroyed']
+  queryParams = ['destroyed'];
 
   @tracked destroyed = false;
 
-  get hasMoreDeployments() {
-    return this.model.filter((deployment: Deployment.AsObject) => deployment.state == 4).length > 0
+  get hasMoreDeployments(): boolean {
+    return this.model.filter((deployment: Deployment.AsObject) => deployment.state == 4).length > 0;
   }
 
-  get deployments() {
+  get deployments(): Deployment.AsObject[] {
     if (this.destroyed) {
-      return this.model
+      return this.model;
     } else {
-      const deploys = this.model.filter((deployment: Deployment.AsObject) => deployment.state != 4)
+      let deploys = this.model.filter((deployment: Deployment.AsObject) => deployment.state != 4);
       return deploys;
     }
   }
 
+  get deploymentsByGeneration(): GenerationGroup[] {
+    let result: GenerationGroup[] = [];
+
+    for (let deployment of this.deployments) {
+      let id = deployment.generation?.id ?? deployment.id;
+      let group = result.find((group) => group.generationID === id);
+
+      if (!group) {
+        group = new GenerationGroup(id);
+        result.push(group);
+      }
+
+      group.deployments.push(deployment);
+    }
+
+    return result;
+  }
+
   @action
-  showDestroyed() {
+  showDestroyed(): void {
     this.destroyed = true;
   }
+}
+
+class GenerationGroup {
+  deployments: Deployment.AsObject[] = [];
+
+  constructor(public generationID: string) {}
 }
