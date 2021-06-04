c1aec54046241befdf89cd480638ca792dbaf691
Rae Krantz <8461333+krantzinator@users.noreply.github.com>
2021-06-04 13:04:34 +0000

    backport of commit 65d14fd449ede396a672f4f432edfc9708c40215

--- internal/cli/deployment_list.go
+++ internal/cli/deployment_list.go
@@ -116,7 +116,7 @@ func (c *DeploymentListCommand) Run(args []string) int {
 			Application: app.Ref(),
 			Workspace:   wsRef,
 		})
-		if status.Code(err) == codes.NotFound {
+		if status.Code(err) == codes.NotFound || status.Code(err) == codes.Unimplemented {
 			err = nil
 			statusReportResp = nil
 		}
