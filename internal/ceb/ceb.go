20ed08faf8daef464855f46b304409eb9e0a77f0
Izaak Lauer <8404559+izaaklauer@users.noreply.github.com>
2021-06-23 11:14:33 -0400

    Changing util to env per Brian's suggestion

--- internal/ceb/ceb.go
+++ internal/ceb/ceb.go
@@ -16,11 +16,11 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
+	"github.com/hashicorp/waypoint/internal/env"
 	"github.com/hashicorp/waypoint/internal/pkg/gatedwriter"
 	"github.com/hashicorp/waypoint/internal/plugin"
 	"github.com/hashicorp/waypoint/internal/server"
 	pb "github.com/hashicorp/waypoint/internal/server/gen"
-	"github.com/hashicorp/waypoint/internal/util"
 	"github.com/hashicorp/waypoint/internal/version"
 )
 
@@ -300,11 +300,11 @@ func WithEnvDefaults() Option {
 
 		cfg.URLServicePort = port
 		cfg.ServerAddr = os.Getenv(envServerAddr)
-		cfg.ServerRequired = util.GetEnvBool(envCEBServerRequired, false)
-		cfg.ServerTls = util.GetEnvBool(envServerTls, false)
-		cfg.ServerTlsSkipVerify = util.GetEnvBool(envServerTlsSkipVerify, false)
+		cfg.ServerRequired = env.GetEnvBool(envCEBServerRequired, false)
+		cfg.ServerTls = env.GetEnvBool(envServerTls, false)
+		cfg.ServerTlsSkipVerify = env.GetEnvBool(envServerTlsSkipVerify, false)
 		cfg.InviteToken = os.Getenv(envCEBToken)
-		cfg.disable = util.GetEnvBool(envCEBDisable, false)
+		cfg.disable = env.GetEnvBool(envCEBDisable, false)
 
 		ceb.deploymentId = os.Getenv(envDeploymentId)
 
--- internal/env/env.go
+++ internal/env/env.go
@@ -1,13 +1,13 @@
-package util
+package env
 
 import (
 	"os"
 	"strconv"
 )
 
-// Extracts a boolean from an env var. Falls back to the default if
-// The key is unset or not a valid boolean.
-func GetEnvBool(key string, defaultValue bool) (value bool) {
+// GetEnvBool Extracts a boolean from an env var. Falls back to the default
+// if the key is unset or not a valid boolean.
+func GetEnvBool(key string, defaultValue bool) bool {
 	envVal := os.Getenv(key)
 	if envVal == "" {
 		return defaultValue
