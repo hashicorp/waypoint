1b10251a8d05714209e9b1ccf4322f442ce5abdf
Greg Hoin <ghoin@hashicorp.com>
2021-06-25 22:39:40 +0200

    WIP: Add log-terminal component and first pass at making it work

--- ui/app/routes/workspace/projects/project/app/exec.ts
+++ ui/app/routes/workspace/projects/project/app/exec.ts
@@ -1,11 +1,19 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 import ApiService from 'waypoint/services/api';
+import { ExecStreamRequest } from 'waypoint-pb';
 
 export default class Exec extends Route {
   @service api!: ApiService;
 
   async model() {
+    let application = this.modelFor('workspace.projects.project.app');
+    let latestDeploymentId = application.releases[0]?.deploymentId;
+    let req = new ExecStreamRequest();
+    let start = new ExecStreamRequest.Start();
+    debugger;
+    start.setDeploymentId(latestDeploymentId);
+    // this.api.client.
     // todo(pearkes): construct GetExecStreamRequest
   }
 }
--- ui/ember-cli-build.js
+++ ui/ember-cli-build.js
@@ -60,5 +60,8 @@ module.exports = function (defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+
+  app.import('node_modules/xterm/css/xterm.css');
+
   return app.toTree();
 };
