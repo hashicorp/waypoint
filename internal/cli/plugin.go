084065ec9ba0f6b31d3cac13cd14c9d403c0e58d
Izaak Lauer <8404559+izaaklauer@users.noreply.github.com>
2021-06-24 21:20:38 -0400

    Adding a debug flag instead of env var

--- internal/cli/plugin.go
+++ internal/cli/plugin.go
@@ -4,33 +4,57 @@ import (
 	"context"
 	"fmt"
 	"github.com/hashicorp/waypoint-plugin-sdk"
+	"github.com/hashicorp/waypoint/internal/pkg/flag"
 	"github.com/hashicorp/waypoint/internal/plugin"
-	"os"
 )
 
 type PluginCommand struct {
 	*baseCommand
+	debugMode bool
 }
 
 func (c *PluginCommand) Run(args []string) int {
-	plugin, ok := plugin.Builtins[args[0]]
+	flags := c.Flags()
+
+	// Initialize. If we fail, we just exit since Init handles the UI.
+	if err := c.Init(
+		WithArgs(args),
+		WithFlags(flags),
+	); err != nil {
+		return 1
+	}
+	args = flags.Args()
+
+	pluginName := args[0]
+
+	plugin, ok := plugin.Builtins[pluginName]
 	if !ok {
-		panic("no such plugin: " + args[0])
+		panic("no such plugin: " + pluginName)
 	}
 
-	debug := os.Getenv("WAYPOINT_PLUGIN_DEBUG") != ""
-
 	// Run the plugin
-	if !debug {
+	if !c.debugMode {
 		sdk.Main(plugin...)
 	} else {
-		err := sdk.Debug(context.Background(), "pack", plugin...)
+		err := sdk.Debug(context.Background(), pluginName, plugin...)
 		if err != nil {
-			panic(fmt.Sprintf("Failed to launch plugin in debug mode: %v", err))
+			panic(fmt.Sprintf("Failed to launch plugin in debug mode: %s", err))
 		}
 	}
 
 	return 0
+}
+
+func (c *PluginCommand) Flags() *flag.Sets {
+	return c.flagSet(0, func(set *flag.Sets) {
+		f := set.NewSet("Command Options")
+		f.BoolVar(&flag.BoolVar{
+			Name:    "debug",
+			Default: false,
+			Target:  &c.debugMode,
+			Usage:   "set to true to run the plugin with support for debuggers like delve",
+		})
+	})
 }
 
 func (c *PluginCommand) Synopsis() string {
