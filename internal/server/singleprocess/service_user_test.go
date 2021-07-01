7ffe2ee906afe49447254025e24647cce08be6d7
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2021-07-01 08:56:10 -0700

    internal/server: don't copy locks

--- internal/server/singleprocess/service_user_test.go
+++ internal/server/singleprocess/service_user_test.go
@@ -19,15 +19,18 @@ func TestServiceUpdateUser(t *testing.T) {
 	client := server.TestServer(t, impl)
 
 	// Get ourself
-	userResp, err := client.GetUser(ctx, &pb.GetUserRequest{})
-	require.NoError(t, err)
-	user := userResp.User
+	getUser := func(t *testing.T) *pb.User {
+		userResp, err := client.GetUser(ctx, &pb.GetUserRequest{})
+		require.NoError(t, err)
+		return userResp.User
+	}
 
 	// Simplify writing tests
 	type Req = pb.UpdateUserRequest
 
 	t.Run("update but do nothing", func(t *testing.T) {
 		require := require.New(t)
+		user := getUser(t)
 
 		resp, err := client.UpdateUser(ctx, &Req{User: user})
 		require.NoError(err)
@@ -46,20 +49,20 @@ func TestServiceUpdateUser(t *testing.T) {
 	t.Run("update with an invalid ID", func(t *testing.T) {
 		require := require.New(t)
 
-		userCopy := *user
-		userCopy.Id = "NOPE"
+		user := getUser(t)
+		user.Id = "NOPE"
 
-		_, err := client.UpdateUser(ctx, &Req{User: &userCopy})
+		_, err := client.UpdateUser(ctx, &Req{User: user})
 		require.Error(err)
 	})
 
 	t.Run("change username", func(t *testing.T) {
 		require := require.New(t)
 
-		userCopy := *user
-		userCopy.Username = "tubes"
+		user := getUser(t)
+		user.Username = "tubes"
 
-		resp, err := client.UpdateUser(ctx, &Req{User: &userCopy})
+		resp, err := client.UpdateUser(ctx, &Req{User: user})
 		require.NoError(err)
 		require.NotNil(resp)
 		require.Equal(user.Id, resp.User.Id)
