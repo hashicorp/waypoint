84b8c8ec6f395006e7b1008a53ac5ac036dc9d65
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2021-06-30 17:37:44 -0700

    internal/server: GetUser

---
+++ internal/server/singleprocess/service_user.go
@@ -0,0 +1,28 @@
+package singleprocess
+
+import (
+	"context"
+
+	pb "github.com/hashicorp/waypoint/internal/server/gen"
+)
+
+func (s *service) GetUser(
+	ctx context.Context,
+	req *pb.GetUserRequest,
+) (*pb.GetUserResponse, error) {
+	// Currently logged in user by default
+	user := userFromContext(ctx)
+
+	// If we have a request, get that user
+	if req.User != nil {
+		var err error
+		user, err = s.state.UserGet(req.User)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return &pb.GetUserResponse{
+		User: user,
+	}, nil
+}
