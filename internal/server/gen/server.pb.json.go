1bf1ab64a3fefdf86ee4bb7a359f814bfe630e3d
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2021-07-07 11:14:08 -0700

    proto: Ref.AuthMethod

--- internal/server/gen/server.pb.json.go
+++ internal/server/gen/server.pb.json.go
@@ -550,6 +550,24 @@ func (msg *Ref_UserUsername) UnmarshalJSON(b []byte) error {
 }
 
 // MarshalJSON implements json.Marshaler
+func (msg *Ref_AuthMethod) MarshalJSON() ([]byte, error) {
+	var buf bytes.Buffer
+	err := (&jsonpb.Marshaler{
+		EnumsAsInts:  false,
+		EmitDefaults: false,
+		OrigName:     false,
+	}).Marshal(&buf, msg)
+	return buf.Bytes(), err
+}
+
+// UnmarshalJSON implements json.Unmarshaler
+func (msg *Ref_AuthMethod) UnmarshalJSON(b []byte) error {
+	return (&jsonpb.Unmarshaler{
+		AllowUnknownFields: false,
+	}).Unmarshal(bytes.NewReader(b), msg)
+}
+
+// MarshalJSON implements json.Marshaler
 func (msg *Component) MarshalJSON() ([]byte, error) {
 	var buf bytes.Buffer
 	err := (&jsonpb.Marshaler{
