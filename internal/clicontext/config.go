319ffad31e5bf12ce19026ab8b6bd7301f991bfb
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2021-07-12 13:29:45 -0700

    internal/clicontext: parse a string into a context

--- internal/clicontext/config.go
+++ internal/clicontext/config.go
@@ -2,6 +2,7 @@ package clicontext
 
 import (
 	"io"
+	"net/url"
 
 	"github.com/hashicorp/hcl/v2/gohcl"
 	"github.com/hashicorp/hcl/v2/hclsimple"
@@ -22,6 +23,46 @@ func LoadPath(path string) (*Config, error) {
 	var cfg Config
 	err := hclsimple.DecodeFile(path, nil, &cfg)
 	return &cfg, err
+}
+
+// FromURL parses a URL to a Waypoint server and populates as much of the
+// context configuration as possible. This makes a number of assumptions:
+//
+//   - assumes TLS
+//   - assumes TLS skip verify
+//   - assumes requires auth
+//
+// The skip verify bit is a bad default but it is the most common UX
+// getting started and this URL is most commonly used with `waypoint login`
+// so we want to provide the smoothest experience there at the expense
+// of a slight risk.
+func (c *Config) FromURL(v string) error {
+	u, err := url.Parse(v)
+	if err != nil {
+		return err
+	}
+
+	// Set our defaults
+	c.Server.Tls = true
+	c.Server.TlsSkipVerify = true
+	c.Server.RequireAuth = true
+
+	// Setting the address as the default allows this to work for
+	// urls like "foo.com:1234" which url.Parse doesn't handle well at all.
+	// We then only override the address if we're sure we got a better value.
+	c.Server.Address = v
+
+	// Override
+	if u.Host != "" {
+		c.Server.Address = u.Host
+	}
+
+	// Specifically http will override TLS
+	if u.Scheme == "http" {
+		c.Server.Tls = false
+	}
+
+	return nil
 }
 
 // WriteTo implements io.WriterTo and encodes this config as HCL.
