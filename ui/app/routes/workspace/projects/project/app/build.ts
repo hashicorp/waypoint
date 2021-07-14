3bad5273bdab358bd4efe1b6c4f3e5965bec3016
Sabrina Ko <sabrina@hashicorp.com>
2021-07-14 20:36:15 +0000

    backport of commit 156b7f6e84b8a3c1a4c8a2d11e8269e04ca12fd9

--- ui/app/routes/workspace/projects/project/app/build.ts
+++ ui/app/routes/workspace/projects/project/app/build.ts
@@ -29,11 +29,13 @@ export default class BuildDetail extends Route {
   async model(params: BuildModelParams) {
     // Setup the build request
     let { builds } = this.modelFor('workspace.projects.project.app');
-    let { id: build_id } = builds.find((obj) => obj.sequence == params.sequence);
+    let { id: build_id } = builds.find((obj) => obj.sequence === Number(params.sequence));
+
     let ref = new Ref.Operation();
     ref.setId(build_id);
     let req = new GetBuildRequest();
     req.setRef(ref);
+
     let build = await this.api.client.getBuild(req, this.api.WithMeta());
     return build.toObject();
   }
