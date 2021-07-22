0475b30d8fdb5a6831eabeb5959c7ee80d7466c5
Jamie White <jamie@jgwhite.co.uk>
2021-07-22 09:38:50 +0000

    backport of commit 3f2f8586c5ceb00b8b4904d9d559a3da9f87ee70

--- ui/config/targets.js
+++ ui/config/targets.js
@@ -2,9 +2,6 @@
 
 const browsers = ['last 1 Chrome versions', 'last 1 Firefox versions', 'last 1 Safari versions'];
 
-const isCI = Boolean(process.env.CI);
-const isProduction = process.env.EMBER_ENV === 'production';
-
 module.exports = {
   browsers,
 };
