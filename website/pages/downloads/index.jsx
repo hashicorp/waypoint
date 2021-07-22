83de99c2ad011af29327a91212d768a660aab4ff
Bryce Kalow <bkalow@hashicorp.com>
2021-07-22 21:26:42 +0000

    backport of commit d4cd28da85a1f6d082e1bf45c5df3252c77be6c2

--- website/pages/downloads/index.jsx
+++ website/pages/downloads/index.jsx
@@ -1,14 +1,15 @@
 import VERSION, { packageManagers } from 'data/version.js'
 import HashiHead from '@hashicorp/react-head'
 import { productName, productSlug } from 'data/metadata'
-import ProductDownloader from '@hashicorp/react-product-downloader'
+import ProductDownloadsPage from '@hashicorp/react-product-downloads-page'
+import { generateStaticProps } from '@hashicorp/react-product-downloads-page/server'
 import styles from './style.module.css'
 
 export default function DownloadsPage({ releases }) {
   return (
     <span className={styles.downloads}>
       <HashiHead title={`Downloads | ${productName} by HashiCorp`} />
-      <ProductDownloader
+      <ProductDownloadsPage
         releases={releases}
         packageManagers={packageManagers}
         productName={productName}
@@ -52,29 +53,8 @@ export default function DownloadsPage({ releases }) {
   )
 }
 
-export async function getStaticProps() {
-  return fetch(`https://releases.hashicorp.com/waypoint/index.json`, {
-    headers: {
-      'Cache-Control': 'no-cache',
-    },
+export const getStaticProps = () =>
+  generateStaticProps({
+    product: 'waypoint',
+    latestVersion: VERSION,
   })
-    .then((res) => res.json())
-    .then((result) => {
-      return {
-        props: {
-          releases: result,
-        },
-      }
-    })
-    .catch(() => {
-      throw new Error(
-        `--------------------------------------------------------
-        Unable to resolve version ${VERSION} on releases.hashicorp.com from link
-        <https://releases.hashicorp.com/${productSlug}/${VERSION}/index.json>. Usually this
-        means that the specified version has not yet been released. The downloads page
-        version can only be updated after the new version has been released, to ensure
-        that it works for all users.
-        ----------------------------------------------------------`
-      )
-    })
-}
