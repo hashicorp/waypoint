495b0eefe3b51e13f295ca90505ccfc156b1f60e
Brian Cain <bcain@hashicorp.com>
2021-07-22 08:33:13 -0700

    Add test for turning off app polling through project upsert

--- internal/server/singleprocess/poll_test.go
+++ internal/server/singleprocess/poll_test.go
@@ -582,3 +582,124 @@ func TestApplicationPollHandler(t *testing.T) {
 	require.NotNil(nextPollTime)
 	require.True(nextPollTime.After(pollTime))
 }
+
+func TestApplicationPollHandler_turnoff(t *testing.T) {
+	require := require.New(t)
+	ctx := context.Background()
+
+	// Create our server
+	impl, err := New(WithDB(testDB(t)))
+	require.NoError(err)
+	client := server.TestServer(t, impl)
+
+	// Create a project with an application
+	respProj, err := client.UpsertProject(ctx, &pb.UpsertProjectRequest{
+		Project: serverptypes.TestProject(t, &pb.Project{
+			Name: "Example",
+			DataSource: &pb.Job_DataSource{
+				Source: &pb.Job_DataSource_Local{
+					Local: &pb.Job_Local{},
+				},
+			},
+			DataSourcePoll: &pb.Project_Poll{
+				Enabled:  true,
+				Interval: "15ms",
+			},
+			StatusReportPoll: &pb.Project_AppPoll{
+				Enabled:  false,
+				Interval: "15ms",
+			},
+			Applications: []*pb.Application{
+				{
+					Project: &pb.Ref_Project{Project: "Example"},
+					Name:    "apple-app",
+				},
+			},
+		}),
+	})
+	require.NoError(err)
+	project := respProj.Project
+
+	// Grab next poll time
+	state := testServiceImpl(impl).state
+	a, _, err := state.ApplicationPollPeek(nil)
+	require.NoError(err)
+	require.Nil(a) // Apps Next Poll should be 0 since not started yet
+
+	// Wait a bit. The interval is so low that this should trigger
+	// multiple loops through the poller. But we want to ensure we
+	// have only one poll job queued.
+	time.Sleep(50 * time.Millisecond)
+
+	// Do a deployment
+	resp, err := client.UpsertDeployment(ctx, &pb.UpsertDeploymentRequest{
+		Deployment: serverptypes.TestValidDeployment(t, &pb.Deployment{
+			Component: &pb.Component{
+				Name: "testapp",
+			},
+			Application: &pb.Ref_Application{
+				Application: "apple-app",
+				Project:     "Example",
+			},
+		}),
+	})
+	require.NoError(err)
+	require.NotNil(resp)
+
+	// Update the app to start polling
+	project.StatusReportPoll = &pb.Project_AppPoll{
+		Enabled: true,
+	}
+	_, err = client.UpsertProject(ctx, &pb.UpsertProjectRequest{
+		Project: project,
+	})
+	require.NoError(err)
+
+	// App poll time should be set
+	a, pollTime, err := state.ApplicationPollPeek(nil)
+	require.NoError(err)
+	require.NotNil(pollTime)
+	require.NotNil(a) // Apps Next Poll should be set
+
+	// Wait a bit. The interval is so low that this should trigger
+	// multiple loops through the poller. But we want to ensure we
+	// have only one poll job queued.
+	time.Sleep(50 * time.Millisecond)
+
+	// Check for our condition, we do eventually here because if we're
+	// in a slow environment then this may still be empty.
+	require.Eventually(func() bool {
+		// We should have a single poll job
+		var jobs []*pb.Job
+		raw, err := testServiceImpl(impl).state.JobList()
+		for _, j := range raw {
+			if j.State != pb.Job_ERROR {
+				jobs = append(jobs, j)
+			}
+		}
+
+		if err != nil {
+			t.Logf("err: %s", err)
+			return false
+		}
+
+		return len(jobs) == 1
+	}, 5*time.Second, 50*time.Millisecond)
+
+	// Update the app to stop polling
+	project.StatusReportPoll = &pb.Project_AppPoll{
+		Enabled: false,
+	}
+	_, err = client.UpsertProject(ctx, &pb.UpsertProjectRequest{
+		Project: project,
+	})
+	require.NoError(err)
+
+	// App poll time should be unset now
+	a, _, err = state.ApplicationPollPeek(nil)
+	require.NoError(err)
+	require.Nil(a) // Apps Next Poll should be 0 since turned off
+
+	// Cancel our poller to ensure it stops
+	testServiceImpl(impl).Close()
+}
