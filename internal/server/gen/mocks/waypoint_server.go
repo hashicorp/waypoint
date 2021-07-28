e265f2b60e9270525438aed6ab8bd20a735b23bf
Brian Cain <bcain@hashicorp.com>
2021-07-28 11:52:53 -0700

    proto gen

--- internal/server/gen/mocks/waypoint_server.go
+++ internal/server/gen/mocks/waypoint_server.go
@@ -279,6 +279,29 @@ func (_m *WaypointServer) EntrypointLogStream(_a0 gen.Waypoint_EntrypointLogStre
 	return r0
 }
 
+// ExpediteStatusReport provides a mock function with given fields: _a0, _a1
+func (_m *WaypointServer) ExpediteStatusReport(_a0 context.Context, _a1 *gen.ExpediteStatusReportRequest) (*gen.ExpediteStatusReportResponse, error) {
+	ret := _m.Called(_a0, _a1)
+
+	var r0 *gen.ExpediteStatusReportResponse
+	if rf, ok := ret.Get(0).(func(context.Context, *gen.ExpediteStatusReportRequest) *gen.ExpediteStatusReportResponse); ok {
+		r0 = rf(_a0, _a1)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*gen.ExpediteStatusReportResponse)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(context.Context, *gen.ExpediteStatusReportRequest) error); ok {
+		r1 = rf(_a0, _a1)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // GenerateInviteToken provides a mock function with given fields: _a0, _a1
 func (_m *WaypointServer) GenerateInviteToken(_a0 context.Context, _a1 *gen.InviteTokenRequest) (*gen.NewTokenResponse, error) {
 	ret := _m.Called(_a0, _a1)
