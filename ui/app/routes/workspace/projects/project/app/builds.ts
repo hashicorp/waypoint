6c3ddd557036f4e5e116ed4f1edc271e85163efe
Jamie White <jamie@jgwhite.co.uk>
2021-07-29 12:30:59 +0200

    ui: de-lint `routes/.../app/builds.ts`

--- ui/app/routes/workspace/projects/project/app/builds.ts
+++ ui/app/routes/workspace/projects/project/app/builds.ts
@@ -1,8 +1,10 @@
 import Route from '@ember/routing/route';
 import { Model as AppRouteModel } from '../app';
 
+type Model = AppRouteModel['builds'];
+
 export default class Builds extends Route {
-  async model() {
+  async model(): Promise<Model> {
     let app = this.modelFor('workspace.projects.project.app') as AppRouteModel;
     return app.builds;
   }
