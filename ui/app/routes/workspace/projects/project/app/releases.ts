50bc1adcba578fd348213bfa33b7158650894c16
Jamie White <jamie@jgwhite.co.uk>
2021-07-29 10:24:31 +0000

    backport of commit 9ab7c91ff80b72fedd8f591b39ed986690c4b6ab

--- ui/app/routes/workspace/projects/project/app/releases.ts
+++ ui/app/routes/workspace/projects/project/app/releases.ts
@@ -1,8 +1,10 @@
 import Route from '@ember/routing/route';
 import { Model as AppRouteModel } from '../app';
 
+type Model = AppRouteModel['releases'];
+
 export default class Releases extends Route {
-  async model() {
+  async model(): Promise<Model> {
     let app = this.modelFor('workspace.projects.project.app') as AppRouteModel;
     return app.releases;
   }
