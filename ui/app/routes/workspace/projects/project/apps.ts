c0b408b30e38aba033e60b77f82afc344ff7a5b7
Jamie White <jamie@jgwhite.co.uk>
2021-07-29 12:21:23 +0200

    ui: de-lint `routes/.../project/apps.ts`
    Note we remove the `afterModel` hook. If a project contained only one
    app then this hook would redirect directly to said app, skipping the
    list.  However, it looks like the code had fallen out of step with the
    model hook (as had the type annotations) so the predicate would never
    match and the redirect would never be performed.
    
    Seeing as the behavior has been inoperative for a while, I chose to
    remove it entirely rather than reinstate it.

--- ui/app/routes/workspace/projects/project/apps.ts
+++ ui/app/routes/workspace/projects/project/apps.ts
@@ -1,20 +1,13 @@
 import Route from '@ember/routing/route';
-import { inject as service } from '@ember/service';
-import ApiService from 'waypoint/services/api';
-import { Project, Application } from 'waypoint-pb';
+import { Model as ProjectRouteModel } from 'waypoint/routes/workspace/projects/project';
+
+type Model = ProjectRouteModel;
 
 export default class Apps extends Route {
-  @service api!: ApiService;
-
-  async model() {
-    let proj = this.modelFor('workspace.projects.project') as Project.AsObject;
-    return proj;
-  }
-
-  afterModel(model: Application.AsObject[]) {
-    if (model.length == 1) {
-      return this.transitionTo('workspace.projects.project.app', model[0].name);
-    }
-    return;
+  async model(): Promise<Model> {
+    // Technically we get this behavior for free because, in Ember, if a
+    // route doesn’t define a model hook then it automatically receives
+    // the model of its parent. Still, it doesn’t hurt to be explicit.
+    return this.modelFor('workspace.projects.project') as ProjectRouteModel;
   }
 }
