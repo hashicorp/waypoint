ac46ca591d96b8bc67b97194d507957142d4357a
Greg Hoin <ghoin@hashicorp.com>
2021-08-04 19:54:52 +0200

    Fetch Project Config Variables

--- ui/app/routes/workspace/projects/project/settings/config-variables.ts
+++ ui/app/routes/workspace/projects/project/settings/config-variables.ts
@@ -1,7 +1,22 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+import ApiService from 'waypoint/services/api';
+import { ConfigGetRequest, Ref } from 'waypoint-pb';
 
-export default class WorkspaceProjectsProjectSettingsConfigVariables extends Route.extend({
-  // anything which *must* be merged to prototype here
-}) {
-  // normal class body definition here
+interface ProjectModelParams {
+  project_id: string;
+}
+export default class WorkspaceProjectsProjectSettingsConfigVariables extends Route {
+  @service api!: ApiService;
+
+  async model() {
+    let ref = new Ref.Project();
+    let params = this.paramsFor('workspace.projects.project') as ProjectModelParams;
+    ref.setProject(params.project_id);
+    let req = new ConfigGetRequest();
+    req.setProject(ref);
+
+    let config = await this.api.client.getConfig(req, this.api.WithMeta());
+    return config?.toObject();
+  }
 }
