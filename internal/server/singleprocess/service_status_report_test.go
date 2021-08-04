63bdf1e4a6080bb5f789c0e76b07a0c8f658a157
Brian Cain <bcain@hashicorp.com>
2021-08-04 14:40:24 -0700

    internal/server: Introduce target field for filtering status reports
    This commit introduces a new request option, Target, which will filter
    the kinds of status reports returned by the ListStatusReports endpoint.
    Previously this endpoint would return all status reports for both
    deployments and releases. This commit changes that by allowing the
    caller to specify if they just want deployment or release status
    reports. Not specifying a target will return both.

--- internal/server/singleprocess/service_status_report_test.go
+++ internal/server/singleprocess/service_status_report_test.go
@@ -120,8 +120,62 @@ func TestServiceStatusReport_ListStatusReports(t *testing.T) {
 	require.NoError(t, err)
 	client := server.TestServer(t, impl)
 
+	// Create a project with an application
+	respProj, err := client.UpsertProject(ctx, &pb.UpsertProjectRequest{
+		Project: serverptypes.TestProject(t, &pb.Project{
+			Name: "Example",
+			DataSource: &pb.Job_DataSource{
+				Source: &pb.Job_DataSource_Local{
+					Local: &pb.Job_Local{},
+				},
+			},
+			Applications: []*pb.Application{
+				{
+					Project: &pb.Ref_Project{Project: "Example"},
+					Name:    "apple-app",
+				},
+			},
+		}),
+	})
+	require.NoError(t, err)
+	require.NotNil(t, respProj)
+
+	deployResp, err := client.UpsertDeployment(ctx, &pb.UpsertDeploymentRequest{
+		Deployment: serverptypes.TestValidDeployment(t, &pb.Deployment{
+			Component: &pb.Component{
+				Name: "testapp",
+			},
+			Application: &pb.Ref_Application{
+				Application: "apple-app",
+				Project:     "Example",
+			},
+		}),
+	})
+	require.NoError(t, err)
+	require.NotNil(t, deployResp)
+
 	resp, err := client.UpsertStatusReport(ctx, &pb.UpsertStatusReportRequest{
-		StatusReport: serverptypes.TestValidStatusReport(t, nil),
+		StatusReport: serverptypes.TestValidStatusReport(t, &pb.StatusReport{
+			TargetId: &pb.StatusReport_DeploymentId{
+				DeploymentId: deployResp.Deployment.Id,
+			},
+		}),
+	})
+	require.NoError(t, err)
+
+	releaseResp, err := client.UpsertRelease(ctx, &pb.UpsertReleaseRequest{
+		Release: serverptypes.TestValidRelease(t, &pb.Release{
+			DeploymentId: deployResp.Deployment.Id,
+		}),
+	})
+	require.NoError(t, err)
+
+	_, err = client.UpsertStatusReport(ctx, &pb.UpsertStatusReportRequest{
+		StatusReport: serverptypes.TestValidStatusReport(t, &pb.StatusReport{
+			TargetId: &pb.StatusReport_ReleaseId{
+				ReleaseId: releaseResp.Release.Id,
+			},
+		}),
 	})
 	require.NoError(t, err)
 
@@ -137,6 +191,27 @@ func TestServiceStatusReport_ListStatusReports(t *testing.T) {
 		require.NoError(err)
 		require.NotEmpty(sr)
 		require.Equal(sr.StatusReports[0].Id, resp.StatusReport.Id)
+	})
+
+	t.Run("list only deployment reports", func(t *testing.T) {
+		require := require.New(t)
+
+		// Get, should return a status report
+		sr, err := client.ListStatusReports(ctx, &Req{
+			Application: resp.StatusReport.Application,
+			Target: &pb.ListStatusReportsRequest_Deployment{
+				Deployment: &pb.Ref_Operation{
+					Target: &pb.Ref_Operation_Id{
+						Id: deployResp.Deployment.Id,
+					},
+				},
+			},
+		})
+		require.NoError(err)
+		require.NotEmpty(sr)
+		require.Equal(len(sr.StatusReports), 1)
+		require.Equal(sr.StatusReports[0].Id, resp.StatusReport.Id)
+		require.Equal(sr.StatusReports[0].TargetId.(*pb.StatusReport_DeploymentId).DeploymentId, deployResp.Deployment.Id)
 	})
 }
 
