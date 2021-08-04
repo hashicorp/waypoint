ac6a62fe41413fa7ca6f776b6564bea9d1b57130
Izaak Lauer <8404559+izaaklauer@users.noreply.github.com>
2021-08-04 16:47:25 -0400

    Converting plugin -> server declared resources with mapstructure
    A field name changed, and doing it this way makes it easier to stay in sync in the future.

--- internal/core/app_deploy.go
+++ internal/core/app_deploy.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"strings"
 
+	"github.com/mitchellh/mapstructure"
+
 	"github.com/golang/protobuf/proto"
 	"github.com/golang/protobuf/ptypes/any"
 	"github.com/google/uuid"
@@ -339,15 +341,12 @@ func (op *deployOperation) Do(ctx context.Context, log hclog.Logger, app *App, m
 
 	// Convert from the plugin declaredResources to server declaredResources. Should be identical.
 	declaredResources := make([]*pb.DeclaredResource, len(declaredResourcesResp.DeclaredResources))
-	for i, resource := range declaredResourcesResp.DeclaredResources {
-		declaredResources[i] = &pb.DeclaredResource{
-			Id:                  resource.Id,
-			Name:                resource.Name,
-			Platform:            resource.Platform,
-			State:               resource.State,
-			StateJson:           resource.StateJson,
-			CategoryDisplayHint: pb.ResourceCategoryDisplayHint(resource.CategoryDisplayHint.Number()),
+	for i, pluginDeclaredResource := range declaredResourcesResp.DeclaredResources {
+		var serverDeclaredResource pb.DeclaredResource
+		if err := mapstructure.Decode(pluginDeclaredResource, &serverDeclaredResource); err != nil {
+			return nil, status.Errorf(codes.Internal, "failed to decode plugin declared resource named %q: %s", pluginDeclaredResource.Name, err)
 		}
+		declaredResources[i] = &serverDeclaredResource
 	}
 
 	deploy.DeclaredResources = declaredResources
