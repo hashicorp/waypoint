0026bf81fca3f6e4ddc268e1c121a91ca8c00604
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-08-05 13:26:59 -0400

    website: replace g-container with g-grid-container

--- website/components/longform-page/index.jsx
+++ website/components/longform-page/index.jsx
@@ -4,7 +4,7 @@ import styles from './LongformPage.module.css'
 export default function LongformPage({ className, title, alert, children }) {
   return (
     <div className={classNames(styles.longformPage, className)}>
-      <div className="g-container">
+      <div className="g-grid-container">
         <div className={styles.longformWrapper}>
           {alert && <div className={styles.alert}>{alert}</div>}
           <h2 className="g-type-display-2">{title}</h2>
