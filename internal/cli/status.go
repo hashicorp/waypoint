69ff364c7876fe315c9b7a60ae903aa0006a284a
Izaak Lauer <8404559+izaaklauer@users.noreply.github.com>
2021-08-11 12:17:08 -0400

    Helpful error when running `waypoint status` against an old version

--- internal/cli/status.go
+++ internal/cli/status.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/hashicorp/waypoint/internal/version"
+
 	"github.com/dustin/go-humanize"
 	"github.com/golang/protobuf/ptypes"
 	"github.com/golang/protobuf/ptypes/empty"
@@ -194,6 +196,27 @@ func (c *StatusCommand) FormatProjectAppStatus(projectTarget string) error {
 			},
 		})
 		if err != nil {
+			if s, ok := status.FromError(err); ok {
+				if s.Code() == codes.Unimplemented {
+					var serverVersion string
+					serverVersionResp := c.project.ServerVersion()
+					if serverVersionResp != nil {
+						serverVersion = serverVersionResp.Version
+					}
+
+					var clientVersion string
+					clientVersionResp := version.GetVersion()
+					if clientVersionResp != nil {
+						clientVersion = clientVersionResp.Version
+					}
+
+					c.project.UI.Output(
+						fmt.Sprintf("This CLI version %q is incompatible with the current server %q - missing UI_ListDeployments method. Upgrade your server to v0.5.0 or higher or downgrade your CLI to v0.4 or older.", clientVersion, serverVersion),
+						terminal.WithErrorStyle(),
+					)
+					return ErrSentinel
+				}
+			}
 			return err
 		}
 		var appDeployStatus *pb.StatusReport
@@ -338,6 +361,27 @@ func (c *StatusCommand) FormatAppStatus(projectTarget string, appTarget string) 
 		},
 	})
 	if err != nil {
+		if s, ok := status.FromError(err); ok {
+			if s.Code() == codes.Unimplemented {
+				var serverVersion string
+				serverVersionResp := c.project.ServerVersion()
+				if serverVersionResp != nil {
+					serverVersion = serverVersionResp.Version
+				}
+
+				var clientVersion string
+				clientVersionResp := version.GetVersion()
+				if clientVersionResp != nil {
+					clientVersion = clientVersionResp.Version
+				}
+
+				c.project.UI.Output(
+					fmt.Sprintf("This CLI version %q is incompatible with the current server %q - missing UI_ListDeployments method. Upgrade your server to v0.5.0 or higher or downgrade your CLI to v0.4 or older.", clientVersion, serverVersion),
+					terminal.WithErrorStyle(),
+				)
+				return ErrSentinel
+			}
+		}
 		return err
 	}
 
@@ -661,6 +705,27 @@ func (c *StatusCommand) FormatProjectStatus() error {
 				},
 			})
 			if err != nil {
+				if s, ok := status.FromError(err); ok {
+					if s.Code() == codes.Unimplemented {
+						var serverVersion string
+						serverVersionResp := c.project.ServerVersion()
+						if serverVersionResp != nil {
+							serverVersion = serverVersionResp.Version
+						}
+
+						var clientVersion string
+						clientVersionResp := version.GetVersion()
+						if clientVersionResp != nil {
+							clientVersion = clientVersionResp.Version
+						}
+
+						c.project.UI.Output(
+							fmt.Sprintf("This CLI version %q is incompatible with the current server %q - missing UI_ListDeployments method. Upgrade your server to v0.5.0 or higher or downgrade your CLI to v0.4 or older.", clientVersion, serverVersion),
+							terminal.WithErrorStyle(),
+						)
+						return ErrSentinel
+					}
+				}
 				return err
 			}
 
