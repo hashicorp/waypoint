950fd186806f1cb55d6c3d56d2318302c2977ae6
Jamie White <jamie@jgwhite.co.uk>
2021-08-23 13:13:25 +0200

    ui: de-lint `routes/.../settings/repository.ts`

--- ui/app/routes/workspace/projects/project/settings/repository.ts
+++ ui/app/routes/workspace/projects/project/settings/repository.ts
@@ -1,7 +1,3 @@
 import Route from '@ember/routing/route';
 
-export default class WorkspaceProjectsProjectSettingsRepository extends Route.extend({
-  // anything which *must* be merged to prototype here
-}) {
-  // normal class body definition here
-}
+export default class WorkspaceProjectsProjectSettingsRepository extends Route {}
