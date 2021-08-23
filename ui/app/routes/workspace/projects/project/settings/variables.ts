86940ffb93210d811040335d98b6ed8745098e42
Jamie White <jamie@jgwhite.co.uk>
2021-08-23 11:13:01 +0000

    backport of commit 0af1bad7c3fc66d5a155913294806d5c09761884

--- ui/app/routes/workspace/projects/project/settings/variables.ts
+++ ui/app/routes/workspace/projects/project/settings/variables.ts
@@ -1,7 +1,3 @@
 import Route from '@ember/routing/route';
 
-export default class WorkspaceProjectsProjectSettingsVariables extends Route.extend({
-  // anything which *must* be merged to prototype here
-}) {
-  // normal class body definition here
-}
+export default class WorkspaceProjectsProjectSettingsVariables extends Route {}
