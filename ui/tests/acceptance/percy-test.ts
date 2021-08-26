68e99caa39a6185238a74a8cc79aef8d5e5b5c97
Jamie White <jamie@jgwhite.co.uk>
2021-08-26 16:54:47 +0200

    ui/percy: de-lint percy-test

--- ui/tests/acceptance/percy-test.ts
+++ ui/tests/acceptance/percy-test.ts
@@ -1,21 +1,9 @@
 import { module, test } from 'qunit';
 import { visit } from '@ember/test-helpers';
-import { visitable, create, collection, clickable } from 'ember-cli-page-object';
 import { setupApplicationTest } from 'ember-qunit';
 import { setupMirage } from 'ember-cli-mirage/test-support';
 import percySnapshot from '@percy/ember';
-import percyScenario from '../../mirage/scenarios/percy';
 import login from '../helpers/login';
-
-const url = '/default/microchip/app/wp-bandwidth/deployments';
-
-const page = create({
-  visit: visitable(url),
-  list: collection('[data-test-deployment-list] li'),
-  deploymentLinks: collection('[data-test-external-deployment-button]'),
-  destroyedBadges: collection('[data-test-destroyed-badge]'),
-  showDestroyed: clickable('[data-test-display-destroyed-button]'),
-});
 
 module('Acceptance (Percy) | navigating the app', function (hooks) {
   setupApplicationTest(hooks);
