660462035abe114ad14e4c30208a019ee88a8d4a
Jamie White <jamie@jgwhite.co.uk>
2021-09-03 16:39:27 +0200

    Tweak config vars mirage handlers some more

--- ui/mirage/services/config.ts
+++ ui/mirage/services/config.ts
@@ -1,17 +1,18 @@
-import { ConfigGetRequest, ConfigSetRequest, ConfigSetResponse } from 'waypoint-pb';
+import { ConfigGetRequest, ConfigGetResponse, ConfigSetRequest, ConfigSetResponse } from 'waypoint-pb';
 import { Request, Response } from 'miragejs';
 import { decode } from '../helpers/protobufs';
-import configVariable from '../factories/config-variable';
-import { Empty } from 'google-protobuf/google/protobuf/empty_pb';
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any
 export function get(schema: any, { requestBody }: Request): Response {
   let requestMsg = decode(ConfigGetRequest, requestBody);
   let project = requestMsg.getProject();
-  let model = schema.configVariables.where((v) => v.project.name === project.getProject());
-  let variablesList = model?.toProtobuf();
+  let variables = schema.configVariables.where((v) => v.project.name === project.getProject());
+  let variablesList = variables.models.toProtobuf();
+  let response = new ConfigGetResponse();
 
-  return this.serialize(variablesList, 'application');
+  response.setVariablesList(variablesList);
+
+  return this.serialize(response, 'application');
 }
 
 export function set(schema: any, { requestBody }: Request): Response {
@@ -23,8 +24,7 @@ export function set(schema: any, { requestBody }: Request): Response {
     let confVar = schema.configVariables.create(v);
     confVar.project = schema.projects.findBy({ name: projName });
   });
-  // This API endpoint returns an empty {} response, not even Empty(), so serialization somehow fails and the code below doesn't work.
-  // let resp = new ConfigSetResponse();
-  // return this.serialize(resp, 'application');
-  return {};
+  let response = new ConfigSetResponse();
+
+  return this.serialize(response, 'application');
 }
