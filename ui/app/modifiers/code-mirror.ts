09099c1340d988615091c0855c9c0fd707361d59
Sabrina Ko <60155296+sabrinako@users.noreply.github.com>
2021-09-03 11:15:28 -0400

    ui: Update HCL field to codemirror (#2168)
    * ui: Update HCL field to codemirror
    
    * ui: Added testing for codemirror modifier/service
    
    * ui: linting
    
    * ui: fixed package.json dependencies
    
    * ui: added changelog
    
    * ui: addressed comments on pr
    
    * ui: fixed the waypoint hcl mode util

---
+++ ui/app/modifiers/code-mirror.ts
@@ -0,0 +1,55 @@
+import Modifier from 'ember-modifier';
+import codemirror from 'codemirror';
+
+import './utils/register-waypoint-hcl-mode';
+import 'codemirror/addon/edit/matchbrackets';
+import 'codemirror/addon/edit/closebrackets';
+import 'codemirror/addon/selection/active-line';
+
+const _PRESET_DEFAULTS: codemirror.EditorConfiguration = {
+  theme: 'monokai',
+  lineNumbers: true,
+  cursorBlinkRate: 500,
+  matchBrackets: true,
+  autoCloseBrackets: true,
+  styleActiveLine: true,
+};
+interface Args {
+  positional: never;
+  named: {
+    value: string;
+    onInput: (value: string) => void;
+    options?: Record<string, unknown>;
+  };
+}
+export default class CodeMirrorModifier extends Modifier<Args> {
+  _editor!: codemirror.Editor;
+
+  didInstall(): void {
+    this._setup();
+  }
+
+  _onChange(editor: codemirror.Editor): void {
+    let newVal = editor.getValue();
+    this.args.named.onInput(newVal);
+  }
+
+  _setup(): void {
+    if (!this.element) {
+      throw new Error('CodeMirror modifier has no element');
+    }
+
+    let editor = codemirror(this.element, {
+      ..._PRESET_DEFAULTS,
+      ...this.args.named.options,
+      value: this.args.named.value,
+    });
+
+    editor.on('change', (editor) => {
+      this._onChange(editor);
+    });
+
+    this._editor = editor;
+    this._editor.setOption('mode', 'waypointHCL');
+  }
+}
---
+++ ui/app/modifiers/utils/register-waypoint-hcl-mode.ts
@@ -0,0 +1,32 @@
+import codemirror from 'codemirror';
+import 'codemirror/addon/mode/simple';
+
+codemirror.defineSimpleMode('waypointHCL', {
+  start: [
+    { regex: /"(?:[^\\]|\\.)*?(?:"|$)/, token: 'string' }, // strings
+    { regex: /(\w+)(\s+)(=)/, token: ['keyword', 'null', 'null'] }, // assignment of variables
+    {
+      regex: /(build|deploy|release|hook|registry|type|runner|url)( )({)/,
+      token: ['keyword', 'null', 'null'],
+    }, // top level keywords
+    { regex: /(variable)\b/, token: 'keyword' }, // input variable keyword
+    { regex: /true|false|null|undefined/, token: 'atom' }, // bool keywords
+    { regex: /0x[a-f\d]+|[-+]?(?:\.\d+|\d+\.?\d*)(?:e[-+]?\d+)?/i, token: 'number' }, // numbers
+    { regex: /(#|\/\/)(\s*\S*)/, token: 'comment' }, // single line comments
+    { regex: /(path)(.)/, token: ['variable-2', 'null'] }, // path variables
+    { regex: /(workspace)(.)(\S*)/, token: ['string', 'null', 'string'] }, // workspace variables
+    { regex: /\/\*/, token: 'comment', next: 'comment' }, // multi-line comment
+    { regex: /[-+/*=<>!]+/, token: 'operator' }, // operators
+    { regex: /[{[(]/, indent: true }, // for auto indent
+    { regex: /[}\])]/, dedent: true },
+  ],
+  // The multi-line comment state.
+  comment: [
+    { regex: /.*?\*\//, token: 'comment', next: 'start' },
+    { regex: /.*/, token: 'comment' },
+  ],
+  meta: {
+    dontIndentStates: ['comment'],
+    lineComment: '#',
+  },
+});
