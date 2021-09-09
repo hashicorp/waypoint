2a81400d1a70e3b1025a8ffb58373552d1990967
Brian Cain <bcain@hashicorp.com>
2021-09-09 15:05:00 -0700

    internal/server: Add validation to UI_GetProject endpoint

---
+++ internal/server/singleprocess/service_ui_project.go
@@ -0,0 +1,56 @@
+package singleprocess
+
+import (
+	"context"
+	"sort"
+
+	pb "github.com/hashicorp/waypoint/internal/server/gen"
+	serverptypes "github.com/hashicorp/waypoint/internal/server/ptypes"
+)
+
+func (s *service) UI_GetProject(
+	ctx context.Context,
+	req *pb.UI_GetProjectRequest,
+) (*pb.UI_GetProjectResponse, error) {
+	if err := serverptypes.ValidateUIGetProjectRequest(req); err != nil {
+		return nil, err
+	}
+
+	project, err := s.state.ProjectGet(req.Project)
+
+	if err != nil {
+		return nil, err
+	}
+
+	jobs, err := s.state.JobList()
+
+	if err != nil {
+		return nil, err
+	}
+
+	// Sort jobs by queue time (descending)
+	sort.Slice(jobs, func(i int, j int) bool {
+		ti := jobs[i].QueueTime.AsTime()
+		tj := jobs[j].QueueTime.AsTime()
+
+		return !ti.After(tj)
+	})
+
+	var latestInitJob *pb.Job
+
+	for _, job := range jobs {
+		switch job.Operation.(type) {
+		case *pb.Job_Init:
+			if job.Application.Project == project.Name {
+				latestInitJob = job
+				break
+			}
+		}
+
+	}
+
+	return &pb.UI_GetProjectResponse{
+		Project:       project,
+		LatestInitJob: latestInitJob,
+	}, nil
+}
