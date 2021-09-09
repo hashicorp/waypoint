1579e511c84d994ac2eab601b43f13618d987126
Brian Cain <bcain@hashicorp.com>
2021-09-09 20:04:14 +0000

    backport of commit 8edffcfe9417757f5b2c678d2dd04198d30c0cf0

---
+++ internal/server/ptypes/build.go
@@ -0,0 +1,94 @@
+package ptypes
+
+import (
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+	"github.com/imdario/mergo"
+	"github.com/mitchellh/go-testing-interface"
+	"github.com/stretchr/testify/require"
+
+	"github.com/hashicorp/waypoint/internal/pkg/validationext"
+	pb "github.com/hashicorp/waypoint/internal/server/gen"
+)
+
+// TestBuild returns a valid user for tests.
+func TestBuild(t testing.T, src *pb.Build) *pb.Build {
+	t.Helper()
+
+	if src == nil {
+		src = &pb.Build{}
+	}
+
+	require.NoError(t, mergo.Merge(src, &pb.Build{
+		Id: "test",
+	}))
+
+	return src
+}
+
+// ValidateBuild validates the user structure.
+func ValidateBuild(v *pb.Build) error {
+	return validationext.Error(validation.ValidateStruct(v,
+		ValidateBuildRules(v)...,
+	))
+}
+
+// ValidateBuildRules
+func ValidateBuildRules(v *pb.Build) []*validation.FieldRules {
+	return []*validation.FieldRules{
+		validation.Field(&v.Id, validation.Required),
+
+		validation.Field(&v.Sequence, validation.When(v.Id == "", validation.Required).Else(validation.Nil)),
+
+		validationext.StructField(&v.Application, func() []*validation.FieldRules {
+			return []*validation.FieldRules{
+				validation.Field(&v.Application.Application, validation.Required),
+				validation.Field(&v.Application.Project, validation.Required),
+			}
+		}),
+
+		validationext.StructField(&v.Workspace, func() []*validation.FieldRules {
+			return []*validation.FieldRules{
+				validation.Field(&v.Workspace.Workspace, validation.Required),
+			}
+		}),
+	}
+}
+
+// ValidateGetBuildRequest
+func ValidateGetBuildRequest(v *pb.GetBuildRequest) error {
+	return validationext.Error(validation.ValidateStruct(v,
+		validation.Field(&v.Ref, validation.Required),
+		validationext.StructField(&v.Ref, func() []*validation.FieldRules {
+			return ValidateRefOperationRules(v.Ref)
+		}),
+	))
+}
+
+// ValidateListBuildsRequest
+func ValidateListBuildsRequest(v *pb.ListBuildsRequest) error {
+	return validationext.Error(validation.ValidateStruct(v,
+		validationext.StructField(&v.Application, func() []*validation.FieldRules {
+			return []*validation.FieldRules{
+				validation.Field(&v.Application.Application, validation.Required),
+				validation.Field(&v.Application.Project, validation.Required),
+			}
+		})))
+}
+
+// ValidateGetLatestBuildRequest
+func ValidateGetLatestBuildRequest(v *pb.GetLatestBuildRequest) error {
+	return validationext.Error(validation.ValidateStruct(v,
+		validationext.StructField(&v.Application, func() []*validation.FieldRules {
+			return []*validation.FieldRules{
+				validation.Field(&v.Application.Application, validation.Required),
+				validation.Field(&v.Application.Project, validation.Required),
+			}
+		}),
+
+		validationext.StructField(&v.Workspace, func() []*validation.FieldRules {
+			return []*validation.FieldRules{
+				validation.Field(&v.Workspace.Workspace, validation.Required),
+			}
+		}),
+	))
+}
