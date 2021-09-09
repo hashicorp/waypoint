d843d3d85f559e680d91dcb60f4e72c6106abb20
Alex Carpenter <im.alexcarpenter@gmail.com>
2021-09-09 14:58:08 -0400

    Build and Deploy animation (#11)
    * Add terminal setup
    
    * Add commit styling
    
    * Github icon alignment
    
    * Trigger once
    
    * Animate the elements inView
    
    * Add terminal setup
    
    * Add commit styling
    
    * Github icon alignment
    
    * Trigger once
    
    * Animate the elements inView
    
    * Add media queries
    
    * Add terminal setup
    
    * Add commit styling
    
    * Github icon alignment
    
    * Trigger once
    
    * Animate the elements inView
    
    * Add terminal setup
    
    * Add commit styling
    
    * Github icon alignment
    
    * Trigger once
    
    * Animate the elements inView
    
    * Add spacing to media object
    
    * Monitor health animation (#12)
    
    * Animate circles
    
    * Dial in animation
    
    * Update naming
    
    * Dial in animation
    
    * Update naming
    
    * Fix breakpoint
    
    * Dial in animation
    
    * Update naming
    
    * Dial in animation
    
    * Refactor how it works section items into smaller components (#14)
    
    * Refactor into smaller components
    
    * Use row gap instead of margin
    
    * Monitor health animation (#12)
    
    * Animate circles
    
    * Dial in animation
    
    * Update naming
    
    * Dial in animation
    
    * Update naming
    
    * Fix breakpoint
    
    * Dial in animation
    
    * Update naming
    
    * Dial in animation
    
    * Refactor into smaller components
    
    * Use row gap instead of margin
    
    * Monitor and manage animation
    
    * Create unique ids
    
    * Add terminal setup
    
    * Add commit styling
    
    * Github icon alignment
    
    * Trigger once
    
    * Animate the elements inView
    
    * Add terminal setup
    
    * Add commit styling
    
    * Github icon alignment
    
    * Trigger once
    
    * Animate the elements inView
    
    * Add commit styling
    
    * Github icon alignment
    
    * Trigger once
    
    * Animate the elements inView
    
    * Add terminal setup
    
    * Add commit styling
    
    * Trigger once
    
    * Animate the elements inView
    
    * Bring back layout

--- website/components/homepage/sections/how-it-works/build-and-deploy/index.tsx
+++ website/components/homepage/sections/how-it-works/build-and-deploy/index.tsx
@@ -1,8 +1,18 @@
+import { useState } from 'react'
+import { useInView } from 'react-intersection-observer'
+import Typical from 'react-typical'
+import InlineSvg from '@hashicorp/react-inline-svg'
 import NumberedBlock from 'components/homepage/numbered-block'
 import MediaObject from 'components/homepage/media-object'
+import classNames from 'classnames'
 import s from './style.module.css'
 
 export default function BuildAndDeploy() {
+  const [typeFinished, setTypeFinished] = useState(false)
+  const { ref, inView } = useInView({
+    threshold: 1,
+    triggerOnce: true,
+  })
   return (
     <div className={s.root}>
       <div className={s.content}>
@@ -14,8 +24,70 @@ export default function BuildAndDeploy() {
           />
         </NumberedBlock>
       </div>
+
       <div className={s.media}>
-        <img src={require('../img/build-and-deploy.png')} alt="" />
+        <div ref={ref} className={classNames({ [s.active]: typeFinished })}>
+          <div className={s.terminalContainer}>
+            <pre className={s.terminal}>
+              <code>
+                <span className={s.terminalLine}>
+                  <span className={s.terminalTilde}>~</span>
+                </span>
+                <span className={s.terminalLine}>
+                  <span className={s.terminalToken}>❯</span>{' '}
+                  {inView && (
+                    <Typical
+                      steps={[
+                        100,
+                        'waypoint up',
+                        500,
+                        () => setTypeFinished(true),
+                      ]}
+                      wrapper="span"
+                    />
+                  )}
+                </span>
+                <span className={s.terminalLine}>
+                  <span className={s.terminalNote}>
+                    Building tech-blog with Pack...
+                  </span>
+                </span>
+              </code>
+            </pre>
+            <p className={s.note}>Instantly deploy from the command line...</p>
+          </div>
+
+          <div className={s.deploymentContainer}>
+            <InlineSvg
+              className={s.arrow}
+              src={require('./arrow.svg?include')}
+            />
+            <div className={s.deploymentWrapper}>
+              <div className={s.deployment}>
+                <div className={s.deploymentHeading}>
+                  <span>Deploy from</span>
+                  <div className={s.deploymentHeadingBranch}>
+                    <InlineSvg src={require('./github.svg?include')} />{' '}
+                    hashicorp/tech-blog
+                  </div>
+                </div>
+                <span className={s.deploymentCommit}>
+                  Last commit 3 seconds ago by{' '}
+                  <img
+                    src={require('./avatar.jpg')}
+                    width={16}
+                    height={16}
+                    alt=""
+                  />
+                  <strong>@almonk</strong>
+                </span>
+              </div>
+              <p className={s.note}>
+                or connect to GitHub for automatic deploys
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
