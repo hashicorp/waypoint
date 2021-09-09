5da226fca91188fe40dd6c3638b8e46404dc4ea0
Brian Cain <bcain@hashicorp.com>
2021-09-09 14:04:34 -0700

    internal/server: Add API validation for config

---
+++ internal/server/ptypes/config.go
@@ -0,0 +1,28 @@
+package ptypes
+
+import (
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+	"github.com/hashicorp/waypoint/internal/pkg/validationext"
+	pb "github.com/hashicorp/waypoint/internal/server/gen"
+)
+
+// ValidateSetConfigSourceRequest
+func ValidateSetConfigSourceRequest(v *pb.SetConfigSourceRequest) error {
+	return validationext.Error(validation.ValidateStruct(v,
+		validation.Field(&v.ConfigSource, validation.Required),
+	))
+}
+
+// ValidateGetConfigRequest
+func ValidateGetConfigRequest(v *pb.ConfigGetRequest) error {
+	return validationext.Error(validation.ValidateStruct(v,
+		validation.Field(&v.Scope, validation.Required),
+	))
+}
+
+// ValidateGetConfigRequest
+func ValidateGetConfigSourceRequest(v *pb.GetConfigSourceRequest) error {
+	return validationext.Error(validation.ValidateStruct(v,
+		validation.Field(&v.Scope, validation.Required),
+	))
+}
