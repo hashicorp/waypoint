fc88403e1173333d457078a60c1be9c1a7dff4c5
Alex Carpenter <im.alexcarpenter@gmail.com>
2021-09-10 09:48:08 -0400

    Fix mobile overflow

--- website/components/homepage/sections/extend-plugins/index.tsx
+++ website/components/homepage/sections/extend-plugins/index.tsx
@@ -9,30 +9,32 @@ import s from './style.module.css'
 export default function SectionExtendPlugins() {
   return (
     <Section className={s.extendPlugins}>
-      <div className={s.content}>
-        <SectionHeading>Extend Waypoint with plugins</SectionHeading>
-        <SectionDescription>
-          Extend workflows via built-in plugins and an extensible interface.
-          Supports custom builders, deployment platforms, registries, release
-          managers, and more
-        </SectionDescription>
-        <div className={s.contentBlocks}>
-          <MediaObject
-            stacked={true}
-            icon={require('../icons/box.svg?include')}
-            heading="Available Plugins"
-            description="View a list of existing HashiCorp maintained plugins"
-          />
-          <MediaObject
-            stacked={true}
-            icon={require('../icons/code-union.svg?include')}
-            heading="Creating Waypoint Plugins"
-            description="Learn to extend Waypoint for your project’s needs"
-          />
+      <div className={s.inner}>
+        <div className={s.content}>
+          <SectionHeading>Extend Waypoint with plugins</SectionHeading>
+          <SectionDescription>
+            Extend workflows via built-in plugins and an extensible interface.
+            Supports custom builders, deployment platforms, registries, release
+            managers, and more
+          </SectionDescription>
+          <div className={s.contentBlocks}>
+            <MediaObject
+              stacked={true}
+              icon={require('../icons/box.svg?include')}
+              heading="Available Plugins"
+              description="View a list of existing HashiCorp maintained plugins"
+            />
+            <MediaObject
+              stacked={true}
+              icon={require('../icons/code-union.svg?include')}
+              heading="Creating Waypoint Plugins"
+              description="Learn to extend Waypoint for your project’s needs"
+            />
+          </div>
         </div>
-      </div>
-      <div className={s.media}>
-        <InlineSvg src={require('./plugins.svg?include')} />
+        <div className={s.media}>
+          <InlineSvg src={require('./plugins.svg?include')} />
+        </div>
       </div>
     </Section>
   )
