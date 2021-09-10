1d938e4dbdc5c3246d02653e45756370e5ad4077
Alex Carpenter <im.alexcarpenter@gmail.com>
2021-09-10 11:22:43 -0400

    Move overflow to wrapper

--- website/pages/home/index.tsx
+++ website/pages/home/index.tsx
@@ -3,15 +3,16 @@ import SectionHowItWorks from 'components/homepage/sections/how-it-works'
 import SectionMonitorAppHealth from 'components/homepage/sections/monitor-app-health'
 import SectionExtendPlugins from 'components/homepage/sections/extend-plugins'
 import SectionWorkflowThatScales from 'components/homepage/sections/workflow-that-scales'
+import s from './style.module.css'
 
 export default function HomePage() {
   return (
-    <>
+    <div className={s.homePage}>
       <Hero />
       <SectionHowItWorks />
       <SectionMonitorAppHealth />
       <SectionExtendPlugins />
       <SectionWorkflowThatScales />
-    </>
+    </div>
   )
 }
