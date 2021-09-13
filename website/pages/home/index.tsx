d36eabbec62e4ce047ad365ac48aa99823c9e8f2
Alex Carpenter <im.alexcarpenter@gmail.com>
2021-09-13 16:35:07 -0400

    Comment out SectionWorkflowThatScales

--- website/pages/home/index.tsx
+++ website/pages/home/index.tsx
@@ -3,7 +3,7 @@ import Intro from 'components/homepage/intro'
 import SectionHowItWorks from 'components/homepage/sections/how-it-works'
 import SectionMonitorAppHealth from 'components/homepage/sections/monitor-app-health'
 import SectionExtendPlugins from 'components/homepage/sections/extend-plugins'
-import SectionWorkflowThatScales from 'components/homepage/sections/workflow-that-scales'
+// import SectionWorkflowThatScales from 'components/homepage/sections/workflow-that-scales'
 import s from './style.module.css'
 
 export default function HomePage() {
@@ -14,7 +14,7 @@ export default function HomePage() {
       <SectionHowItWorks />
       <SectionMonitorAppHealth />
       <SectionExtendPlugins />
-      <SectionWorkflowThatScales />
+      {/* <SectionWorkflowThatScales /> */}
     </div>
   )
 }
