eb1870a56b1030aeb779948b6f14c4c32ba265f0
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-09-15 16:25:15 -0400

    delete redirects file, moved into docs-preview

--- website/redirects.js
+++
@@ -1,8 +0,0 @@
-module.exports = [
-  // This is an example redirect, it can be removed once other redirects have been added
-  {
-    source: '/__test',
-    destination: '/',
-    permanent: true,
-  },
-]
