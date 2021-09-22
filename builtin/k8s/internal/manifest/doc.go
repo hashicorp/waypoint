92b127226bb582fed2b1d7940fcc28ae48ebb978
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2021-09-22 09:26:58 -0700

    builtin/k8s/internal/manifest to parse multi-doc manifests

---
+++ builtin/k8s/internal/manifest/doc.go
@@ -0,0 +1,4 @@
+// Package manifest provides helpers to parse a Kubernetes "manifest" or
+// multi-resource YAML document. This is used by parts of the plugin such
+// as the Helm chart deployment platform to build status reports.
+package manifest
