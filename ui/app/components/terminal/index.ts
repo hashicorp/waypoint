97bd813d6cdbc31d93904380ce371aeb5d9bc166
Greg Hoin <ghoin@hashicorp.com>
2021-09-24 18:46:59 +0200

    Add folowLogs button

--- ui/app/components/terminal/index.ts
+++ ui/app/components/terminal/index.ts
@@ -2,6 +2,7 @@ import Component from '@glimmer/component';
 import { FitAddon } from 'xterm-addon-fit';
 import { Terminal } from 'xterm';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 interface TerminalComponentArgs {
   terminal: Terminal;
@@ -11,11 +12,13 @@ export default class LogTerminal extends Component<TerminalComponentArgs> {
   element!: HTMLElement;
   terminal: Terminal;
   fitAddon!: FitAddon;
+  @tracked isFollowingLogs: boolean;
 
   constructor(owner: unknown, args: TerminalComponentArgs) {
     super(owner, args);
     let { terminal } = args;
     this.terminal = terminal;
+    this.isFollowingLogs = true;
   }
 
   @action
@@ -33,11 +36,30 @@ export default class LogTerminal extends Component<TerminalComponentArgs> {
       this.fitAddon = fitAddon;
       this.terminal.loadAddon(fitAddon);
       this.fitAddon.fit();
+      this.terminal.onScroll(() => this.viewPortdidScroll());
+      this.element.querySelector('.xterm-viewport')?.addEventListener('scroll', this.viewPortdidScroll);
     }
+  }
+
+  setIsFollowingLogs(): void {
+    let viewport = this.element.querySelector('.xterm-viewport');
+    if (viewport) {
+      this.isFollowingLogs = viewport?.scrollTop >= viewport?.scrollHeight - viewport?.offsetHeight;
+    }
+  }
+
+  viewPortdidScroll = (): void => {
+    this.setIsFollowingLogs();
+  };
+
+  @action
+  followLogs(): void {
+    this.terminal.scrollToBottom();
   }
 
   willDestroyNode = (): void => {
     this.terminal.dispose();
+    this.element.querySelector('.xterm-viewport')?.removeEventListener('scroll', this.viewPortdidScroll);
   };
 
   didResize = (): void => {
