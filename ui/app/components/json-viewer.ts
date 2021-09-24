198e49fcbb95bd7001ce3754feb52f32ae14c158
Jamie White <jamie@jgwhite.co.uk>
2021-09-24 16:15:17 +0200

    ui: add `<JsonViewer>`
    Co-authored-by: Sabrina Ko <sabrina@hashicorp.com>

---
+++ ui/app/components/json-viewer.ts
@@ -0,0 +1,41 @@
+import Component from '@glimmer/component';
+
+interface Args {
+  json?: string;
+  label?: string;
+}
+
+export default class extends Component<Args> {
+  get parseResult(): { data: unknown } | { error: Error } {
+    if (!this.args.json) {
+      return { error: new Error('No source JSON provided') };
+    }
+
+    try {
+      return { data: JSON.parse(this.args.json) };
+    } catch (error) {
+      return { error };
+    }
+  }
+
+  get formattedJSON(): string | undefined {
+    if ('data' in this.parseResult) {
+      return JSON.stringify(this.parseResult.data, null, 2);
+    } else {
+      return;
+    }
+  }
+
+  get screenReaderLabel(): string {
+    return this.args.label ?? 'JSON';
+  }
+
+  get codeMirrorOptions(): Record<string, unknown> {
+    return {
+      mode: { name: 'javascript', json: true },
+      readOnly: true,
+      viewportMargin: Infinity,
+      screenReaderLabel: this.screenReaderLabel,
+    };
+  }
+}
