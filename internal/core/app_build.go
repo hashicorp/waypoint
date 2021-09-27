f8e5c606dd499ffe56d9cba00f004e3f3cbc5b85
catsby <clint@ctshryock.com>
2021-09-27 10:46:20 -0500

    clean up messaging

--- internal/core/app_build.go
+++ internal/core/app_build.go
@@ -2,6 +2,7 @@ package core
 
 import (
 	"context"
+	"errors"
 
 	"github.com/golang/protobuf/proto"
 	"github.com/golang/protobuf/ptypes/any"
@@ -168,11 +169,12 @@ func (op *buildOperation) Do(ctx context.Context, log hclog.Logger, app *App, _ 
 					args = append(args, plugin.ArgNamedAny("access_info", any))
 					log.Debug("injected access info")
 				} else {
-					log.Warn("unexpected response type from callDynamicFunc")
+					log.Error("unexpected response type from callDynamicFunc", "type", hclog.Fmt("%T", raw))
+					return nil, errors.New("AccessInfoFunc didn't provide a typed any")
 				}
 			} else {
 				log.Error("error calling dynamic func", "error", err)
-				return err
+				return nil, err
 			}
 		}
 	}
