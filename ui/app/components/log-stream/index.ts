7dfee73133b79bc36148e367e47fe8b110e73552
Greg Hoin <ghoin@hashicorp.com>
2021-09-27 18:12:59 +0200

    migrate the application logs to the new component too

--- ui/app/components/log-stream/index.ts
+++ ui/app/components/log-stream/index.ts
@@ -1,12 +1,12 @@
-import Component from '@glimmer/component';
-import { tracked } from '@glimmer/tracking';
-import { action } from '@ember/object';
+import { GetLogStreamRequest, LogBatch } from 'waypoint-pb';
 
 import ApiService from 'waypoint/services/api';
-import { inject as service } from '@ember/service';
-import { GetLogStreamRequest, LogBatch } from 'waypoint-pb';
-import { Status } from 'grpc-web';
+import Component from '@glimmer/component';
+import { Terminal } from 'xterm';
+import { createTerminal } from 'waypoint/utils/terminal';
 import { formatRFC3339 } from 'date-fns';
+import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
 
 interface LogStreamArgs {
   req: GetLogStreamRequest;
@@ -15,70 +15,36 @@ interface LogStreamArgs {
 export default class LogStream extends Component<LogStreamArgs> {
   @service api!: ApiService;
 
-  @tracked lines: string[];
-  @tracked isFollowingLogs = true;
-  @tracked badgeCount = 0;
+  @tracked terminal!: Terminal;
+
 
   constructor(owner: unknown, args: LogStreamArgs) {
     super(owner, args);
-    this.lines = [];
-    this.start();
+    this.terminal = createTerminal({ inputDisabled: true });
+    this.startTerminalStream();
   }
 
-  addLine(line: string): void {
-    this.lines = [...this.lines, line];
-    if (this.isFollowingLogs === false) {
-      this.badgeCount = this.badgeCount + 1;
-    }
-  }
-
-  @action
-  followLogs(element: HTMLElement | Event): void {
-    if (element instanceof Event) {
-      if (element.target instanceof HTMLElement) {
-        element = element.target;
-      } else {
-        return;
-      }
-    }
-
-    let scrollableElement = element.closest('.output-scroll-y');
-
-    if (!scrollableElement) {
-      return;
-    }
-
-    scrollableElement.scroll(0, scrollableElement.scrollHeight);
-    this.badgeCount = 0;
-  }
-
-  @action
-  updateScroll(element: HTMLElement): void {
-    if (this.isFollowingLogs === true) {
-      element.scrollIntoView(false);
-      this.badgeCount = 0;
-    }
-  }
-
-  async start(): Promise<void> {
-    let onData = (response: LogBatch) => {
-      response.getLinesList().forEach((entry) => {
-        let ts = entry.getTimestamp();
-        if (!ts) {
-          return;
-        }
-        let prefix = formatRFC3339(ts.toDate());
-        this.addLine(`${prefix}: ${entry.getLine()}`);
-      });
-    };
-
-    let onStatus = (status: Status) => {
-      this.addLine(status.details);
-    };
-
+  async startTerminalStream(): Promise<void> {
     let stream = this.api.client.getLogStream(this.args.req, this.api.WithMeta());
 
-    stream.on('data', onData);
-    stream.on('status', onStatus);
+    stream.on('data', this.onData);
+    stream.on('status', this.onStatus);
   }
+
+  onData = (response: LogBatch): void => {
+    response.getLinesList().forEach((entry) => {
+      let ts = entry.getTimestamp();
+      if (!ts) {
+        return;
+      }
+      let prefix = formatRFC3339(ts.toDate());
+      this.terminal.writeln(`${prefix}: ${entry.getLine()}`);
+    });
+  };
+
+  onStatus = (status: any): void => {
+    if (status.details) {
+      this.terminal.writeln(status.details);
+    }
+  };
 }
