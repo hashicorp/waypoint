57d2b00a6ffae7f2df6b21be97c9595a8973925e
Greg Hoin <ghoin@hashicorp.com>
2021-09-28 15:40:46 +0200

    Add rendering test for terminal component

---
+++ ui/tests/integration/components/terminal-test.ts
@@ -0,0 +1,28 @@
+import { module, test } from 'qunit';
+
+import { createTerminal } from 'waypoint/utils/terminal';
+import hbs from 'htmlbars-inline-precompile';
+import { later } from '@ember/runloop';
+import { render } from '@ember/test-helpers';
+import { setupRenderingTest } from 'ember-qunit';
+
+module('Integration | Component | terminal', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('basic rendering', async function (assert) {
+    // Setup terminal externally, as expected by the component
+    let terminal = createTerminal({ inputDisabled: true });
+    this.set('terminal', terminal);
+    // pass terminal and render
+    await render(hbs`<Terminal @terminal={{this.terminal}}/>`);
+    // write line and see if it renders
+    terminal.writeln('Welcome to Waypoint!');
+    // We have to use the runloop as writeln isn't async
+    // Note that even the xterm.js rendering tests use polling to evaluate rendering
+    later(() => {
+      assert.equal(terminal?.buffer?.active?.getLine(0)?.translateToString(true), 'Welcome to Waypoint!');
+    }, 300);
+    // Clean up terminal
+    terminal.dispose();
+  });
+});
