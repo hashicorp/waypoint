80493efb5f67cbcd65acc4d70b88d747e28a7935
Sabrina Ko <sabrina@hashicorp.com>
2021-09-29 18:24:37 -0400

    ui: updated where image/tag is being pulled from

--- ui/app/components/container-image-tag/index.ts
+++ ui/app/components/container-image-tag/index.ts
@@ -1,45 +1,27 @@
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 import ApiService from 'waypoint/services/api';
-import { GetPushedArtifactRequest, Ref } from 'waypoint-pb';
+import { StatusReport } from 'waypoint-pb';
 import { tracked } from '@glimmer/tracking';
 
 interface DockerImageBadgeArgs {
-  artifactId: string;
+  statusReport: StatusReport.AsObject;
 }
 
 export default class DockerImageBadge extends Component<DockerImageBadgeArgs> {
   @service api!: ApiService;
-  @tracked registry?: string;
   @tracked image?: string;
   @tracked tag?: string;
 
   constructor(owner: unknown, args: DockerImageBadgeArgs) {
     super(owner, args);
 
-    this.checkArtifact();
+    this.parseImageAndTag();
   }
 
-  clearUnicodeCharacters(string: string): string {
-    let newString = string.replace(/[\u0006-\u00ff|\n]/, '');
-    newString = newString.replace(/"/, '');
-    return newString;
-  }
-
-  async checkArtifact(): Promise<void> {
-    let ref = new Ref.Operation();
-    ref.setId(this.args.artifactId);
-
-    let artifactReq = new GetPushedArtifactRequest();
-    artifactReq.setRef(ref);
-
-    let resp = await this.api.client.getPushedArtifact(artifactReq, this.api.WithMeta());
-
-    let textArtifact = atob(resp.getArtifact()?.getArtifact()?.getValue_asB64() || '');
-    textArtifact = this.clearUnicodeCharacters(textArtifact);
-    let arr = textArtifact.split(/\b[\u0006-\u0012]+\b/);
-
-    this.image = arr[0];
-    this.tag = arr[1];
+  parseImageAndTag(): void {
+    let container = this.args.statusReport.resourcesList.find((r) => r.type === 'container');
+    let containerState = JSON.parse(container?.stateJson ?? '');
+    [this.image, this.tag] = containerState['dockerContainerInfo']['Config']['Image'].split(':');
   }
 }
