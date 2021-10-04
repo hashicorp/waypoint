a34d1dabd9ab47cd07c38a97bfe06131c6d693b1
Greg Hoin <ghoin@hashicorp.com>
2021-10-04 18:08:29 +0200

    Add Edge to supported browsers

--- ui/config/targets.js
+++ ui/config/targets.js
@@ -1,6 +1,11 @@
 'use strict';
 
-const browsers = ['last 1 Chrome versions', 'last 1 Firefox versions', 'last 1 Safari versions'];
+const browsers = [
+  'last 1 Chrome versions',
+  'last 1 Firefox versions',
+  'last 1 Safari versions',
+  'last 1 Edge versions',
+];
 
 module.exports = {
   browsers,
