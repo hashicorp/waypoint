5f93ee6ad815c3ac7d057b79f318203ac3cc7462
Jamie White <jamie@jgwhite.co.uk>
2021-10-06 13:56:45 +0200

    ui: extract `<ImageRef>` from `<ContainerImageTag>`

--- ui/tests/integration/components/status-report-meta-table-test.ts
+++ ui/tests/integration/components/status-report-meta-table-test.ts
@@ -35,7 +35,7 @@ module('Integration | Component | meta-table', function (hooks) {
     `);
 
     assert.ok(this.element.textContent?.includes('Image'));
-    assert.dom('[data-test-container-info]').exists();
+    assert.dom('[data-test-image-ref]').exists();
     assert.ok(this.element.textContent?.includes('Health Check'));
     assert.dom('[data-test-status-report-indicator]').exists();
   });
