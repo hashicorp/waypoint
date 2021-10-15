574fe361cdcb7ab0653ac7013fc92a41e3490fb6
Noel Quiles <3746694+EnMod@users.noreply.github.com>
2021-10-15 22:50:40 +0000

    backport of commit 5ebde8fbb7e96e679713a427ffab1ad8b683e354

--- website/data/alert-banner.js
+++ website/data/alert-banner.js
@@ -6,7 +6,7 @@ export default {
   url: 'https://www.hashicorp.com/blog/announcing-hashicorp-waypoint-0-6 ',
   text:
     'Waypoint 0.6 is available to download! Check out the announcement blog for more details.',
-  linkText: 'Download',
+  linkText: 'View Post',
   // Set the `expirationDate prop with a datetime string (e.g. `2020-01-31T12:00:00-07:00`)
   // if you'd like the component to stop showing at or after a certain date
   expirationDate: '',
