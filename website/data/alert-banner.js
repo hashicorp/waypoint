065fdba8b046a5219da72a6628992462c9707057
Noel Quiles <3746694+EnMod@users.noreply.github.com>
2021-10-18 14:22:26 +0000

    backport of commit 07b7c2a286336ce7dc41adc11450a674305cf65d

--- website/data/alert-banner.js
+++ website/data/alert-banner.js
@@ -2,12 +2,11 @@ export const ALERT_BANNER_ACTIVE = true
 
 // https://github.com/hashicorp/web-components/tree/master/packages/alert-banner
 export default {
-  tag: 'New Release',
-  url: 'https://www.hashicorp.com/blog/announcing-hashicorp-waypoint-0-6',
-  text:
-    'Waypoint 0.6 is available to download! Check out the announcement blog for more details.',
-  linkText: 'View Post',
+  tag: 'Oct 19-21',
+  url: 'https://hashiconf.com/global/?utm_source=DocsBanner',
+  text: 'HashiConf Global is live.',
+  linkText: 'Join Now',
   // Set the `expirationDate prop with a datetime string (e.g. `2020-01-31T12:00:00-07:00`)
   // if you'd like the component to stop showing at or after a certain date
-  expirationDate: '',
+  expirationDate: `2021-10-22T12:00:00-07:00`,
 }
