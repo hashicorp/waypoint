d69efef8bbf44ee95eb84702106d3aa1abb42001
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2021-10-22 13:36:12 -0700

    builtin/k8s: fmt

--- builtin/k8s/task.go
+++ builtin/k8s/task.go
@@ -13,7 +13,6 @@ import (
 	"google.golang.org/grpc/status"
 	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/errors"
 	k8sresource "k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/utils/pointer"
