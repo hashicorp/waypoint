7aa1178327ffe6edc557632954d7286e53ef9564
Sabrina Ko <sabrina@hashicorp.com>
2021-10-28 15:06:18 -0400

    ui: fixed failing tests and added test for PanelHeader component

---
+++ ui/tests/integration/components/panel-header-test.ts
@@ -0,0 +1,35 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | panel-title', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders an empty component when params not passed', async function (assert) {
+    await render(hbs`<PanelHeader/>`);
+
+    assert.dom('[data-test-panel-header]').exists();
+    assert.equal(this.element?.textContent?.trim(), '');
+  });
+
+  test('it renders an empty component when params passed', async function (assert) {
+    await render(hbs`
+      <PanelHeader @artifact="build" @sequence={{3}}/>
+    `);
+
+    assert.dom('[data-test-panel-header]').exists();
+    assert.dom('[data-test-panel-header]').containsText('Build');
+    assert.dom('[data-test-panel-header]').containsText('v3');
+  });
+
+  test('it does not renders empty badge when sequence param missing', async function (assert) {
+    await render(hbs`
+      <PanelHeader @artifact="build"/>
+    `);
+
+    assert.dom('[data-test-panel-header]').exists();
+    assert.dom('[data-test-panel-header]').containsText('Build');
+    assert.dom('[data-test-panel-header]').doesNotContainText('v');
+  });
+});
