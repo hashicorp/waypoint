7067665193567d4f4a9e88ec46f331292f6a9b85
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2021-10-29 13:30:47 -0700

    internal/serverstate/statetest

--- internal/server/singleprocess/state/state_test.go
+++ internal/server/singleprocess/state/state_test.go
@@ -2,10 +2,20 @@ package state
 
 import (
 	"math/rand"
+	"testing"
 	"time"
+
+	"github.com/hashicorp/waypoint/internal/serverstate"
+	"github.com/hashicorp/waypoint/internal/serverstate/statetest"
 )
 
 func init() {
 	// Seed our test randomness
 	rand.Seed(time.Now().UnixNano())
+}
+
+func TestImpl(t *testing.T) {
+	statetest.Test(t, func(t *testing.T) serverstate.Interface {
+		return TestState(t)
+	})
 }
---
+++ internal/serverstate/statetest/statetest.go
@@ -0,0 +1,31 @@
+package statetest
+
+import (
+	"testing"
+
+	"github.com/hashicorp/waypoint/internal/serverstate"
+)
+
+// Factory is the function type used to create a new serverstate
+// implementation. To fail, this should fail the test.
+type Factory func(t *testing.T) serverstate.Interface
+
+// Test runs a validation test suite for a state implementation. All
+// state implementations should pass this suite with no errors to ensure
+// the correct behavior of the state when Waypoint uses it.
+func Test(t *testing.T, f Factory) {
+	for name, funcs := range tests {
+		t.Run(name, func(t *testing.T) {
+			for _, tf := range funcs {
+				tf(t, f)
+			}
+		})
+	}
+}
+
+// tests is the list of tests to run.
+var tests = map[string][]testFunc{}
+
+// testFunc is the type of the function that a test that is run as part of
+// Test implements. This is an internal only type.
+type testFunc func(*testing.T, Factory)
