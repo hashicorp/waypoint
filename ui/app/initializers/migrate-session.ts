bb3f9d14bc6705d32d1dfbb106251998567fa86a
Greg Hoin <ghoin@hashicorp.com>
2021-10-29 18:32:49 +0200

    Add initializer comment

--- ui/app/initializers/migrate-session.ts
+++ ui/app/initializers/migrate-session.ts
@@ -1,3 +1,6 @@
+// This initializer is used to migrate the session token in localStorage from the old format to the new one used by ember-simple-auth.
+// It should be safe to remove after the feature has been released.
+
 export function initialize(/*application: Application*/): void {
   let waypointToken = window.localStorage.waypointAuthToken;
   if (waypointToken) {
