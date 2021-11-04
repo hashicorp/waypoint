22d21453c08b699e439cbf7aabd670db95e62857
Kevin Wang <kwangsan@gmail.com>
2021-11-04 17:33:07 -0400

    chore: use dataloader

--- website/pages/docs/[[...page]].jsx
+++ website/pages/docs/[[...page]].jsx
@@ -1,9 +1,6 @@
 import { productName, productSlug } from 'data/metadata'
 import DocsPage from '@hashicorp/react-docs-page'
-import {
-  generateStaticPaths,
-  generateStaticProps,
-} from '@hashicorp/react-docs-page/server'
+import { getStaticGenerationFunctions } from '@hashicorp/react-docs-page/server'
 import Placement from 'components/placement-table'
 import NestedNode from 'components/nested-node'
 
@@ -24,38 +21,13 @@ export default function DocsLayout(props) {
   )
 }
 
-export async function getStaticPaths() {
-  const paths = await generateStaticPaths({
-    navDataFile: NAV_DATA_FILE,
-    localContentDir: CONTENT_DIR,
-    // new ----
-    product: { name: productName, slug: productSlug },
-    basePath,
-  })
-  return {
-    fallback: 'blocking',
-    paths,
-  }
-}
+const { getStaticPaths, getStaticProps } = getStaticGenerationFunctions({
+  basePath: basePath,
+  localContentDir: CONTENT_DIR,
+  navDataFile: NAV_DATA_FILE,
+  product: productSlug,
+  strategy: process.env.STATIC_GENERATION_STRATEGY || 'fs',
+  revalidate: 10,
+})
 
-export async function getStaticProps({ params }) {
-  try {
-    const props = await generateStaticProps({
-      navDataFile: NAV_DATA_FILE,
-      localContentDir: CONTENT_DIR,
-      product: { name: productName, slug: productSlug },
-      params,
-      additionalComponents,
-      basePath,
-    })
-    return {
-      props,
-      revalidate: 10,
-    }
-  } catch (err) {
-    console.warn(err)
-    return {
-      notFound: true,
-    }
-  }
-}
+export { getStaticPaths, getStaticProps }
