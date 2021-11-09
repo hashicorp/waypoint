30f6db299fa03d171d63abd5df5e60e1591a926a
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-11-09 16:42:44 -0500

    feat: experiment with deploy preview

---
+++ website/scripts/website-preview-build.sh
@@ -0,0 +1,4 @@
+mkdir ./website-preview
+mkdir ./website-preview/out
+touch ./website-preview/out/index.html
+echo "hello deployed website preview" >>./website-preview/out/index.html
--- website/scripts/website-preview-setup.sh
+++ website/scripts/website-preview-setup.sh
@@ -1,5 +1,6 @@
 rm -rf ./website-preview
-git clone https://github.com/hashicorp/dev-portal.git website-preview
+git config --global credential.helper store
+git clone "https://zchsh:$GITHUB_PAT_FOR_CLONE@github.com/hashicorp/dev-portal.git" website-preview
 cd website-preview
 git checkout zs.cleanup-refine-migration
 npm ci
