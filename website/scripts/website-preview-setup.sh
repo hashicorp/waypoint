25e438cd4e54d9d7869f97bb478c4b87510f86b7
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-11-09 16:50:49 -0500

    feat: test cloning dev-portal repo in build process

--- website/scripts/website-preview-setup.sh
+++ website/scripts/website-preview-setup.sh
@@ -1,6 +1,6 @@
 rm -rf ./website-preview
 git config --global credential.helper store
-git clone "https://zchsh:$GITHUB_PAT_FOR_CLONE@github.com/hashicorp/dev-portal.git" website-preview
+git clone "https://zchsh:${GITHUB_WEBSITE_PREVIEW_PAT}@github.com/hashicorp/dev-portal.git" website-preview
 cd website-preview
 git checkout zs.cleanup-refine-migration
 npm ci
