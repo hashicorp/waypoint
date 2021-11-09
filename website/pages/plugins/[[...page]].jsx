4681568b3003dd76f37c63cfe0d08185219cbdb7
Kevin Wang <kwangsan@gmail.com>
2021-11-09 22:18:33 +0000

    backport of commit 5b7ecb1492e82f2bfcd33e8045e569fc8fdea5bb

--- website/pages/plugins/[[...page]].jsx
+++ website/pages/plugins/[[...page]].jsx
@@ -12,18 +12,28 @@ export default function DocsLayout(props) {
       product={{ name: productName, slug: productSlug }}
       baseRoute={basePath}
       staticProps={props}
-      showVersionSelect={process.env.ENABLE_VERSIONED_DOCS}
+      showVersionSelect={process.env.ENABLE_VERSIONED_DOCS === 'true'}
     />
   )
 }
 
-const { getStaticPaths, getStaticProps } = getStaticGenerationFunctions({
-  basePath: basePath,
-  localContentDir: CONTENT_DIR,
-  navDataFile: NAV_DATA_FILE,
-  product: productSlug,
-  strategy: process.env.STATIC_GENERATION_STRATEGY || 'fs',
-  revalidate: 10,
-})
+const { getStaticPaths, getStaticProps } = getStaticGenerationFunctions(
+  process.env.ENABLE_VERSIONED_DOCS === 'true'
+    ? {
+        strategy: 'remote',
+        basePath: basePath,
+        fallback: 'blocking',
+        revalidate: 360, // 1 hour
+        product: productSlug,
+      }
+    : {
+        strategy: 'fs',
+        basePath: basePath,
+        localContentDir: CONTENT_DIR,
+        navDataFile: NAV_DATA_FILE,
+        product: productSlug,
+        revalidate: false,
+      }
+)
 
 export { getStaticPaths, getStaticProps }
