c17192ba9a4a3e8f2c792ef17b3cec69cd8be20c
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-11-10 16:01:58 -0500

    fix: confusing copying in build script

--- website/scripts/website-preview-build.sh
+++ website/scripts/website-preview-build.sh
@@ -11,6 +11,13 @@ git clone "https://zchsh:${GITHUB_WEBSITE_PREVIEW_PAT}@github.com/hashicorp/dev-
 # TODO: to be an arg of renderPageMdx, not fixed. ref:
 # TODO: https://github.com/hashicorp/react-components/blob/a546b9dc9a874df81a324b9e54e4fb1034b79a5d/packages/docs-page/render-page-mdx.js#L14
 cp -R ./content/ ./website-preview/content
+# Merge the local images (all in ./public/img) into
+# the shared dev-portal public folder
+cp -R ./public/img/** ./website-preview/public/img/
+# Delete the local public folder and replace it
+# with the now-combined dev-portal public folder
+rm -rf ./public
+cp -R ./website-preview/public/ ./public
 # Change into the cloned dev-portal directory
 cd ./website-preview
 # Install dependencies
@@ -23,10 +30,3 @@ npm run build
 # Copy .next build output folder into project root,
 # so that Vercel's NextJS preset picks up on the build output
 cp -R .next/ ../.next
-# Merge the local images (all in ./public/img) into
-# the shared dev-portal public folder
-cp -R ./public/img/** ./website-preview/public/img/
-# Delete the local public folder and replace it
-# with the now-combined dev-portal public folder
-rm -rf ../public
-cp -R public/ ../public
