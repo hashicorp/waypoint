c3d42eb2b1d5853707d9f47846cd6001dd0f47d4
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-11-10 15:33:39 -0500

    chore: clean up setup script

--- website/scripts/website-preview-setup.sh
+++ website/scripts/website-preview-setup.sh
@@ -1,2 +1,14 @@
+# TODO: would be nice to only reset, clone,
+# TODO: install deps, etc if website-preview
+# TODO: does not already exist
+# Clean up existing clone
 rm -rf ./website-preview
-git clone https://github.com/hashicorp/dev-portal.git website-preview && cd website-preview && git checkout zs.cleanup-refine-migration && npm i --production=false
+# Clone project, and cd into it
+git clone https://github.com/hashicorp/dev-portal.git website-preview
+cd website-preview
+# Checkout target branch
+# TODO: should be main once this PR merges:
+# TODO: https://github.com/hashicorp/dev-portal/pull/34
+git checkout zs.cleanup-refine-migration
+# Install dependencies
+npm i --production=false
