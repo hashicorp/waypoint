ecaf8dc2cfbaebdc7e585d0c3db0b2d995dac60c
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-11-10 09:36:18 -0500

    fix: lcopy local images so they load

--- website/scripts/website-preview-setup.sh
+++ website/scripts/website-preview-setup.sh
@@ -1,4 +1,4 @@
 rm -rf ./website-preview
-# git clone https://github.com/hashicorp/dev-portal.git website-preview && cd website-preview && git checkout zs.cleanup-refine-migration && npm i
+# git clone https://github.com/hashicorp/dev-portal.git website-preview && cd website-preview && git checkout zs.cleanup-refine-migration && npm i --production=false
 git config --global credential.helper store
 git clone "https://zchsh:${GITHUB_WEBSITE_PREVIEW_PAT}@github.com/hashicorp/dev-portal.git" website-preview && cd website-preview && git checkout zs.cleanup-refine-migration && npm i --production=false
--- website/scripts/website-preview-start.sh
+++ website/scripts/website-preview-start.sh
@@ -1,3 +1,6 @@
 cd website-preview
+git clean -f
 rm -rf .next
+cp -R ../content/ ./content
+cp -R ../public/img/** ./public/img/
 DEV_IO_PROXY=waypoint IS_CONTENT_DEPLOY_PREVIEW=true npm run start
