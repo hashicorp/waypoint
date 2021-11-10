0937280297286fa60695c8b61a97e5563d10f73e
Rae Krantz <8461333+krantzinator@users.noreply.github.com>
2021-11-10 22:55:03 +0000

    backport of commit fe012adb9c915152ca1880125887747ea18570cf

--- internal/serverinstall/nomad.go
+++ internal/serverinstall/nomad.go
@@ -1101,6 +1101,7 @@ func (i *NomadInstaller) OnDemandRunnerConfig() *pb.OnDemandRunnerConfig {
 	}
 
 	return &pb.OnDemandRunnerConfig{
+		Name:         "nomad",
 		OciUrl:       i.config.odrImage,
 		PluginType:   "nomad",
 		Default:      true,
