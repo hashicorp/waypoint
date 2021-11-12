954ee445018fb889b5111c5cd62e2d1d0f5834fd
Izaak Lauer <8404559+izaaklauer@users.noreply.github.com>
2021-11-12 16:03:28 -0500

    First tests working!

--- internal/pkg/vcsutil/vcs_util.go
+++ internal/pkg/vcsutil/vcs_util.go
@@ -3,19 +3,38 @@ package vcsutil
 import (
 	"fmt"
 	"os/exec"
+	"strings"
+
+	"github.com/hashicorp/go-hclog"
+
+	"github.com/pkg/errors"
 )
 
-func IsDirty(path string) (bool, error) {
-	cmd := exec.Command("git", "-C", path, "status", "-s")
-	stdout, err := cmd.Output()
-	if err != nil {
-		return false, err
+type VcsChecker struct {
+	log  hclog.Logger
+	path string
+}
+
+func NewVcsChecker(log hclog.Logger, path string) *VcsChecker {
+	return &VcsChecker{
+		log:  log,
+		path: path,
 	}
-	if len(stdout) > 0 {
+}
+
+// TODO(izaak): think about http vs ssh urls
+func (v *VcsChecker) IsDirty(remoteUrl string, remoteBranch string) (bool, error) {
+	gitStatus, err := v.runVcsGitCommand("status", "-s")
+	if len(gitStatus) > 0 {
 		return true, nil
 	}
 
-	diff, err := remoteHasDiff(path)
+	remoteName, err := v.getRemoteName(remoteUrl)
+	if err != nil {
+		return false, errors.Wrapf(err, "Failed to get remote name for url %s", remoteUrl)
+	}
+
+	diff, err := v.remoteHasDiff(remoteName, remoteBranch)
 	if err != nil {
 		return false, err
 	}
@@ -23,31 +42,74 @@ func IsDirty(path string) (bool, error) {
 	return diff, nil
 }
 
-func remoteHasDiff(path string) (bool, error) {
-
-	// check that local dir has a default git remote
-	cmd := exec.Command("git", "-C", path, "rev-parse", "--abbrev-ref", "HEAD")
-	fmt.Println(cmd)
-	sdout, err := cmd.Output()
+func (v *VcsChecker) getRemoteName(url string) (name string, err error) {
+	remotes, err := v.runVcsGitCommand("remote", "-v")
 	if err != nil {
-		return false, err
+		return "", errors.Wrapf(err, "failed to list git remotes")
 	}
 
-	cmd = exec.Command("git", "-C", path, "config", "branch."+string(sdout)+".remote")
-	remote, err := cmd.Output()
-	fmt.Println(string(remote))
-	if err != nil {
-		return false, err
+	var matchingRemoteName string
+
+	for _, line := range strings.Split(remotes, "\n") {
+		if line == "" {
+			// This always happens once
+			continue
+		}
+		split1 := strings.Split(line, "\t")
+		if len(split1) != 2 {
+			// That's weird
+			continue
+		}
+		remoteName := split1[0]
+		theRestOfTheRemote := strings.Split(split1[1], " ")
+		if len(theRestOfTheRemote) != 2 {
+			// That's weird too
+			continue
+		}
+		remoteUrl := theRestOfTheRemote[0]
+		remoteType := theRestOfTheRemote[1]
+
+		if url != remoteUrl {
+			continue
+		}
+
+		// So the url matches, but can we fetch from it?
+		// It would be weird if we couldn't.
+
+		if remoteType != "(fetch)" {
+			// TODO: it could be nice to warn if we find a remote with the right URI but wrong type
+			continue
+		}
+
+		matchingRemoteName = remoteName
 	}
 
-	// cmd = exec.Command("git", "-C", path, "diff", string(remote))
-	// stdout, err := cmd.Output()
-	// if err != nil {
-	// 	return false, err
-	// }
-	// if len(stdout) > 0 {
-	// 	return true, nil
-	// }
+	if matchingRemoteName == "" {
+		return "", fmt.Errorf("no remote with url matching %s found", url)
+	}
+	return matchingRemoteName, nil
+}
 
+// will error if no remote url is found
+func (v *VcsChecker) remoteHasDiff(remoteName string, remoteBranch string) (bool, error) {
+	diff, err := v.runVcsGitCommand("diff", fmt.Sprintf("%s/%s", remoteName, remoteBranch))
+	if err != nil {
+		return false, errors.Wrapf(err, "failed to diff against remote %s", remoteName)
+	}
+	if len(diff) > 0 {
+		return true, nil
+	}
 	return false, nil
+}
+
+func (v *VcsChecker) runVcsGitCommand(gitArgs ...string) (output string, err error) {
+	return runGitCommand(v.log, v.path, gitArgs...)
+}
+
+func runGitCommand(log hclog.Logger, path string, gitArgs ...string) (output string, err error) {
+	args := append([]string{"-C", path}, gitArgs...)
+	log.Debug(fmt.Sprintf("Running this command: git %s", strings.Join(args, " ")))
+	cmd := exec.Command("git", args...)
+	out, err := cmd.Output()
+	return string(out), err
 }
--- internal/pkg/vcsutil/vcs_util_test.go
+++ internal/pkg/vcsutil/vcs_util_test.go
@@ -3,89 +3,142 @@ package vcsutil
 import (
 	"io/ioutil"
 	"os"
-	"os/exec"
 	"testing"
+
+	"github.com/hashicorp/go-hclog"
 
 	"github.com/stretchr/testify/require"
 )
 
+// TODO(izaak): clean up afterwards
+
+func generateGitState(branchName string) (repoFolder string, remoteUrl string, err error) {
+
+	log := hclog.Default()
+
+	// Create a temporary directory for our test
+	td, err := ioutil.TempDir("", "test")
+	if err != nil {
+		return "", "", err
+	}
+	//defer os.RemoveAll(td)
+
+	// Create a temporary directory for our remote
+	remote, err := ioutil.TempDir("", "test")
+	if err != nil {
+		return "", "", err
+	}
+	//defer os.RemoveAll(remote)
+
+	if _, err := runGitCommand(log, td, "init", "-b", branchName); err != nil {
+		return "", "", err
+	}
+
+	if _, err := runGitCommand(log, remote, "init"); err != nil {
+		return "", "", err
+	}
+
+	remoteUrl = remote + "/.git"
+
+	remoteName := "remote"
+	// add remote
+	if _, err := runGitCommand(log, td, "remote", "add", remoteName, remoteUrl); err != nil {
+		return "", "", err
+	}
+
+	// Create a test file and commit
+	file := td + "/main"
+	r, err := os.OpenFile(file, os.O_CREATE, 0600)
+	r.Close()
+
+	if _, err := runGitCommand(log, td, "add", "main"); err != nil {
+		return "", "", err
+	}
+
+	if _, err := runGitCommand(log, td, "commit", "-m", "'first commit'"); err != nil {
+		return "", "", err
+	}
+
+	if _, err := runGitCommand(log, td, "push", remoteName, branchName); err != nil {
+		return "", "", err
+	}
+
+	return td, remoteUrl, nil
+}
+
 func TestIsDirty(t *testing.T) {
+	hclog.Default().SetLevel(hclog.Debug)
+
 	require := require.New(t)
+	branchName := "waypoint"
 
-	// Create a temporary directory for our test
-	td, err := ioutil.TempDir("", "test")
-	require.NoError(err)
-	defer os.RemoveAll(td)
-
-	// Create a temporary directory for our remote
-	remote, err := ioutil.TempDir("", "test")
-	require.NoError(err)
-	defer os.RemoveAll(remote)
-
-	cmd := exec.Command("git", "-C", td, "init")
-	err = cmd.Run()
+	repoPath, remoteUrl, err := generateGitState(branchName)
 	require.NoError(err)
 
-	// add remote
-	cmd = exec.Command("git", "-C", td, "remote", "add", "remote", remote+"/.git")
-	err = cmd.Run()
-	require.NoError(err)
+	v := NewVcsChecker(
+		hclog.Default(),
+		repoPath,
+	)
 
-	dirty, err := IsDirty(td)
-	require.NoError(err)
-	require.False(dirty)
+	t.Run("Initial state is clean", func(t *testing.T) {
+		dirty, err := v.IsDirty(remoteUrl, branchName)
+		require.NoError(err)
+		require.False(dirty)
+	})
 
-	file := td + "/dirtyfile"
-	r, err := os.OpenFile(file, os.O_CREATE, 0600)
-	r.Close()
-	require.NoError(err)
+	t.Run("Creating (but not commiting) a new file is dirty", func(t *testing.T) {
+		file := repoPath + "/dirtyfile"
+		r, err := os.OpenFile(file, os.O_CREATE, 0600)
+		r.Close()
+		require.NoError(err)
 
-	dirty, err = IsDirty(td)
-	require.NoError(err)
-	require.True(dirty)
+		dirty, err := v.IsDirty(remoteUrl, branchName)
+		require.NoError(err)
+		require.True(dirty)
+	})
 }
 
-func TestRemotesMatchCommitted(t *testing.T) {
-	require := require.New(t)
-
-	// Create a temporary directory for our test
-	td, err := ioutil.TempDir("", "test")
-	require.NoError(err)
-	defer os.RemoveAll(td)
-
-	// Create a temporary directory for our remote
-	remote, err := ioutil.TempDir("", "test")
-	require.NoError(err)
-	defer os.RemoveAll(remote)
-
-	cmd := exec.Command("git", "-C", td, "init")
-	err = cmd.Run()
-	require.NoError(err)
-
-	// add remote
-	cmd = exec.Command("git", "-C", td, "remote", "add", "remote", remote+"/.git")
-	err = cmd.Run()
-	require.NoError(err)
-
-	// check if local commits differ from remote
-	match, err := remoteHasDiff(td)
-	require.NoError(err)
-	require.False(match)
-
-	// commit file
-	file := td + "/dirtyfile"
-	r, err := os.OpenFile(file, os.O_CREATE, 0600)
-	r.Close()
-	require.NoError(err)
-	cmd = exec.Command("git", "-C", td, "add", ".")
-	err = cmd.Run()
-	require.NoError(err)
-	cmd = exec.Command("git", "-C", td, "commit", "-m", "'nothing much'")
-	err = cmd.Run()
-	require.NoError(err)
-
-	// check if local commits differ from remote
-	match, err = remoteHasDiff(td)
-	require.NoError(err)
-	require.True(match)
-}
+//func TestRemotesMatchCommitted(t *testing.T) {
+//	require := require.New(t)
+//
+//	// Create a temporary directory for our test
+//	td, err := ioutil.TempDir("", "test")
+//	require.NoError(err)
+//	defer os.RemoveAll(td)
+//
+//	// Create a temporary directory for our remote
+//	remote, err := ioutil.TempDir("", "test")
+//	require.NoError(err)
+//	defer os.RemoveAll(remote)
+//
+//	cmd := exec.Command("git", "-C", td, "init")
+//	err = cmd.Run()
+//	require.NoError(err)
+//
+//	// add remote
+//	cmd = exec.Command("git", "-C", td, "remote", "add", "remote", remote+"/.git")
+//	err = cmd.Run()
+//	require.NoError(err)
+//
+//	// check if local commits differ from remote
+//	match, err := remoteHasDiff(td, remote)
+//	require.NoError(err)
+//	require.False(match)
+//
+//	// commit file
+//	file := td + "/dirtyfile"
+//	r, err := os.OpenFile(file, os.O_CREATE, 0600)
+//	r.Close()
+//	require.NoError(err)
+//	cmd = exec.Command("git", "-C", td, "add", ".")
+//	err = cmd.Run()
+//	require.NoError(err)
+//	cmd = exec.Command("git", "-C", td, "commit", "-m", "'nothing much'")
+//	err = cmd.Run()
+//	require.NoError(err)
+//
+//	// check if local commits differ from remote
+//	match, err = remoteHasDiff(td, remote)
+//	require.NoError(err)
+//	require.True(match)
+//}
