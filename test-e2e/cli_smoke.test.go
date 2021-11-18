1f724928eefd2285509854ad8c9a40dfad3c0d42
Izaak Lauer <8404559+izaaklauer@users.noreply.github.com>
2021-11-18 15:03:08 -0500

    uncomment important bit

--- test-e2e/cli_smoke.test.go
+++ test-e2e/cli_smoke.test.go
@@ -1,18 +1,23 @@
 package test
 
-//func TestWaypointInstall(t *testing.T) {
-//	t.Logf("Testing waypoint is available...")
-//	wp := NewBinary(wpBinary, ".")
-//	stdout, stderr, err := wp.Run("version")
-//	if err != nil {
-//		t.Errorf("unexpected error getting version: %s", err)
-//	}
-//
-//	if stderr != "" {
-//		t.Errorf("unexpected stderr output getting version: %s", err)
-//	}
-//
-//	if !strings.Contains(stdout, "Waypoint v") {
-//		t.Errorf("No version output detected:\n%s", stdout)
-//	}
-//}
+import (
+	"strings"
+	"testing"
+)
+
+func TestWaypointInstall(t *testing.T) {
+	t.Logf("Testing waypoint is available...")
+	wp := NewBinary(t, wpBinary, ".")
+	stdout, stderr, err := wp.RunRaw("version")
+	if err != nil {
+		t.Errorf("unexpected error getting version: %s", err)
+	}
+
+	if stderr != "" {
+		t.Errorf("unexpected stderr output getting version: %s", err)
+	}
+
+	if !strings.Contains(stdout, "Waypoint v") {
+		t.Errorf("No version output detected:\n%s", stdout)
+	}
+}
