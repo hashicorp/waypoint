bde8909cb988b2644f70d15646eb1b2e9acf0b62
Jamie White <jamie@jgwhite.co.uk>
2021-11-22 17:25:14 +0100

    ui/mirage: use Schema in build service

--- ui/mirage/services/build.ts
+++ ui/mirage/services/build.ts
@@ -1,18 +1,21 @@
 import { ListBuildsRequest, ListBuildsResponse, GetBuildRequest } from 'waypoint-pb';
 import { Request, Response } from 'miragejs';
-import { RouteHandler } from '../types';
+import { RouteHandler, Schema } from '../types';
 import { decode } from '../helpers/protobufs';
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any
-export function list(this: RouteHandler, schema: any, { requestBody }: Request): Response {
+export function list(this: RouteHandler, schema: Schema, { requestBody }: Request): Response {
   let requestMsg = decode(ListBuildsRequest, requestBody);
   let projectName = requestMsg.getApplication()?.getProject();
   let appName = requestMsg.getApplication()?.getApplication();
   let workspaceName = requestMsg.getWorkspace()?.getWorkspace();
-  let project = schema.projects.findBy({ name: projectName });
-  let application = schema.applications.findBy({ name: appName, projectId: project.id });
-  let workspace = schema.workspaces.findBy({ name: workspaceName });
-  let builds = schema.builds.where({ applicationId: application?.id, workspaceId: workspace?.id });
+  let project = schema.findBy('project', { name: projectName });
+  if (!project) {
+    throw `Project ${projectName} not found`;
+  }
+  let application = schema.findBy('application', { name: appName, projectId: project.id });
+  let workspace = schema.findBy('workspace', { name: workspaceName });
+  let builds = schema.where('build', { applicationId: application?.id, workspaceId: workspace?.id });
   let buildProtobufs = builds.models.map((b) => b.toProtobuf());
   let resp = new ListBuildsResponse();
 
@@ -24,10 +27,13 @@ export function list(this: RouteHandler, schema: any, { requestBody }: Request):
 }
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any
-export function get(this: RouteHandler, schema: any, { requestBody }: Request): Response {
+export function get(this: RouteHandler, schema: Schema, { requestBody }: Request): Response {
   let requestMsg = decode(GetBuildRequest, requestBody);
   let id = requestMsg.getRef()?.getId();
-  let model = schema.builds.find(id);
+  if (!id) {
+    throw 'id is required';
+  }
+  let model = schema.find('build', id);
   let build = model?.toProtobuf();
 
   return this.serialize(build, 'application');
