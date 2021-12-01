e36f0d49793ce0b0336cf02473e38d312486c0b8
Brian Cain <bcain@hashicorp.com>
2021-12-01 13:35:45 -0800

    Ensure requests to trigger service API is valid

--- internal/server/singleprocess/service_trigger.go
+++ internal/server/singleprocess/service_trigger.go
@@ -5,13 +5,16 @@ import (
 
 	"github.com/golang/protobuf/ptypes/empty"
 	pb "github.com/hashicorp/waypoint/internal/server/gen"
+	serverptypes "github.com/hashicorp/waypoint/internal/server/ptypes"
 )
 
 func (s *service) UpsertTrigger(
 	ctx context.Context,
 	req *pb.UpsertTriggerRequest,
 ) (*pb.UpsertTriggerResponse, error) {
-	// TODO: Validate request with ptypes
+	if err := serverptypes.ValidateUpsertTriggerRequest(req); err != nil {
+		return nil, err
+	}
 
 	result := req.Trigger
 	if err := s.state.TriggerPut(result); err != nil {
@@ -26,7 +29,9 @@ func (s *service) GetTrigger(
 	ctx context.Context,
 	req *pb.GetTriggerRequest,
 ) (*pb.GetTriggerResponse, error) {
-	// TODO: Validate request with ptypes
+	if err := serverptypes.ValidateGetTriggerRequest(req); err != nil {
+		return nil, err
+	}
 
 	t, err := s.state.TriggerGet(req.Ref)
 	if err != nil {
@@ -41,7 +46,9 @@ func (s *service) DeleteTrigger(
 	ctx context.Context,
 	req *pb.DeleteTriggerRequest,
 ) (*empty.Empty, error) {
-	// TODO: Validate request with ptypes
+	if err := serverptypes.ValidateDeleteTriggerRequest(req); err != nil {
+		return nil, err
+	}
 
 	err := s.state.TriggerDelete(req.Ref)
 	if err != nil {
@@ -55,7 +62,7 @@ func (s *service) ListTriggers(
 	ctx context.Context,
 	req *pb.ListTriggerRequest,
 ) (*pb.ListTriggerResponse, error) {
-	// TODO: Validate request with ptypes
+	// NOTE: no ptype validation at the moment, as all Ref fields are optional
 
 	result, err := s.state.TriggerList()
 	if err != nil {
