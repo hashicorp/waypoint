7a7a6d0d3ec972dc45ef85e725b2970cfb74511e
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-12-07 16:14:33 -0500

    refactor: consolidate into single start script

--- website/scripts/website-preview-start.sh
+++ website/scripts/website-preview-start.sh
@@ -1,3 +1,31 @@
+# TODO: would be nice to only reset, clone,
+# TODO: install deps, etc if website-preview
+# TODO: does not already exist
+# Clean up existing clone
+# TODO: actually, don't do this,
+# TODO: just git clean and then checkout the
+# TODO: target branch (if the repo already exists)
+# Clone project, and cd into it
+# IF ./website-preview does NOT exist...
+FILE=website-preview
+if [ ! -d "$FILE" ]; then
+    echo "Cloning the dev-portal repo, this might take a while..."
+    git clone https://github.com/hashicorp/dev-portal.git website-preview
+fi
+# cd into the project
+cd website-preview
+# Ensure we're on the branch intended for preview mode
+# TODO: this should always be main,
+# TODO: ie no checkout command, once we're ready
+git checkout zs.refine-preview-modes
+# TODO: move the below into a script within dev-portal,
+# TODO: which we can now run since we'll
+# TODO: have cloned it and checked out latest main
+# Install dependencies
+# TODO maybe some way to optimize this?
+# TODO likely slow with npm ci deleting node_modules
+# TODO every time...
+npm i --production=false
 # TODO: clean this up, add comments
 cd ./website-preview
 git restore --staged .
