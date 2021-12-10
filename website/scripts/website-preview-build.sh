0bd9a1bc1345c81b24767ba34d3569f38cfd186f
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-12-10 13:15:36 -0500

    chore: move bulk of build & start scripts into dev-portal

--- website/scripts/website-preview-build.sh
+++ website/scripts/website-preview-build.sh
@@ -1,67 +1,12 @@
-# Delete cached .next, if present
-# TODO: would be nice to leverage caching,
-# TODO: deleted for now as it makes debugging harder
-echo "Files before .next cache delete:"
-ls -a
-echo "Deleting .next cache..."
-rm -rf .next
-echo "Done"
-# Clone the dev-portal repo in ./website-preview
-git clone "https://github.com/hashicorp/dev-portal.git" website-preview
-# Merge the local images (all in ./public/img) into
-# the shared dev-portal public folder
-cp -R ./public/img/** ./website-preview/public/img/
-# Delete the local public folder and replace it
-# with the now-combined dev-portal public folder
-rm -rf ./public
-cp -R ./website-preview/public/ ./public
+# Set the subdirectory name for the dev-portal app
+PREVIEW_DIR=website-preview
+# Clone the dev-portal repo
+git clone "https://github.com/hashicorp/dev-portal.git" "$PREVIEW_DIR"
+# TODO cd & checkout is only necessary
+# TODO during proof-of-concept of dev-portal work.
+# TODO will later just run off of main branch.
 # Change into the cloned dev-portal directory
-cd ./website-preview
-# Install dependencies
+cd "$PREVIEW_DIR"
 git checkout zs.refine-preview-modes
-npm i --production=false
-#
-# BEGIN "CLEAR OTHER PRODUCTS"
-#
-# Delete other products' docs pages,
-# these will just increase build times
-# TODO instead of doing thing, should perhaps
-# TODO delete /pages/_proxied-dot-io/boundary
-# TODO entirely? Such routes will be redirected
-# TODO anyways, so not point wasting time
-# TODO in rendering them...
-# TODO however, this requires product repo awareness.
-# TODO perhaps we could achieve a similar effect
-# TODO with logic in the dev-portal docs routes
-# TODO which would return an empty array
-# TODO from getStaticPaths if the product
-# TODO is not the target product (based on env check)
-# TODO during a build
-# Remove Boundary
-# ... but first save some global CSS files,
-# ... otherwise the build breaks
-mkdir ./temp-global-page-css
-mv ./src/pages/_proxied-dot-io/boundary/home/style.css ./temp-global-page-css/boundary-home.css
-rm -rf ./src/pages/_proxied-dot-io/boundary
-mkdir ./src/pages/_proxied-dot-io/boundary
-mkdir ./src/pages/_proxied-dot-io/boundary/home
-# ... put the saved CSS files back
-mv ./temp-global-page-css/boundary-home.css ./src/pages/_proxied-dot-io/boundary/home/style.css
-# Remove Waypoint
-# rm -rf ./src/pages/_proxied-dot-io/waypoint
-# Remove Sentinel
-rm -rf ./src/pages/_proxied-dot-io/sentinel
-#
-# END "CLEAR OTHER PRODUCTS"
-#
-# Build the site
-# Note that DEV_IO and IS_CONTENT_PREVIEW are set
-# in Vercel configuration for the project
-npm run build
-# Copy .next build output folder into project root,
-# so that Vercel's NextJS preset picks up on the build output
-echo "Listing files after build..."
-ls -a
-echo "Copying .next output to project root..."
-cp -R .next/ ../.next
-echo "Done."
+# Run the dev-portal content-repo build script
+REPO=waypoint PREVIEW_DIR="$PREVIEW_DIR" ./scripts/content-repo-preview/build.sh
--- website/scripts/website-preview-start.sh
+++ website/scripts/website-preview-start.sh
@@ -1,44 +1,15 @@
-# TODO: would be nice to only reset, clone,
-# TODO: install deps, etc if website-preview
-# TODO: does not already exist
-# Clean up existing clone
-# TODO: actually, don't do this,
-# TODO: just git clean and then checkout the
-# TODO: target branch (if the repo already exists)
-# Clone project, and cd into it
-# IF ./website-preview does NOT exist...
-FILE=website-preview
-if [ ! -d "$FILE" ]; then
-    echo "Cloning the dev-portal repo, this might take a while..."
-    git clone https://github.com/hashicorp/dev-portal.git website-preview
+# Set the subdirectory name for the dev-portal app
+PREVIEW_DIR=website-preview
+# Clone the dev-portal project, if needed
+if [ ! -d "$PREVIEW_DIR" ]; then
+    echo "⏳ Cloning the dev-portal repo, this might take a while..."
+    git clone https://github.com/hashicorp/dev-portal.git "$PREVIEW_DIR"
 fi
-# cd into the project
-cd website-preview
+# cd into the dev-portal project
+cd "$PREVIEW_DIR"
 # Ensure we're on the branch intended for preview mode
 # TODO: this should always be main,
 # TODO: ie no checkout command, once we're ready
 git checkout zs.refine-preview-modes
-# TODO: move the below into a script within dev-portal,
-# TODO: which we can now run since we'll
-# TODO: have cloned it and checked out latest main
-# Install dependencies
-# TODO maybe some way to optimize this?
-# TODO likely slow with npm ci deleting node_modules
-# TODO every time...
-npm i --production=false
-# TODO: clean this up, add comments
-cd ./website-preview
-git restore --staged .
-git restore .
-git clean -f -d
-git checkout zs.refine-preview-modes
-git pull
-rm -rf .next
-# cp -R ../content/ ./content
-# TODO set up watcher to sync all files
-# TODO under website/public into website/website-preview/public
-cp -R ../public/img/** ./public/img/
-# TODO make DEV_IO dynamic, based on the
-# TODO name of the grandparent dir of
-# TODO the cloned website-preview folder
-DEV_IO=waypoint IS_CONTENT_PREVIEW=true ./node_modules/.bin/next
+# Run the dev-portal content-repo start script
+REPO=waypoint PREVIEW_DIR="$PREVIEW_DIR" ./scripts/content-repo-preview/start.sh
