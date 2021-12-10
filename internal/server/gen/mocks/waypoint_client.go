d9c70497a8af33492f1e0c617733d826c277aced
Brian Cain <bcain@hashicorp.com>
2021-12-10 11:14:59 -0800

    Define RunTrigger grpc method

--- internal/server/gen/mocks/waypoint_client.go
+++ internal/server/gen/mocks/waypoint_client.go
@@ -1758,6 +1758,36 @@ func (_m *WaypointClient) RestoreSnapshot(ctx context.Context, opts ...grpc.Call
 	return r0, r1
 }
 
+// RunTrigger provides a mock function with given fields: ctx, in, opts
+func (_m *WaypointClient) RunTrigger(ctx context.Context, in *gen.RunTriggerRequest, opts ...grpc.CallOption) (*gen.RunTriggerResponse, error) {
+	_va := make([]interface{}, len(opts))
+	for _i := range opts {
+		_va[_i] = opts[_i]
+	}
+	var _ca []interface{}
+	_ca = append(_ca, ctx, in)
+	_ca = append(_ca, _va...)
+	ret := _m.Called(_ca...)
+
+	var r0 *gen.RunTriggerResponse
+	if rf, ok := ret.Get(0).(func(context.Context, *gen.RunTriggerRequest, ...grpc.CallOption) *gen.RunTriggerResponse); ok {
+		r0 = rf(ctx, in, opts...)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*gen.RunTriggerResponse)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(context.Context, *gen.RunTriggerRequest, ...grpc.CallOption) error); ok {
+		r1 = rf(ctx, in, opts...)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // RunnerConfig provides a mock function with given fields: ctx, opts
 func (_m *WaypointClient) RunnerConfig(ctx context.Context, opts ...grpc.CallOption) (gen.Waypoint_RunnerConfigClient, error) {
 	_va := make([]interface{}, len(opts))
