52b404c8cbf1096e8227e024d355211206dd9d97
Sabrina Ko <60155296+sabrinako@users.noreply.github.com>
2022-01-10 10:26:43 -0500

    ui: build and release tabs tables view (#2852)
    * ui: initial build/release tables
    
    * ui: added tests for build and release tables and table row component
    
    * ui: added changelog
    
    * ui: updated tests and cleaned up styling of table views
    
    * ui: added link to deployments column

--- ui/app/routes/workspace/projects/project/app/builds.ts
+++ ui/app/routes/workspace/projects/project/app/builds.ts
@@ -1,11 +1,31 @@
 import Route from '@ember/routing/route';
 import { Model as AppRouteModel } from '../app';
 
-type Model = AppRouteModel['builds'];
+type Model = {
+  builds: AppRouteModel['builds'];
+  buildDeploymentPairs: Record<number, number>;
+};
 
 export default class Builds extends Route {
   async model(): Promise<Model> {
     let app = this.modelFor('workspace.projects.project.app') as AppRouteModel;
-    return app.builds;
+    let bdPairs = {};
+    for (let build of app.builds) {
+      let matchingDeployment = app.deployments.find((obj) => {
+        if (obj.preload && obj.preload.artifact) {
+          return obj.preload.artifact.id === build.pushedArtifact?.id;
+        }
+        return obj.artifactId === build.pushedArtifact?.id;
+      });
+
+      if (matchingDeployment) {
+        bdPairs[build.sequence] = matchingDeployment.sequence;
+      }
+    }
+
+    return {
+      builds: app.builds,
+      buildDeploymentPairs: bdPairs,
+    };
   }
 }
