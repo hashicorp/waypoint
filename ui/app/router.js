5c3e5f0eefe851ced8c92e684bb79ad2f7590b76
Greg Hoin <1416421+gregone@users.noreply.github.com>
2022-01-12 15:50:16 +0100

    Resources route (#2777)
    * setup resources route
    
    * Update resource table
    
    * Add changelog
    
    * Fine-tuning

--- ui/app/router.js
+++ ui/app/router.js
@@ -58,6 +58,7 @@ Router.map(function () {
             this.route('resource', { path: 'resources/:resource_id' });
           });
           this.route('logs');
+          this.route('resources');
           this.route('exec');
           this.route('deployments');
         });
---
+++ ui/app/routes/workspace/projects/project/app/resources.ts
@@ -0,0 +1,44 @@
+import { DeploymentExtended, ReleaseExtended } from 'waypoint/services/api';
+
+import { Model as AppRouteModel } from '../app';
+import Route from '@ember/routing/route';
+import { StatusReport } from 'waypoint-pb';
+
+type Model = ResourceMap[];
+
+interface ResourceMap {
+  resource: StatusReport.Resource.AsObject;
+  type: string;
+  source: DeploymentExtended | ReleaseExtended;
+}
+export default class Resources extends Route {
+  async model(): Promise<Model> {
+    let app = this.modelFor('workspace.projects.project.app') as AppRouteModel;
+
+    let deployments = app.deployments;
+    let releases = app.releases;
+
+    let resources: ResourceMap[] = [];
+
+    deployments.forEach((dep) => {
+      dep.statusReport?.resourcesList.forEach((resource) => {
+        resources.push({
+          resource,
+          type: 'deployment',
+          source: dep,
+        } as ResourceMap);
+      });
+    });
+    releases.forEach((rel) => {
+      rel.statusReport?.resourcesList.forEach((resource) => {
+        resources.push({
+          resource,
+          type: 'release',
+          source: rel,
+        } as ResourceMap);
+      });
+    });
+
+    return resources;
+  }
+}
