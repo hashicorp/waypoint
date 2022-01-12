0bd4eb33d308e845b43a5579d90f5759020d4e35
Bryce Kalow <bkalow@hashicorp.com>
2022-01-12 09:06:08 -0600

    rename scripts and add a make command

--- website/scripts/website-build.sh
+++ website/scripts/website-build.sh
@@ -3,9 +3,10 @@ PREVIEW_DIR=website-preview
 # Clone the dev-portal project, if needed
 if [ ! -d "$PREVIEW_DIR" ]; then
     echo "⏳ Cloning the dev-portal repo, this might take a while..."
-    git clone --depth=1 https://github.com/hashicorp/dev-portal.git "$PREVIEW_DIR"
+    git clone --depth 1 https://github.com/hashicorp/dev-portal.git "$PREVIEW_DIR"
 fi
 # cd into the dev-portal project
 cd "$PREVIEW_DIR"
+
 # Run the dev-portal content-repo start script
-REPO=waypoint PREVIEW_DIR="$PREVIEW_DIR" ./scripts/content-repo-preview/start.sh
+REPO=waypoint DEV_IO=waypoint IS_CONTENT_PREVIEW=true npm run build:deploy-preview
--- website/scripts/website-start.sh
+++ website/scripts/website-start.sh
@@ -3,10 +3,9 @@ PREVIEW_DIR=website-preview
 # Clone the dev-portal project, if needed
 if [ ! -d "$PREVIEW_DIR" ]; then
     echo "⏳ Cloning the dev-portal repo, this might take a while..."
-    git clone --branch brk.feat/build-script --depth 1 https://github.com/hashicorp/dev-portal.git "$PREVIEW_DIR"
+    git clone --branch brk.feat/local-preview-script-tweaks --depth=1 https://github.com/hashicorp/dev-portal.git "$PREVIEW_DIR"
 fi
 # cd into the dev-portal project
 cd "$PREVIEW_DIR"
-
 # Run the dev-portal content-repo start script
-REPO=waypoint DEV_IO=waypoint IS_CONTENT_PREVIEW=true npm run build:deploy-preview
+REPO=waypoint PREVIEW_DIR="$PREVIEW_DIR" npm run start:local-preview
