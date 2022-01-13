8b8f860733269c2c94e100a62c6c7831516fd89c
Jamie White <jamie@jgwhite.co.uk>
2022-01-13 17:35:46 +0100

    ui/tests: pretend the platform is Macintosh

--- ui/tests/helpers/xterm.ts
+++ ui/tests/helpers/xterm.ts
@@ -1,5 +1,17 @@
 import { triggerEvent, triggerKeyEvent, focus } from '@ember/test-helpers';
 
+// At the time of writing, xterm.js only implements the Macintosh “select all”
+// keyboard shortcut (cmd+a), not the Linux and Windows equivalent (ctrl+a).
+// What follows is a dreadful hack to make `getTerminalText` work in Linux CI.
+// Note: this needs to happen before xterm is required, so you’ll find an
+// import in `tests/test-helper.js`.
+Object.defineProperty(navigator, 'platform', {
+  value: 'Macintosh',
+  configurable: true,
+  enumerable: true,
+  writable: false,
+});
+
 /**
  * Returns the text from an xterm.js terminal element.
  *
@@ -10,9 +22,8 @@ import { triggerEvent, triggerKeyEvent, focus } from '@ember/test-helpers';
 export async function getTerminalText(): Promise<string> {
   await focus('.xterm-helper-textarea');
 
-  // Trigger cmd+a and ctrl+a to select all
+  // Trigger cmd+a to select all
   await triggerKeyEvent('.xterm-helper-textarea', 'keydown', 65, { metaKey: true });
-  await triggerKeyEvent('.xterm-helper-textarea', 'keydown', 65, { ctrlKey: true });
 
   let clipboardData = new DataTransfer();
   await triggerEvent('.xterm', 'copy', { clipboardData });
