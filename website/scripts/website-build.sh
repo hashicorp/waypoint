546138020bf650b419b727f954ea26ab10d43876
Bryce Kalow <bkalow@hashicorp.com>
2022-01-18 17:15:39 -0600

    update start script and remove branch specification used for testing

--- website/scripts/website-build.sh
+++ website/scripts/website-build.sh
@@ -3,7 +3,7 @@ PREVIEW_DIR=website-preview
 # Clone the dev-portal project, if needed
 if [ ! -d "$PREVIEW_DIR" ]; then
     echo "⏳ Cloning the dev-portal repo, this might take a while..."
-    git clone --depth 1 https://github.com/hashicorp/dev-portal.git "$PREVIEW_DIR"
+    git clone --depth=1 https://github.com/hashicorp/dev-portal.git "$PREVIEW_DIR"
 fi
 # cd into the dev-portal project
 cd "$PREVIEW_DIR"
