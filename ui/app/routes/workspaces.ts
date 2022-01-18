79db362c54a7cc2e24f483275137fea4d1ebccd0
Jamie White <jamie@jgwhite.co.uk>
2022-01-18 12:08:26 +0000

    backport of commit 50677bf6cb5bda96b60c65b14287fd38c8e2ac75

--- ui/app/routes/workspaces.ts
+++ ui/app/routes/workspaces.ts
@@ -1,12 +1,32 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 import ApiService from 'waypoint/services/api';
+import SessionService from 'ember-simple-auth/services/session';
 
 export default class Workspaces extends Route {
   @service api!: ApiService;
+  @service session!: SessionService;
 
-  redirect(): void {
-    // For now, we just support the default workspace
+  async redirect(): Promise<void> {
+    let workspaces = await this.api.listWorkspaces();
+    let workspaceNames = workspaces.map((w) => w.name).sort();
+    let storedWorkspaceName = this.session.data.workspace;
+
+    if (storedWorkspaceName && workspaceNames.includes(storedWorkspaceName)) {
+      this.transitionTo('workspace', storedWorkspaceName);
+      return;
+    }
+
+    if (workspaceNames.includes('default')) {
+      this.transitionTo('workspace', 'default');
+      return;
+    }
+
+    if (workspaceNames.length) {
+      this.transitionTo('workspace', workspaceNames[0]);
+      return;
+    }
+
     this.transitionTo('workspace', 'default');
   }
 }
