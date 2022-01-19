a8212c5f8197f247dd20f1c38d4ef0f82081e68d
Brian Cain <bcain@hashicorp.com>
2022-01-19 01:15:04 +0000

    backport of commit 84be9884a2390b7a14712cc645769496436f3826

--- internal/server/http.go
+++ internal/server/http.go
@@ -8,6 +8,9 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/hashicorp/go-hclog"
+	"google.golang.org/grpc"
+
 	assetfs "github.com/elazarl/go-bindata-assetfs"
 	"github.com/gorilla/mux"
 	"github.com/hashicorp/waypoint/internal/server/gen"
@@ -50,6 +53,8 @@ func newHttpServer(grpcServer *grpc.Server, ln net.Listener, opts *options) *htt
 	// Create our full router
 	r := mux.NewRouter()
 	r.HandleFunc("/v1/exec", httpapi.HandleExec(grpcAddr, true))
+	// TODO(briancain): query params for job blocking and streaming
+	r.HandleFunc("/v1/trigger/{id:[a-zA-Z0-9]+}", httpapi.HandleTrigger(grpcAddr))
 	r.PathPrefix("/grpc").Handler(grpcWrapped)
 	r.PathPrefix("/").Handler(uifs)
 
