769fd9523f7265f1978f698778bf7a9040eee8c0
Brian Cain <bcain@hashicorp.com>
2022-01-25 16:19:43 -0800

    internal/server: Support multi-job concurrent streaming via http
    This commit updates the run trigger http func to stream all job output
    that's running concurrently back to the requester.

--- internal/server/httpapi/trigger_test.go
+++ internal/server/httpapi/trigger_test.go
@@ -1,9 +1,26 @@
 package httpapi
 
 import (
+	"net/http/httptest"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestHandleTrigger(t *testing.T) {
-	// TODO(briancain): write me
+	//ctx := context.Background()
+
+	// Get our gRPC server
+	impl := &execImpl{}
+	addr := testServer(t, impl)
+
+	// Start up our test HTTP server
+	httpServer := httptest.NewServer(HandleTrigger(addr, false))
+	defer httpServer.Close()
+
+	t.Run("a request with a non-valid trigger", func(t *testing.T) {
+		require := require.New(t)
+
+		require.Equal(1, 1)
+	})
 }
