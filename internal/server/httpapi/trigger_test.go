04da5e8f6fbfe024389adc78d64bb656b8f08da6
Brian Cain <bcain@hashicorp.com>
2022-02-01 14:54:58 -0800

    wip: Attempt to write unit test for HandleTrigger

--- internal/server/httpapi/trigger_test.go
+++ internal/server/httpapi/trigger_test.go
@@ -22,8 +22,21 @@ func TestHandleTrigger(t *testing.T) {
 
 	t.Run("a request with a non-valid trigger", func(t *testing.T) {
 		require := require.New(t)
-
 		require.Equal(1, 1)
+
+		//request := httptest.NewRequest("GET", "/v1/trigger/123", nil)
+		//responseRecorder := httptest.NewRecorder()
+
+		//triggerHandler := HandleTrigger(httpServer.URL, false)
+		//triggerHandler.ServeHTTP(responseRecorder, request)
+
+		//if responseRecorder.Code != 404 {
+		//	t.Errorf("Want status '%d', got '%d'", 404, responseRecorder.Code)
+		//}
+
+		//if strings.TrimSpace(responseRecorder.Body.String()) != "404" {
+		//	t.Errorf("Want '%s', got '%s'", "404", responseRecorder.Body)
+		//}
 	})
 }
 
