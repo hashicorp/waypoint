3f6a0f9f4f66fe51e479d6d538774e17d87f59b5
Izaak Lauer <8404559+izaaklauer@users.noreply.github.com>
2022-02-03 16:25:50 -0500

    using the swagger plugin to generate a swagger spec from the proto definitions

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1,5 +1,6 @@
 package server
 
-//go:generate sh -c "protoc -I../../thirdparty/proto/api-common-protos -I ../.. ../../pkg/server/proto/server.proto --go_out=plugins=grpc:../.. --go-json_out=../.."
+//go:generate sh -c "protoc -I../../thirdparty/proto/api-common-protos -I ../.. ../../pkg/server/proto/server.proto --go_out=plugins=grpc:../.. --go-json_out=../.. --swagger_out=../.." 
+//go:generate mv ./proto/server.swagger.json ./gen/
 //go:generate mv ./proto/server.pb.json.go ./gen
 //go:generate mockery -all -case underscore -dir ./gen -output ./gen/mocks
