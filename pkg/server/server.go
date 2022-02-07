10e3709dc13219570013cce79f50fdb1c2894910
Izaak Lauer <8404559+izaaklauer@users.noreply.github.com>
2022-02-07 11:40:26 -0500

    Using better protoc swagger_out options

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1,6 +1,6 @@
 package server
 
-//go:generate sh -c "protoc -I../../thirdparty/proto/api-common-protos -I ../.. ../../pkg/server/proto/server.proto --go_out=plugins=grpc:../.. --go-json_out=../.. --swagger_out=../.."
+//go:generate sh -c "protoc -I../../thirdparty/proto/api-common-protos -I ../.. ../../pkg/server/proto/server.proto --go_out=plugins=grpc:../.. --go-json_out=../.. --swagger_out=logtostderr=true,fqn_for_swagger_name=true:../.."
 //go:generate mv ./proto/server.swagger.json ./gen/
 //go:generate mv ./proto/server.pb.json.go ./gen
 //go:generate mockery -all -case underscore -dir ./gen -output ./gen/mocks
