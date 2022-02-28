e0966313bb473e42b69f7859d285c3bc723d5780
Izaak Lauer <8404559+izaaklauer@users.noreply.github.com>
2022-02-28 09:27:21 -0500

    Updating docs

--- internal/cli/context_create.go
+++ internal/cli/context_create.go
@@ -61,7 +61,7 @@ func (c *ContextCreateCommand) Flags() *flag.Sets {
 			Name:    "set-default",
 			Target:  &c.flagSetDefault,
 			Default: true,
-			Usage:   "Set this context as the new default for the CLI.",
+			Usage:   "Set this context as the new default for the CLI. Defaults to true.",
 		})
 		f.StringVar(&flag.StringVar{
 			Name:   "server-addr",
