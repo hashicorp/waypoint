8b029faaa3023ba2d89a00a8a207474220d3098a
Bryce Kalow <bkalow@hashicorp.com>
2022-03-03 16:17:07 -0600

    Testing deploy preview on a new branch

--- website/scripts/website-build.sh
+++ website/scripts/website-build.sh
@@ -3,10 +3,10 @@ PREVIEW_DIR=website-preview
 # Clone the dev-portal project, if needed
 if [ ! -d "$PREVIEW_DIR" ]; then
     echo "⏳ Cloning the dev-portal repo, this might take a while..."
-    git clone --depth=1 https://github.com/hashicorp/dev-portal.git "$PREVIEW_DIR"
+    git clone --branch brk.feat/project-preview-config --depth=1 https://github.com/hashicorp/dev-portal.git "$PREVIEW_DIR"
 fi
 # cd into the dev-portal project
 cd "$PREVIEW_DIR"
 
 # Run the dev-portal content-repo start script
-REPO=waypoint DEV_IO=waypoint IS_CONTENT_PREVIEW=true npm run build:deploy-preview
+REPO=waypoint DEV_IO=waypoint IS_CONTENT_PREVIEW=true HASHI_ENV=project-preview npm run build:deploy-preview
