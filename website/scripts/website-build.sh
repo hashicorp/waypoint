7c71ac2f4ddea5f0549eb8efc84d48eb14958f4e
Bryce Kalow <bkalow@hashicorp.com>
2022-03-07 13:26:19 -0600

    test branch with node_modules cache adjustments

--- website/scripts/website-build.sh
+++ website/scripts/website-build.sh
@@ -3,7 +3,7 @@ PREVIEW_DIR=website-preview
 # Clone the dev-portal project, if needed
 if [ ! -d "$PREVIEW_DIR" ]; then
     echo "⏳ Cloning the dev-portal repo, this might take a while..."
-    git clone --branch brk.feat/project-preview-config --depth=1 https://github.com/hashicorp/dev-portal.git "$PREVIEW_DIR"
+    git clone --branch brk.feat/io-preview-cache --depth=1 https://github.com/hashicorp/dev-portal.git "$PREVIEW_DIR"
 fi
 # cd into the dev-portal project
 cd "$PREVIEW_DIR"
