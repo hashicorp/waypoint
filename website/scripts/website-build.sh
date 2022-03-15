39412156d306e2982041c46c0d658d6ddc66539d
Bryce Kalow <bkalow@hashicorp.com>
2022-03-15 16:57:26 -0500

    adjust build script for reproduction

--- website/scripts/website-build.sh
+++ website/scripts/website-build.sh
@@ -1,12 +1,31 @@
-# Set the subdirectory name for the dev-portal app
+# Repo which we are cloning and executing npm run build:deploy-preview within
+REPO_TO_CLONE=dev-portal
+# Set the subdirectory name for the base project
 PREVIEW_DIR=website-preview
-# Clone the dev-portal project, if needed
-if [ ! -d "$PREVIEW_DIR" ]; then
-    echo "⏳ Cloning the dev-portal repo, this might take a while..."
-    git clone --branch brk.feat/io-preview-cache --depth=1 https://github.com/hashicorp/dev-portal.git "$PREVIEW_DIR"
+# The directory we want to clone the project into
+CLONE_DIR=website-preview
+# The product for which we are building the deploy preview
+PRODUCT=waypoint
+
+from_cache=false
+
+if [ -d "$PREVIEW_DIR" ]; then
+  echo "$PREVIEW_DIR found"
+  CLONE_DIR="$PREVIEW_DIR-tmp"
+  from_cache=true
 fi
-# cd into the dev-portal project
+
+# Clone the base project, if needed
+echo "⏳ Cloning the $REPO_TO_CLONE repo, this might take a while..."
+git clone --depth=1 --branch=brk.feat/io-preview-cache "https://github.com/hashicorp/$REPO_TO_CLONE.git" "$CLONE_DIR"
+
+if [ "$from_cache" = true ]; then
+  echo "Setting up $PREVIEW_DIR"
+  cp -R "./$CLONE_DIR/." "./$PREVIEW_DIR"
+fi
+
+# cd into the preview directory project
 cd "$PREVIEW_DIR"
 
-# Run the dev-portal content-repo start script
-REPO=waypoint DEV_IO=waypoint IS_CONTENT_PREVIEW=true HASHI_ENV=preview npm run build:deploy-preview
+# Run the build:deploy-preview start script
+REPO=$PRODUCT DEV_IO=$PRODUCT IS_CONTENT_PREVIEW=true HASHI_ENV=preview npm run build:deploy-preview
