41a96f76381e2256e3234893a044d11345c83d72
Izaak Lauer <8404559+izaaklauer@users.noreply.github.com>
2022-03-16 14:31:37 -0400

    More comments and doc

---
+++ pkg/server/handlers/doc.go
@@ -0,0 +1,6 @@
+// Package handlers exports a reference implementation of the waypoint
+// protobuf service handlers.
+//
+// Usage of these handlers requires an implementation of
+// handlers.Service
+package handlers
--- pkg/server/handlers/service.go
+++ pkg/server/handlers/service.go
@@ -7,16 +7,25 @@ import (
 	"github.com/hashicorp/waypoint/pkg/serverstate"
 )
 
+// Service is an implementation of pb.WaypointServer capable of
+// using shared service handlers in this package.
 type Service interface {
 	pb.WaypointServer
 
+	// State returns something capable of persisting
+	// waypoint's state (i.e. a boltdb-backed state struct)
 	State(ctx context.Context) serverstate.Interface
 
 	// SuperUser - if true, forces all API actions to behave as if a superuser
 	// made them. This is usually turned on for local mode only.
 	SuperUser() bool
 
-	DecodeId(id string) (decodedId string, err error)
+	// DecodeId takes a string that contains an ID (likely created
+	// with EncodeId), and returns only the waypoint-relevant ID.
+	DecodeId(encodedId string) (id string, err error)
 
-	EncodeId(ctx context.Context, id string) string
+	// EncodeId takes a waypoint ID (user id, runner id, etc.),
+	// uses the provided context to encode additional metadata
+	// (if present), and returns an ID that can be decoded by DecodeId.
+	EncodeId(ctx context.Context, id string) (encodedId string)
 }
