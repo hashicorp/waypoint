fbda1f559f5972d40b70eaadf57a8d4be2bdf376
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2022-03-21 09:11:46 -0700

    internal/runner: test more down scenarios

--- internal/runner/accept_test.go
+++ internal/runner/accept_test.go
@@ -208,6 +208,90 @@ func TestRunnerAccept_serverDown(t *testing.T) {
 	require.Equal(pb.Job_SUCCESS, job.State)
 }
 
+// Test how accept behaves when the server is down while waiting for
+// assignment.
+func TestRunnerAccept_serverDownAssign(t *testing.T) {
+	require := require.New(t)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// New context that will be used for server restart.
+	serverCtx, serverCancel := context.WithCancel(context.Background())
+	defer serverCancel()
+
+	// Setup the server
+	restartCh := make(chan struct{})
+	impl := singleprocess.TestImpl(t)
+	client := serverpkg.TestServer(t, impl,
+		serverpkg.TestWithContext(serverCtx),
+		serverpkg.TestWithRestart(restartCh),
+	)
+
+	// Setup our runner
+	runner := TestRunner(t, WithClient(client))
+	require.NoError(runner.Start(ctx))
+
+	// Initialize our app
+	singleprocess.TestApp(t, client, serverptypes.TestJobNew(t, nil).Application)
+
+	// Start accept
+	errCh := make(chan error, 1)
+	go func() {
+		errCh <- runner.Accept(ctx)
+	}()
+
+	// The runner should not error
+	select {
+	case <-time.After(100 * time.Millisecond):
+		// Good
+
+	case <-errCh:
+		t.Fatal("runner should not return")
+	}
+
+	// Shut it down
+	serverCancel()
+	serverCtx, serverCancel = context.WithCancel(context.Background())
+	defer serverCancel()
+
+	// Wait to get an unavailable error so we know the server is down
+	require.Eventually(func() bool {
+		_, err := client.GetRunner(ctx, &pb.GetRunnerRequest{RunnerId: runner.Id()})
+		return status.Code(err) == codes.Unavailable
+	}, 5*time.Second, 10*time.Millisecond)
+
+	// Restart
+	restartCh <- struct{}{}
+
+	// Queue a job
+	var jobId string
+	require.Eventually(func() bool {
+		queueResp, err := client.QueueJob(ctx, &pb.QueueJobRequest{
+			Job: serverptypes.TestJobNew(t, nil),
+		})
+		if err != nil {
+			return false
+		}
+
+		jobId = queueResp.JobId
+		return true
+	}, 5*time.Second, 10*time.Millisecond)
+
+	// Accept should return
+	select {
+	case err := <-errCh:
+		require.NoError(err)
+
+	case <-time.After(5 * time.Second):
+		t.Fatal("accept never returned")
+	}
+
+	// Verify that the job is completed
+	job, err := client.GetJob(ctx, &pb.GetJobRequest{JobId: jobId})
+	require.NoError(err)
+	require.Equal(pb.Job_SUCCESS, job.State)
+}
+
 func TestRunnerAccept_closeCancelesAccept(t *testing.T) {
 	require := require.New(t)
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
