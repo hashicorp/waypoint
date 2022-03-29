92d43cb9210a53cc1ac783f89b739ef097cf891a
Jamie White <jamie@jgwhite.co.uk>
2022-03-29 13:37:45 +0200

    wip! ui/mirage: add job stream simulation

--- ui/mirage/services/job.ts
+++ ui/mirage/services/job.ts
@@ -1,11 +1,37 @@
 import { Response } from 'miragejs';
 import { RouteHandler } from '../types';
 import { GetJobStreamResponse } from 'waypoint-pb';
+import { dateToTimestamp } from '../utils';
 
 export function stream(this: RouteHandler): Response {
   let result = new GetJobStreamResponse();
+  let terminal = new GetJobStreamResponse.Terminal();
+
+  terminal.setEventsList([
+    event('Deploying v1...'),
+    event('Deploying with Kubernetes...'),
+    event('Creating resources...'),
+    event('Having some cake...'),
+    event('This cake is delicious...'),
+    event('Would you like some?'),
+    event('OK, suit yourself'),
+  ]);
+
+  result.setTerminal(terminal);
 
   // TODO(jgwhite): Implement GetJobStream handler
 
   return this.serialize(result, 'application');
+}
+
+function event(msg) {
+  let result = new GetJobStreamResponse.Terminal.Event();
+  let line = new GetJobStreamResponse.Terminal.Event.Line();
+
+  line.setMsg(msg);
+
+  result.setLine(line);
+  result.setTimestamp(dateToTimestamp(new Date()));
+
+  return result;
 }
