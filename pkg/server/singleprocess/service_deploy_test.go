d511c3e2f2394e8b9390d0ef3cf2d0ea7bb11b18
Izaak Lauer <8404559+izaaklauer@users.noreply.github.com>
2022-04-29 16:04:22 -0400

    Re-adding TestServiceDeployment_URLService
    It was removed accidentally

---
+++ pkg/server/singleprocess/service_deploy_test.go
@@ -0,0 +1,64 @@
+package singleprocess
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/hashicorp/waypoint/pkg/server"
+	pb "github.com/hashicorp/waypoint/pkg/server/gen"
+	serverptypes "github.com/hashicorp/waypoint/pkg/server/ptypes"
+)
+
+func TestServiceDeployment_URLService(t *testing.T) {
+	ctx := context.Background()
+
+	// Create our server
+	impl, err := New(WithDB(testDB(t)), TestWithURLService(t, nil))
+	require.NoError(t, err)
+	client := server.TestServer(t, impl)
+
+	// Simplify writing tests
+	type Req = pb.UpsertDeploymentRequest
+
+	t.Run("default hostname", func(t *testing.T) {
+		require := require.New(t)
+
+		deploy := serverptypes.TestValidDeployment(t, nil)
+
+		// Create, should get an ID back
+		resp, err := client.UpsertDeployment(ctx, &Req{
+			Deployment: deploy,
+		})
+		require.NoError(err)
+		require.NotNil(resp)
+		result := resp.Deployment
+		require.NotEmpty(result.Id)
+
+		// Should have the hostname
+		{
+			resp, err := client.ListHostnames(ctx, &pb.ListHostnamesRequest{})
+			require.NoError(err)
+			require.Len(resp.Hostnames, 1)
+		}
+
+		// Let's write some data
+		result.Status = server.NewStatus(pb.Status_RUNNING)
+		resp, err = client.UpsertDeployment(ctx, &Req{
+			Deployment: result,
+		})
+		require.NoError(err)
+		require.NotNil(resp)
+		result = resp.Deployment
+		require.NotNil(result.Status)
+		require.Equal(pb.Status_RUNNING, result.Status.State)
+
+		// Should have the hostname
+		{
+			resp, err := client.ListHostnames(ctx, &pb.ListHostnamesRequest{})
+			require.NoError(err)
+			require.Len(resp.Hostnames, 1)
+		}
+	})
+}
