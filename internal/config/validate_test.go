b529b1de655b12b30e1933fb44a765e5d8d784d7
Brian Cain <bcain@hashicorp.com>
2022-05-05 09:53:14 -0700

    internal/config: Validate pipeline stanzas
    This commit adds some validation that pipelines must have at least one
    step stanza, and includes a test to validate with example hcl configs.

--- internal/config/validate_test.go
+++ internal/config/validate_test.go
@@ -51,3 +51,41 @@ func TestConfigValidate(t *testing.T) {
 		})
 	}
 }
+
+func TestConfigValidatePipelines(t *testing.T) {
+	cases := []struct {
+		File string
+		Err  string
+	}{
+		{
+			"pipeline_step.hcl",
+			"",
+		},
+		{
+			"pipeline_multi_step.hcl",
+			"",
+		},
+		{
+			"pipeline_no_step.hcl",
+			"'step' stanza",
+		},
+	}
+
+	for _, tt := range cases {
+		t.Run(tt.File, func(t *testing.T) {
+			require := require.New(t)
+
+			cfg, err := Load(filepath.Join("testdata", "pipelines", tt.File), nil)
+			require.NoError(err)
+
+			err = cfg.Validate()
+			if tt.Err == "" {
+				require.NoError(err)
+				return
+			}
+
+			require.Error(err)
+			require.Contains(err.Error(), tt.Err)
+		})
+	}
+}
