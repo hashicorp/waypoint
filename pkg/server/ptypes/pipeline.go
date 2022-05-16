42850f04fbd9ad9058b4149f9ae84ff6917d4b7b
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2022-05-16 13:52:48 -0700

    pkg/server: add PipelienSteps

--- pkg/server/ptypes/pipeline.go
+++ pkg/server/ptypes/pipeline.go
@@ -14,6 +14,12 @@ import (
 	pb "github.com/hashicorp/waypoint/pkg/server/gen"
 )
 
+// PipelineGraph returns the graph of steps for a pipeline. The graph
+// vertices are the pipeline step names.
+func PipelineGraph(v *pb.Pipeline) (*graph.Graph, error) {
+	return pipelineGraph(v.Steps)
+}
+
 // TestPipeline returns a valid user for tests.
 func TestPipeline(t testing.T, src *pb.Pipeline) *pb.Pipeline {
 	t.Helper()
@@ -152,6 +158,11 @@ func stepSingleRoot(v interface{}) error {
 // builds and validates the step graph.
 func stepGraph(v interface{}) error {
 	steps := v.(map[string]*pb.Pipeline_Step)
+	_, err := pipelineGraph(steps)
+	return err
+}
+
+func pipelineGraph(steps map[string]*pb.Pipeline_Step) (*graph.Graph, error) {
 	var stepGraph graph.Graph
 	for _, step := range steps {
 		// Add our job
@@ -163,15 +174,15 @@ func stepGraph(v interface{}) error {
 			stepGraph.AddEdge(dep, step.Name)
 
 			if _, ok := steps[dep]; !ok {
-				return fmt.Errorf(
+				return nil, fmt.Errorf(
 					"step %q depends on non-existent step %q", step, dep)
 			}
 		}
 	}
 	if cycles := stepGraph.Cycles(); len(cycles) > 0 {
-		return fmt.Errorf(
+		return nil, fmt.Errorf(
 			"step dependencies contain one or more cycles: %s", cycles)
 	}
 
-	return nil
+	return &stepGraph, nil
 }
