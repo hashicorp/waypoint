f3017e3907d693a9f3e51d5f8dd71e98dee7fc90
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2022-05-16 14:58:52 -0700

    server: RunPipeline

--- pkg/server/singleprocess/service_pipeline.go
+++ pkg/server/singleprocess/service_pipeline.go
@@ -3,6 +3,9 @@ package singleprocess
 import (
 	"context"
 
+	"google.golang.org/protobuf/proto"
+
+	"github.com/hashicorp/waypoint/pkg/server"
 	pb "github.com/hashicorp/waypoint/pkg/server/gen"
 	serverptypes "github.com/hashicorp/waypoint/pkg/server/ptypes"
 )
@@ -21,4 +24,67 @@ func (s *Service) UpsertPipeline(
 	}
 
 	return &pb.UpsertPipelineResponse{Pipeline: result}, nil
+}
+
+func (s *Service) RunPipeline(
+	ctx context.Context,
+	req *pb.RunPipelineRequest,
+) (*pb.RunPipelineResponse, error) {
+	if err := serverptypes.ValidateRunPipelineRequest(req); err != nil {
+		return nil, err
+	}
+
+	// Get the pipeline we should execute
+	pipeline, err := s.state(ctx).PipelineGet(req.Pipeline)
+	if err != nil {
+		return nil, err
+	}
+
+	// Generate job IDs for each of the steps. We need to know the IDs in
+	// advance to setup the dependency chain.
+	stepIds := map[string]string{}
+	for name := range pipeline.Steps {
+		stepIds[name], err = server.Id()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// Generate the jobs for each of the steps
+	var stepJobs []*pb.QueueJobRequest
+	for name, step := range pipeline.Steps {
+		var dependsOn []string
+		for _, dep := range step.DependsOn {
+			dependsOn = append(dependsOn, stepIds[dep])
+		}
+
+		job := proto.Clone(req.JobTemplate).(*pb.Job)
+		job.Id = stepIds[name]
+		job.DependsOn = append(job.DependsOn, dependsOn...)
+		job.Operation = &pb.Job_PipelineStep{
+			PipelineStep: &pb.Job_PipelineStepOp{
+				Step: step,
+			},
+		}
+
+		stepJobs = append(stepJobs, &pb.QueueJobRequest{
+			Job: job,
+		})
+	}
+
+	// Get the graph for the steps so we can get the root. We enforce a
+	// single root so the root is always the first step.
+	stepGraph, err := serverptypes.PipelineGraph(pipeline)
+	if err != nil {
+		return nil, err
+	}
+	order := stepGraph.KahnSort()
+	rootId := stepIds[order[0].(string)]
+
+	// Queue all the jobs atomically
+	if _, err := s.queueJobMulti(ctx, stepJobs); err != nil {
+		return nil, err
+	}
+
+	return &pb.RunPipelineResponse{JobId: rootId}, nil
 }
