91f487a40c9fa2ccaee0f82b2bcd4a6f4c617a8d
Brian Cain <bcain@hashicorp.com>
2022-05-18 16:19:50 -0700

    internal/{server,cli}: Update `waypoint config sync` to sync pipeline configs

--- internal/core/pipeline_config.go
+++ internal/core/pipeline_config.go
@@ -4,6 +4,8 @@ import (
 	"context"
 
 	pb "github.com/hashicorp/waypoint/pkg/server/gen"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 // ConfigSync will evaluate the current hcl config for a given Pipeline and
@@ -21,7 +23,37 @@ func (p *Pipeline) ConfigSync(ctx context.Context) error {
 	// TODO(briancain): do we need a Multi upsert?
 	p.logger.Debug("syncing pipeline config")
 	for _, pipeline := range pipelines {
-		_, err := p.client.UpsertPipeline(ctx, &pb.UpsertPipelineRequest{
+		pipeOwner, ok := pipeline.Owner.(*pb.Pipeline_Project)
+		if !ok {
+			return status.Error(codes.FailedPrecondition,
+				"failed to determine pipeline owner. Only expecting pipeline owner to be a Pipeline_Project type.")
+		}
+
+		// Look up if this pipeline already exists by Pipeline Owner (project and pipeline name)
+		// If so, use the existing id to upsert into an existing config. Otherwise
+		// create a new pipeline entry.
+		pipeResp, err := p.client.GetPipeline(ctx, &pb.GetPipelineRequest{
+			Pipeline: &pb.Ref_Pipeline{
+				Ref: &pb.Ref_Pipeline_Owner{
+					Owner: &pb.Ref_PipelineOwner{
+						Project:      pipeOwner.Project,
+						PipelineName: pipeline.Name,
+					},
+				},
+			},
+		})
+		if err != nil {
+			// If not found, that's fine, we're going to upsert it
+			if status.Code(err) != codes.NotFound {
+				return err
+			}
+		}
+		if pipeResp != nil {
+			pipeline.Id = pipeResp.Pipeline.Id
+			p.logger.Trace("existing pipeline already in db, using existing id", "pipeline_id", pipeline.Id)
+		}
+
+		_, err = p.client.UpsertPipeline(ctx, &pb.UpsertPipelineRequest{
 			Pipeline: pipeline,
 		})
 		if err != nil {
