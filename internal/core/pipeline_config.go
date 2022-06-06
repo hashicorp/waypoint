2d4beee23f228a1600ab191e0c8e44a44c0c7f70
Brian Cain <bcain@hashicorp.com>
2022-06-06 16:10:47 -0700

    internal/core: Look up pipeline id on upsert on server
    Prior to this commit, the config sync frontend would look up if a
    pipeline already existed by Owner and Name to see if it should reuse the
    pipeline ID when a config sync happens. This was to prevent duplicate
    pipelines being upserted with separate Ids. This commit changes that by
    pushing back the pipeline lookup into the db side.

--- internal/core/pipeline_config.go
+++ internal/core/pipeline_config.go
@@ -4,8 +4,6 @@ import (
 	"context"
 
 	pb "github.com/hashicorp/waypoint/pkg/server/gen"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
 
 // ConfigSync will evaluate the current hcl config for a given Pipeline and
@@ -22,38 +20,6 @@ func (p *Pipeline) ConfigSync(ctx context.Context) error {
 
 	p.logger.Debug("syncing pipeline config")
 	for _, pipeline := range pipelines {
-		pipeOwner, ok := pipeline.Owner.(*pb.Pipeline_Project)
-		if !ok {
-			return status.Error(codes.FailedPrecondition,
-				"failed to determine pipeline owner. Only expecting pipeline owner to be a Pipeline_Project type.")
-		}
-
-		p.logger.Trace("looking up pipeline by owner and name", "owner", pipeOwner.Project, "name", pipeline.Name)
-
-		// Look up if this pipeline already exists by Pipeline Owner (project and pipeline name)
-		// If so, use the existing id to upsert into an existing config. Otherwise
-		// create a new pipeline entry.
-		pipeResp, err := p.client.GetPipeline(ctx, &pb.GetPipelineRequest{
-			Pipeline: &pb.Ref_Pipeline{
-				Ref: &pb.Ref_Pipeline_Owner{
-					Owner: &pb.Ref_PipelineOwner{
-						Project:      pipeOwner.Project,
-						PipelineName: pipeline.Name,
-					},
-				},
-			},
-		})
-		if err != nil {
-			// If not found, that's fine, we're going to upsert it
-			if status.Code(err) != codes.NotFound {
-				return err
-			}
-		}
-		if pipeResp != nil {
-			pipeline.Id = pipeResp.Pipeline.Id
-			p.logger.Trace("existing pipeline already in db, using existing id", "pipeline_id", pipeline.Id)
-		}
-
 		// TODO(briancain): We might want a multi-upsert for pipelines if this
 		// request gets out of hand.
 		_, err = p.client.UpsertPipeline(ctx, &pb.UpsertPipelineRequest{
