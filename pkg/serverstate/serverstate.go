232a73d1be29aa3e3c08315685a5913692a6b0e2
Brian Cain <bcain@hashicorp.com>
2022-06-15 15:01:28 -0700

    internal/server: Set expiry time on runner accept
    Prior to this commit, Waypoint server only expected queued jobs to wait
    for 60 seconds before a runner accepted the job and began executing it
    from its creation time. With the introduction of pipelines, this no
    longer makes sense because a pipeline will queue all jobs in a pipeline
    right away. This means downstream jobs could be waiting around to be
    executing for a while, much longer than 1 minute. This is expected.
    
    Given that, this commit updates the server behavior to set a jobs expiry
    time to something much longer, 60 minutes, and then when the runner goes
    to accept the job, we update the expiry time to the original expected
    behavior of 60 seconds, because if the runner has accepted a job to work
    on, it should run that job fairly quickly like the original intended
    behavior.

--- pkg/serverstate/serverstate.go
+++ pkg/serverstate/serverstate.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/hashicorp/go-memdb"
+	"google.golang.org/protobuf/types/known/timestamppb"
 
 	pb "github.com/hashicorp/waypoint/pkg/server/gen"
 )
@@ -162,6 +163,7 @@ type Interface interface {
 	JobAssignForRunner(context.Context, *pb.Runner) (*Job, error)
 	JobAck(string, bool) (*Job, error)
 	JobUpdateRef(string, *pb.Job_DataSource_Ref) error
+	JobUpdateExpiry(string, *timestamppb.Timestamp) error
 	JobUpdate(string, func(*pb.Job) error) error
 	JobComplete(string, *pb.Job_Result, error) error
 	JobCancel(string, bool) error
