c8912b171e8060249c1d3516424009156cbb1ec0
Joseph Rajewski <joe.rajewski@hashicorp.com>
2022-07-12 14:49:16 +0000

    backport of commit 4a17dcfa74ad8581010c8503d1e747133f5219b5

--- builtin/nomad/jobspec/plugin.go
+++ builtin/nomad/jobspec/plugin.go
@@ -9,5 +9,5 @@ import (
 // Options are the SDK options to use for instantiation for
 // the Nomad plugin.
 var Options = []sdk.Option{
-	sdk.WithComponents(&Platform{}, &Releaser{}),
+	sdk.WithComponents(&Platform{}),
 }
