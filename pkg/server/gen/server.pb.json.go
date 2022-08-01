36b7e569f66bbfa521a0a33b54528813e5faff0b
Joseph Rajewski <joe.rajewski@hashicorp.com>
2022-08-01 15:34:46 -0400

    Update server proto.
    The DestroyProjectReponse message is removed, and the DestroyProject RPC now returns an empty protobuf message. DestroyProjectRequest is updated to no longer include the option for destroying resources, which has instead been moved to a new field in DestroyProjectOp.

--- pkg/server/gen/server.pb.json.go
+++ pkg/server/gen/server.pb.json.go
@@ -3736,22 +3736,6 @@ func (msg *DestroyProjectRequest) UnmarshalJSON(b []byte) error {
 }
 
 // MarshalJSON implements json.Marshaler
-func (msg *DestroyProjectResponse) MarshalJSON() ([]byte, error) {
-	return protojson.MarshalOptions{
-		UseEnumNumbers:  false,
-		EmitUnpopulated: false,
-		UseProtoNames:   false,
-	}.Marshal(msg)
-}
-
-// UnmarshalJSON implements json.Unmarshaler
-func (msg *DestroyProjectResponse) UnmarshalJSON(b []byte) error {
-	return protojson.UnmarshalOptions{
-		DiscardUnknown: false,
-	}.Unmarshal(b, msg)
-}
-
-// MarshalJSON implements json.Marshaler
 func (msg *GetApplicationRequest) MarshalJSON() ([]byte, error) {
 	return protojson.MarshalOptions{
 		UseEnumNumbers:  false,
