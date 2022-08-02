75e0ab6ddc0b0a06b325fe60c4cdbc7a5205de19
Joseph Rajewski <joe.rajewski@hashicorp.com>
2022-08-02 12:05:42 -0400

    Add log to destroy project operation. Loop through workspaces during destruction of resources.

--- internal/runner/operation_project_destroy.go
+++ internal/runner/operation_project_destroy.go
@@ -2,12 +2,15 @@ package runner
 
 import (
 	"context"
+	"github.com/hashicorp/go-hclog"
 	"github.com/hashicorp/waypoint/internal/core"
 	pb "github.com/hashicorp/waypoint/pkg/server/gen"
+	empty "google.golang.org/protobuf/types/known/emptypb"
 )
 
 func (r *Runner) executeDestroyProjectOp(
 	ctx context.Context,
+	log hclog.Logger,
 	job *pb.Job,
 	project *core.Project,
 ) (*pb.Job_Result, error) {
@@ -31,25 +34,48 @@ func (r *Runner) executeDestroyProjectOp(
 	if err != nil {
 		return nil, err
 	}
+
 	if !destroyProjectOp.DestroyProject.SkipDestroyResources {
 		for _, app := range project.Apps() {
-			_, err := r.executeDestroyOp(ctx,
-				&pb.Job{
+			// Get the workspaces for the app we're currently destroying
+			workspaces, err := client.ListWorkspaces(ctx, &pb.ListWorkspacesRequest{
+				Scope: &pb.ListWorkspacesRequest_Application{
 					Application: &pb.Ref_Application{
+						Project:     destroyProjectOp.DestroyProject.Project.Name,
 						Application: app,
 					},
-					Operation: &pb.Job_Destroy{
-						Destroy: &pb.Job_DestroyOp{
-							// TODO: Do we need to specify a workspace? We want resources
-							// in all workspaces to be deleted
-							Target: &pb.Job_DestroyOp_Workspace{},
-						},
-					},
-				}, project,
-			)
+				},
+			})
 			if err != nil {
 				return nil, err
 			}
+			// Destroy the resources in the workspaces for the app
+			for _, workspace := range workspaces.Workspaces {
+				log.Debug("Destroying resources in workspace %s", workspace.Name)
+				// TODO: The destroy operation doesn't currently respect the Workspace
+				// being set on the Job - this should be updated
+				_, err := r.executeDestroyOp(ctx,
+					&pb.Job{
+						Application: &pb.Ref_Application{
+							Application: app,
+						},
+						Operation: &pb.Job_Destroy{
+							Destroy: &pb.Job_DestroyOp{
+								Target: &pb.Job_DestroyOp_Workspace{
+									Workspace: &empty.Empty{},
+								},
+							},
+						},
+						Workspace: &pb.Ref_Workspace{
+							Workspace: workspace.Name,
+						},
+					}, project,
+				)
+				if err != nil {
+					return nil, err
+				}
+			}
+
 		}
 	}
 
