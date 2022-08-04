68ead79cf439763100d5797b5847206fd5d2f64d
Joseph Rajewski <joe.rajewski@hashicorp.com>
2022-08-04 15:49:14 -0400

    Update server proto to include DestroyedResources message, and add a field of that type to the Deployment message.

--- pkg/server/gen/server.pb.json.go
+++ pkg/server/gen/server.pb.json.go
@@ -1032,6 +1032,22 @@ func (msg *DeclaredResource) UnmarshalJSON(b []byte) error {
 }
 
 // MarshalJSON implements json.Marshaler
+func (msg *DestroyedResource) MarshalJSON() ([]byte, error) {
+	return protojson.MarshalOptions{
+		UseEnumNumbers:  false,
+		EmitUnpopulated: false,
+		UseProtoNames:   false,
+	}.Marshal(msg)
+}
+
+// UnmarshalJSON implements json.Unmarshaler
+func (msg *DestroyedResource) UnmarshalJSON(b []byte) error {
+	return protojson.UnmarshalOptions{
+		DiscardUnknown: false,
+	}.Unmarshal(b, msg)
+}
+
+// MarshalJSON implements json.Marshaler
 func (msg *TaskLaunchInfo) MarshalJSON() ([]byte, error) {
 	return protojson.MarshalOptions{
 		UseEnumNumbers:  false,
