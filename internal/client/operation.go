b036d64a481e71aab1289856a35a95b37a9fec94
Cassie Coyle <25733780+cicoyle@users.noreply.github.com>
2022-08-04 16:21:26 -0500

    moved DestroyProject to be a project method and pulled logic out of DoApp, since DestroyProject is at the project level, and not app level.

--- internal/client/operation.go
+++ internal/client/operation.go
@@ -28,6 +28,21 @@ func (c *Project) Validate(ctx context.Context, op *pb.Job_ValidateOp) (*pb.Job_
 	return result.Validate, nil
 }
 
+func (c *Project) DestroyProject(ctx context.Context, op *pb.Job_DestroyProjectOp) (*pb.Job_ProjectDestroyResult, error) {
+	if op == nil {
+		op = &pb.Job_DestroyProjectOp{}
+	}
+	// Build our job
+	job := c.job()
+	job.Operation = &pb.Job_DestroyProject{
+		DestroyProject: op,
+	}
+
+	// Execute it
+	result, err := c.doJob(ctx, job, c.UI)
+	return result.ProjectDestroy, err
+}
+
 func (c *App) Auth(ctx context.Context, op *pb.Job_AuthOp) (*pb.Job_AuthResult, error) {
 	if op == nil {
 		op = &pb.Job_AuthOp{}
@@ -158,22 +173,6 @@ func (c *App) Destroy(ctx context.Context, op *pb.Job_DestroyOp) error {
 	// Execute it
 	_, err := c.doJob(ctx, job)
 	return err
-}
-
-func (c *App) DestroyProject(ctx context.Context, op *pb.Job_DestroyProjectOp) (*pb.Job_ProjectDestroyResult, error) {
-	if op == nil {
-		op = &pb.Job_DestroyProjectOp{}
-	}
-
-	// Build our job
-	job := c.job()
-	job.Operation = &pb.Job_DestroyProject{
-		DestroyProject: op,
-	}
-
-	// Execute it
-	result, err := c.doJob(ctx, job)
-	return result.ProjectDestroy, err
 }
 
 func (c *App) Exec(ctx context.Context, ec *execclient.Client) (exitCode int, err error) {
