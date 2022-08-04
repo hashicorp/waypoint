ba9fa20ec1dc0c09858c0ddeace9457ff33c7539
Joseph Rajewski <joe.rajewski@hashicorp.com>
2022-08-04 15:56:09 -0400

    Set destroyed and remaining resources after a destroy op runs.
    The proto message used in the execution of a deployDestroyOperation now sets two fields of a Deployment proto message, DeclaredResources and DestroyedResources. These are available to the caller that initiated the operation with doOperation. The data in these fields is used to output the resources that were and were not destroyed during the destroy operation.

--- internal/core/app_deploy_destroy.go
+++ internal/core/app_deploy_destroy.go
@@ -3,6 +3,10 @@ package core
 import (
 	"context"
 	"fmt"
+	"github.com/hashicorp/go-argmapper"
+	"github.com/mitchellh/mapstructure"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 
 	"github.com/hashicorp/go-hclog"
 	"github.com/hashicorp/hcl/v2"
@@ -114,11 +118,23 @@ func (a *App) destroyDeploy(
 	}
 	defer c.Close()
 
-	_, _, err = a.doOperation(ctx, a.logger.Named("deploy"), &deployDestroyOperation{
+	_, destroyment, err := a.doOperation(ctx, a.logger.Named("deploy"), &deployDestroyOperation{
 		Component:  c,
 		Deployment: d,
 	})
-	return err
+
+	destroyProto := destroyment.(*pb.Deployment)
+
+	for _, resource := range destroyProto.DeclaredResources {
+		a.logger.Debug("Resource " + resource.Name + " was not destroyed")
+		a.UI.Output("Resource "+resource.Name+" was not destroyed", terminal.WithWarningStyle())
+	}
+	for _, resource := range destroyProto.DestroyedResources {
+		a.logger.Debug("Resource " + resource.Name + " was destroyed")
+		a.UI.Output("Resource "+resource.Name+" was destroyed", terminal.WithSuccessStyle())
+	}
+
+	return nil
 }
 
 // destroyDeployWorkspace will call the DestroyWorkspace hook if there
@@ -229,7 +245,9 @@ func (op *deployDestroyOperation) Upsert(
 	return resp.Deployment, nil
 }
 
-func (op *deployDestroyOperation) Do(ctx context.Context, log hclog.Logger, app *App, _ proto.Message) (interface{}, error) {
+func (op *deployDestroyOperation) Do(ctx context.Context, log hclog.Logger, app *App, msg proto.Message) (interface{}, error) {
+	destroy := msg.(*pb.Deployment)
+
 	destroyer, ok := op.Component.Value.(component.Destroyer)
 	if !ok || destroyer.DestroyFunc() == nil {
 		return nil, nil
@@ -240,13 +258,49 @@ func (op *deployDestroyOperation) Do(ctx context.Context, log hclog.Logger, app 
 		return nil, nil // Fail silently for now, this will be fixed in v0.2
 	}
 
-	return app.callDynamicFunc(ctx,
+	baseArgs := []argmapper.Arg{plugin.ArgNamedAny("deployment", op.Deployment.Deployment)}
+	declaredResourcesResp := &component.DeclaredResourcesResp{}
+	destroyedResourcesResp := &component.DestroyedResourcesResp{}
+	args := append(baseArgs, argmapper.Typed(declaredResourcesResp), argmapper.Typed(destroyedResourcesResp))
+
+	// We don't need the result, we just need the declared and destroyed resources
+	// which we can access without the result since they were passed by reference
+	_, err := app.callDynamicFunc(ctx,
 		log,
 		nil,
 		op.Component,
 		destroyer.DestroyFunc(),
-		plugin.ArgNamedAny("deployment", op.Deployment.Deployment),
+		args...,
 	)
+	if err != nil {
+		return nil, err
+	}
+
+	declaredResources := make([]*pb.DeclaredResource, len(declaredResourcesResp.DeclaredResources))
+	if len(declaredResourcesResp.DeclaredResources) > 0 {
+		for i, pluginDeclaredResource := range declaredResourcesResp.DeclaredResources {
+			var serverDeclaredResource pb.DeclaredResource
+			if err := mapstructure.Decode(pluginDeclaredResource, &serverDeclaredResource); err != nil {
+				return nil, status.Errorf(codes.Internal, "failed to decode plugin declared resource named %q: %s", pluginDeclaredResource.Name, err)
+			}
+			declaredResources[i] = &serverDeclaredResource
+		}
+	}
+	destroy.DeclaredResources = declaredResources
+
+	destroyedResources := make([]*pb.DestroyedResource, len(destroyedResourcesResp.DestroyedResources))
+	if len(destroyedResourcesResp.DestroyedResources) > 0 {
+		for i, pluginDestroyedResource := range destroyedResourcesResp.DestroyedResources {
+			var serverDestroyedResource pb.DestroyedResource
+			if err := mapstructure.Decode(pluginDestroyedResource, &serverDestroyedResource); err != nil {
+				return nil, status.Errorf(codes.Internal, "failed to decode plugin declared resource named %q: %s", pluginDestroyedResource.Name, err)
+			}
+			destroyedResources[i] = &serverDestroyedResource
+		}
+	}
+	destroy.DestroyedResources = destroyedResources
+
+	return nil, err
 }
 
 func (op *deployDestroyOperation) StatusPtr(msg proto.Message) **pb.Status {
