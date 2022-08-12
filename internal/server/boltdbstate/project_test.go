6667d724157302fba3f30fd2125700cedb2cd53a
Joseph Rajewski <joe.rajewski@hashicorp.com>
2022-08-12 15:13:09 -0400

    Fix name of project in project test for bolt, & fix pipeline get after deletion of project.

--- internal/server/boltdbstate/project_test.go
+++ internal/server/boltdbstate/project_test.go
@@ -13,19 +13,21 @@ func TestProject(t *testing.T) {
 		s := TestState(t)
 		defer s.Close()
 
+		const projectName = "testproject"
+		const appName = "testapp"
 		// Create a project with one app
 		require.NoError(s.ProjectPut(&pb.Project{
-			Name: "test",
+			Name: projectName,
 			Applications: []*pb.Application{
 				{
-					Project: &pb.Ref_Project{Project: "test"},
-					Name:    "testApp",
+					Project: &pb.Ref_Project{Project: projectName},
+					Name:    appName,
 				},
 			},
 		}))
 
 		// Read it back
-		projectBeforeDelete, err := s.ProjectGet(&pb.Ref_Project{Project: "test"})
+		projectBeforeDelete, err := s.ProjectGet(&pb.Ref_Project{Project: projectName})
 		require.NoError(err)
 		require.NotNil(projectBeforeDelete)
 
@@ -33,8 +35,8 @@ func TestProject(t *testing.T) {
 		require.NoError(s.BuildPut(false, &pb.Build{
 			Id: "testBuild",
 			Application: &pb.Ref_Application{
-				Application: "testApp",
-				Project:     "testProject",
+				Application: appName,
+				Project:     projectName,
 			},
 			Workspace: &pb.Ref_Workspace{Workspace: "default"},
 		}))
@@ -42,8 +44,8 @@ func TestProject(t *testing.T) {
 		require.NoError(s.ArtifactPut(false, &pb.PushedArtifact{
 			Id: "testArtifact",
 			Application: &pb.Ref_Application{
-				Application: "testApp",
-				Project:     "testProject",
+				Application: appName,
+				Project:     projectName,
 			},
 			Workspace: &pb.Ref_Workspace{Workspace: "default"},
 		}))
@@ -51,8 +53,8 @@ func TestProject(t *testing.T) {
 		require.NoError(s.DeploymentPut(false, &pb.Deployment{
 			Id: "testDeployment",
 			Application: &pb.Ref_Application{
-				Application: "testApp",
-				Project:     "testProject",
+				Application: appName,
+				Project:     projectName,
 			},
 			Workspace: &pb.Ref_Workspace{Workspace: "default"},
 		}))
@@ -60,8 +62,8 @@ func TestProject(t *testing.T) {
 		require.NoError(s.ReleasePut(false, &pb.Release{
 			Id: "testRelease",
 			Application: &pb.Ref_Application{
-				Application: "testApp",
-				Project:     "testProject",
+				Application: appName,
+				Project:     projectName,
 			},
 			Workspace: &pb.Ref_Workspace{Workspace: "default"},
 		}))
@@ -69,8 +71,8 @@ func TestProject(t *testing.T) {
 		require.NoError(s.StatusReportPut(false, &pb.StatusReport{
 			Id: "testStatusReport",
 			Application: &pb.Ref_Application{
-				Application: "testApp",
-				Project:     "testProject",
+				Application: appName,
+				Project:     projectName,
 			},
 			Workspace: &pb.Ref_Workspace{Workspace: "default"},
 		}))
@@ -79,7 +81,7 @@ func TestProject(t *testing.T) {
 			Name: "testWorkspace",
 			Projects: []*pb.Workspace_Project{
 				{
-					Project: &pb.Ref_Project{Project: "testProject"},
+					Project: &pb.Ref_Project{Project: projectName},
 				},
 			},
 		}))
@@ -89,7 +91,7 @@ func TestProject(t *testing.T) {
 			Name: "testPipeline",
 			Owner: &pb.Pipeline_Project{
 				Project: &pb.Ref_Project{
-					Project: "testProject",
+					Project: projectName,
 				},
 			},
 			Steps: map[string]*pb.Pipeline_Step{
@@ -105,16 +107,16 @@ func TestProject(t *testing.T) {
 		require.NoError(s.TriggerPut(&pb.Trigger{
 			Id:        "testTrigger",
 			Name:      "testTrigger",
-			Project:   &pb.Ref_Project{Project: "testProject"},
+			Project:   &pb.Ref_Project{Project: projectName},
 			Workspace: &pb.Ref_Workspace{Workspace: "testWorkspace"},
 		}))
 
 		// Delete the project (this should also delete the build)
-		err = s.ProjectDelete(&pb.Ref_Project{Project: "testProject"})
+		err = s.ProjectDelete(&pb.Ref_Project{Project: projectName})
 		require.NoError(err)
 
 		// Attempt to get the project again (expected error)
-		_, err = s.ProjectGet(&pb.Ref_Project{Project: "testProject"})
+		_, err = s.ProjectGet(&pb.Ref_Project{Project: projectName})
 		require.Error(err)
 
 		_, err = s.BuildGet(&pb.Ref_Operation{Target: &pb.Ref_Operation_Id{Id: "testBuild"}})
@@ -138,7 +140,10 @@ func TestProject(t *testing.T) {
 		_, err = s.TriggerGet(&pb.Ref_Trigger{Id: "testTrigger"})
 		require.Error(err)
 
-		_, err = s.PipelineGet(&pb.Ref_Pipeline{Ref: &pb.Ref_Pipeline_Id{Id: &pb.Ref_PipelineId{Id: "testPipeline"}}})
+		_, err = s.PipelineGet(&pb.Ref_Pipeline{Ref: &pb.Ref_Pipeline_Owner{&pb.Ref_PipelineOwner{
+			Project:      &pb.Ref_Project{Project: projectName},
+			PipelineName: "testPipeline",
+		}}})
 		require.Error(err)
 	})
 }
