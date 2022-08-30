4a2ddbbe5cc73be6243b17a35cee85bd567bef0b
krantzinator <8461333+krantzinator@users.noreply.github.com>
2022-08-30 09:56:44 -0400

    actually abort stepgroup this way

--- internal/cli/runner_uninstall.go
+++ internal/cli/runner_uninstall.go
@@ -137,6 +137,8 @@ func (c *RunnerUninstallCommand) Run(args []string) int {
 		Id:         c.id,
 	})
 	if err != nil {
+		s.Update("Unable to uninstall runner", terminal.StatusError)
+		s.Abort()
 		c.ui.Output(clierrors.Humanize(err), terminal.WithErrorStyle())
 		return 1
 	}
