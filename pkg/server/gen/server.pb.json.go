fb9b41356a0525b2f81536a562c3e6afdac720a8
Shirley Xiaolin Xu <xiaolin@hashicorp.com>
2022-08-31 16:41:27 -0400

    refactor pipeline message

--- pkg/server/gen/server.pb.json.go
+++ pkg/server/gen/server.pb.json.go
@@ -856,22 +856,6 @@ func (msg *Ref_Pipeline) UnmarshalJSON(b []byte) error {
 }
 
 // MarshalJSON implements json.Marshaler
-func (msg *Ref_PipelineId) MarshalJSON() ([]byte, error) {
-	return protojson.MarshalOptions{
-		UseEnumNumbers:  false,
-		EmitUnpopulated: false,
-		UseProtoNames:   false,
-	}.Marshal(msg)
-}
-
-// UnmarshalJSON implements json.Unmarshaler
-func (msg *Ref_PipelineId) UnmarshalJSON(b []byte) error {
-	return protojson.UnmarshalOptions{
-		DiscardUnknown: false,
-	}.Unmarshal(b, msg)
-}
-
-// MarshalJSON implements json.Marshaler
 func (msg *Ref_PipelineOwner) MarshalJSON() ([]byte, error) {
 	return protojson.MarshalOptions{
 		UseEnumNumbers:  false,
