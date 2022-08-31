bf88fc4b6ca1d0912f5374130fec3fdd24a80358
henryestberg <11286201+HenryEstberg@users.noreply.github.com>
2022-08-31 16:17:00 -0700

    added comments and refactored code

--- embedJson/embed_json.go
+++ embedJson/embed_json.go
@@ -4,5 +4,9 @@ import (
 	"embed"
 )
 
+// This file embeds all of the files in /gen/ into the Files variable below. The comment above
+// tells the compiler where to find these files. Once waypoint is built, these files are avaliable
+// through this package at runtime.
+
 //go:embed gen/*.json
 var Files embed.FS
--- internal/cli/hclgen/hclgen.go
+++ internal/cli/hclgen/hclgen.go
@@ -96,7 +96,7 @@ func HclGen(ui terminal.UI) bool {
 
 	ui.Output("Configure builder", terminal.WithHeaderStyle())
 	// Select a builder
-	plug, err, close := selectPlugin(1, fList, embedJson.Files, ui)
+	plug, close, err := selectPlugin("builder", fList, embedJson.Files, ui)
 	if err != nil {
 		ui.Output(clierrors.Humanize(err), terminal.WithErrorStyle())
 		return false
@@ -109,7 +109,7 @@ func HclGen(ui terminal.UI) bool {
 	if plug.Name != "" {
 		hclFileByte = append(hclFileByte, []byte(fmt.Sprintf(genIndent(brackets)+"use \"%s\" {\n", plug.Name))...)
 		brackets++
-		fieldMap, err, close := populatePlugins(plug, ui)
+		fieldMap, close, err := populatePlugins(plug, ui)
 		if err != nil {
 			ui.Output(clierrors.Humanize(err), terminal.WithErrorStyle())
 			return false
@@ -134,7 +134,7 @@ func HclGen(ui terminal.UI) bool {
 
 	ui.Output("Configure registry", terminal.WithHeaderStyle())
 	// Select a registry
-	plug, err, close = selectPlugin(4, fList, embedJson.Files, ui)
+	plug, close, err = selectPlugin("registry", fList, embedJson.Files, ui)
 	if err != nil {
 		ui.Output(clierrors.Humanize(err), terminal.WithErrorStyle())
 		return false
@@ -148,7 +148,7 @@ func HclGen(ui terminal.UI) bool {
 		brackets++
 		hclFileByte = append(hclFileByte, []byte(fmt.Sprintf(genIndent(brackets)+"use \"%s\" {\n", plug.Name))...)
 		brackets++
-		fieldMap, err, close := populatePlugins(plug, ui)
+		fieldMap, close, err := populatePlugins(plug, ui)
 		if err != nil {
 			ui.Output(clierrors.Humanize(err), terminal.WithErrorStyle())
 			return false
@@ -173,7 +173,7 @@ func HclGen(ui terminal.UI) bool {
 
 	ui.Output("Configure deployment platform", terminal.WithHeaderStyle())
 	// Select a deployer
-	plug, err, close = selectPlugin(2, fList, embedJson.Files, ui)
+	plug, close, err = selectPlugin("deployment platform", fList, embedJson.Files, ui)
 	if err != nil {
 		ui.Output(clierrors.Humanize(err), terminal.WithErrorStyle())
 		return false
@@ -188,7 +188,7 @@ func HclGen(ui terminal.UI) bool {
 		brackets++
 		hclFileByte = append(hclFileByte, []byte(fmt.Sprintf(genIndent(brackets)+"use \"%s\" {\n", plug.Name))...)
 		brackets++
-		fieldMap, err, close := populatePlugins(plug, ui)
+		fieldMap, close, err := populatePlugins(plug, ui)
 		if err != nil {
 			ui.Output(clierrors.Humanize(err), terminal.WithErrorStyle())
 			return false
@@ -211,7 +211,7 @@ func HclGen(ui terminal.UI) bool {
 
 	ui.Output("Configure releaser", terminal.WithHeaderStyle())
 	// Select a releaser
-	plug, err, close = selectPlugin(3, fList, embedJson.Files, ui)
+	plug, close, err = selectPlugin("releaser", fList, embedJson.Files, ui)
 	if err != nil {
 		ui.Output(clierrors.Humanize(err), terminal.WithErrorStyle())
 		return false
@@ -226,7 +226,7 @@ func HclGen(ui terminal.UI) bool {
 		brackets++
 		hclFileByte = append(hclFileByte, []byte(fmt.Sprintf(genIndent(brackets)+"use \"%s\" {\n", plug.Name))...)
 		brackets++
-		fieldMap, err, close := populatePlugins(plug, ui)
+		fieldMap, close, err := populatePlugins(plug, ui)
 		if err != nil {
 			ui.Output(clierrors.Humanize(err), terminal.WithErrorStyle())
 			return false
@@ -270,11 +270,17 @@ func writeFields(byteS []byte, fieldMap map[string]fieldInfo, brackets int, ui t
 			byteS = append(byteS, []byte(fmt.Sprintf(genIndent(brackets)+"%s {\n", key))...)
 			brackets++
 			for name, cont := range elem.children {
+				if cont == "" {
+					byteS = append(byteS, []byte(fmt.Sprintf(genIndent(brackets)+"//The following field was skipped during file generation\n"))...)
+				}
 				byteS = append(byteS, []byte(fmt.Sprintf(genIndent(brackets)+"%s = \"%s\"\n", name, cont))...)
 			}
 			byteS = closeBrackets(byteS, 1, brackets)
 			brackets--
 		} else {
+			if elem.contents == "" {
+				byteS = append(byteS, []byte(fmt.Sprintf(genIndent(brackets)+"//The following field was skipped during file generation\n"))...)
+			}
 			byteS = append(byteS, []byte(fmt.Sprintf(genIndent(brackets)+"%s = \"%s\"\n", key, elem.contents))...)
 		}
 	}
@@ -310,7 +316,11 @@ func genIndent(outstanding int) string {
 	}
 	return spaces
 }
-func populatePlugins(plug PlugDocs, ui terminal.UI) (map[string]fieldInfo, error, bool) {
+
+// populatePlugins returns a map of field name strings as keys mapped to fieldInfo structs containing the
+// user generated value for the field and information about if the field is a subfield and its children if so.
+// It also returns a boolean indicating if we want to exit the generator completely and an error if one exists
+func populatePlugins(plug PlugDocs, ui terminal.UI) (map[string]fieldInfo, bool, error) {
 	m := make(map[string]fieldInfo)
 	fCount := 0
 	for _, f := range plug.PlugDocs {
@@ -349,30 +359,32 @@ func populatePlugins(plug PlugDocs, ui terminal.UI) (map[string]fieldInfo, error
 				m[field.Field] = fieldInfo{isParent: true, children: make(map[string]string)}
 				for _, sfield := range field.PlugSubDocs {
 					if !sfield.Optional {
-						cont, err, close := populateField(sfield.Field, sfield.Type, ui)
+						cont, close, err := populateField(sfield.Field, sfield.Type, ui)
 						if err != nil {
-							return m, err, false
+							return m, false, err
 						} else if close {
-							return m, nil, true
+							return m, true, nil
 						}
 						m[field.Field].children[sfield.Field] = cont
 					}
 				}
 			} else {
-				cont, err, close := populateField(field.Field, field.Type, ui)
+				cont, close, err := populateField(field.Field, field.Type, ui)
 				if err != nil {
-					return m, err, false
+					return m, false, err
 				} else if close {
-					return m, nil, true
+					return m, true, nil
 				}
 				m[field.Field] = fieldInfo{contents: cont, isParent: false}
 			}
 		}
 	}
-	return m, nil, false
+	return m, false, nil
 }
 
-func populateField(name string, fType string, ui terminal.UI) (string, error, bool) {
+// populateField returns the string value inputted by the user for a field, a boolean indicating if we
+// want to exit, and an error if one exists
+func populateField(name string, fType string, ui terminal.UI) (string, bool, error) {
 	getField := true
 	typeString := fType
 	if typeString == "" {
@@ -380,7 +392,7 @@ func populateField(name string, fType string, ui terminal.UI) (string, error, bo
 	}
 	for getField {
 		fieldVal, err := ui.Input(&terminal.Input{
-			Prompt: fmt.Sprintf("%s <%s>: ", strings.Title(name), typeString),
+			Prompt: fmt.Sprintf("%s <%s>: ", name, typeString),
 			Style:  "",
 			Secret: false,
 		})
@@ -390,9 +402,9 @@ func populateField(name string, fType string, ui terminal.UI) (string, error, bo
 				clierrors.Humanize(err),
 				terminal.WithErrorStyle(),
 			)
-			return "", err, false
+			return "", false, err
 		} else if strings.ToLower(fieldVal) == "exit" {
-			return "", nil, true
+			return "", true, nil
 		} else if strings.ToLower(fieldVal) == "" {
 			ui.Output(fmt.Sprintf("You have selected to skip the %s field.", name))
 			pNameConfirm, err := ui.Input(&terminal.Input{
@@ -406,12 +418,12 @@ func populateField(name string, fType string, ui terminal.UI) (string, error, bo
 					clierrors.Humanize(err),
 					terminal.WithErrorStyle(),
 				)
-				return "", err, false
+				return "", false, err
 			} else if strings.ToLower(pNameConfirm) == "exit" {
-				return "", nil, true
+				return "", true, nil
 			} else if strings.ToLower(pNameConfirm) == "yes" || strings.ToLower(pNameConfirm) == "y" {
 				ui.Output("%s skipped\n", strings.Title(name), terminal.WithWarningStyle())
-				return "", nil, false
+				return "", false, nil
 			} else {
 				ui.Output("Skip cancelled\n")
 			}
@@ -429,51 +441,42 @@ func populateField(name string, fType string, ui terminal.UI) (string, error, bo
 					clierrors.Humanize(err),
 					terminal.WithErrorStyle(),
 				)
-				return "", err, false
+				return "", false, err
 			} else if strings.ToLower(fieldConfirm) == "exit" {
-				return "", nil, true
+				return "", true, nil
 			} else if strings.ToLower(fieldConfirm) == "yes" || strings.ToLower(fieldConfirm) == "y" {
 				ui.Output("%s confirmed\n", strings.Title(name), terminal.WithSuccessStyle())
-				return fieldVal, nil, false
+				return fieldVal, false, nil
 			} else {
 				ui.Output("%s rejected\n", strings.Title(name))
 			}
 		}
 	}
-	return "", nil, true
+	return "", true, nil
 }
 
-// <plug> indicates the plugin that the user needs to select. 1: Builder, 2: Deployer/Platform, 3: Releaser, 4: Registry
-func selectPlugin(plug int, fList []string, fSystem embed.FS, ui terminal.UI) (PlugDocs, error, bool) {
-	var plugType string
+// <plug> indicates the plugin that the user needs to select.
+// selectPlugin returns a PlugDocs struct filled with the relevant information for the user selected plugin
+// selectPlugin also returns a boolean indicating if the program should exit early and an error if one exists
+func selectPlugin(plug string, fList []string, fSystem embed.FS, ui terminal.UI) (PlugDocs, bool, error) {
 	var plugDocs PlugDocs
-	switch plug {
-	case 1:
-		plugType = "builder"
-	case 2:
-		plugType = "deployment platform"
-	case 3:
-		plugType = "releaser"
-	case 4:
-		plugType = "registry"
-	}
 	var plugList []string
 	for _, file := range fList {
 		if filepath.Ext(file) == ".json" {
 			switch plug {
-			case 1:
+			case "builder":
 				if strings.HasPrefix(file, "builder") {
 					plugList = append(plugList, file)
 				}
-			case 2:
+			case "deployment platform":
 				if strings.HasPrefix(file, "platform") {
 					plugList = append(plugList, file)
 				}
-			case 3:
+			case "releaser":
 				if strings.HasPrefix(file, "release") {
 					plugList = append(plugList, file)
 				}
-			case 4:
+			case "registry":
 				if strings.HasPrefix(file, "registry") {
 					plugList = append(plugList, file)
 				}
@@ -482,7 +485,7 @@ func selectPlugin(plug int, fList []string, fSystem embed.FS, ui terminal.UI) (P
 	}
 	sort.Strings(plugList)
 	ui.Output(fmt.Sprintf("Select a %s: learn more at https://www.waypointproject.io/plugins. To use a %s that’s not shown here hit return, then edit the .hcl file after it’s been generated.\n",
-		plugType, plugType))
+		plug, plug))
 	jMap := make(map[string]interface{})
 	var selList []string
 	var nameSelList []string
@@ -490,7 +493,7 @@ func selectPlugin(plug int, fList []string, fSystem embed.FS, ui terminal.UI) (P
 	for _, f := range plugList {
 		byteValue, err := fSystem.ReadFile(fmt.Sprintf("gen/%s", f))
 		if err != nil {
-			return plugDocs, err, false
+			return plugDocs, false, err
 		}
 		json.Unmarshal(byteValue, &jMap)
 
@@ -526,13 +529,13 @@ func selectPlugin(plug int, fList []string, fSystem embed.FS, ui terminal.UI) (P
 				clierrors.Humanize(err),
 				terminal.WithErrorStyle(),
 			)
-			return plugDocs, err, false
+			return plugDocs, false, err
 		} else if strings.ToLower(num) == "exit" {
-			return plugDocs, nil, true
+			return plugDocs, true, nil
 		} else if val, err := strconv.Atoi(num); err == nil && (0 < val && val < count) {
 			ui.Output(fmt.Sprintf("You have selected the %s plugin.", nameSelList[val-1]))
 			pNameConfirm, err := ui.Input(&terminal.Input{
-				Prompt: fmt.Sprintf("Is this %s plugin correct? (y/N): ", plugType),
+				Prompt: fmt.Sprintf("Is this %s plugin correct? (y/N): ", plug),
 				Style:  "",
 				Secret: false,
 			})
@@ -542,20 +545,20 @@ func selectPlugin(plug int, fList []string, fSystem embed.FS, ui terminal.UI) (P
 					clierrors.Humanize(err),
 					terminal.WithErrorStyle(),
 				)
-				return plugDocs, err, false
+				return plugDocs, false, err
 			} else if strings.ToLower(pNameConfirm) == "exit" {
-				return plugDocs, nil, true
+				return plugDocs, true, nil
 			} else if strings.ToLower(pNameConfirm) == "yes" || strings.ToLower(pNameConfirm) == "y" {
-				ui.Output("%s plugin confirmed\n", strings.Title(plugType), terminal.WithSuccessStyle())
+				ui.Output("%s plugin confirmed\n", strings.Title(plug), terminal.WithSuccessStyle())
 				selFileName = selList[val-1]
 				getSelect = false
 			} else {
-				ui.Output("%s plugin rejected\n", strings.Title(plugType))
+				ui.Output("%s plugin rejected\n", strings.Title(plug))
 			}
 		} else if num == "" {
-			ui.Output(fmt.Sprintf("You have selected to skip the %s stage.", plugType))
+			ui.Output(fmt.Sprintf("You have selected to skip the %s stage.", plug))
 			pNameConfirm, err := ui.Input(&terminal.Input{
-				Prompt: fmt.Sprintf("Do you really want to skip the %s stage? (y/N): ", plugType),
+				Prompt: fmt.Sprintf("Do you really want to skip the %s stage? (y/N): ", plug),
 				Style:  "",
 				Secret: false,
 			})
@@ -565,13 +568,13 @@ func selectPlugin(plug int, fList []string, fSystem embed.FS, ui terminal.UI) (P
 					clierrors.Humanize(err),
 					terminal.WithErrorStyle(),
 				)
-				return plugDocs, err, false
+				return plugDocs, false, err
 			} else if strings.ToLower(pNameConfirm) == "exit" {
-				return plugDocs, nil, true
+				return plugDocs, true, nil
 			} else if strings.ToLower(pNameConfirm) == "yes" || strings.ToLower(pNameConfirm) == "y" {
-				ui.Output("Step complete: %s stage skipped", strings.Title(plugType), terminal.WithWarningStyle())
+				ui.Output("Step complete: %s stage skipped", strings.Title(plug), terminal.WithWarningStyle())
 				plugDocs.Name = ""
-				return plugDocs, nil, false
+				return plugDocs, false, nil
 			} else {
 				ui.Output("Skip cancelled\n")
 			}
@@ -583,19 +586,19 @@ func selectPlugin(plug int, fList []string, fSystem embed.FS, ui terminal.UI) (P
 	if selFileName != "" {
 		byteValue, err := fSystem.ReadFile(fmt.Sprintf("gen/%s", selFileName))
 		if err != nil {
-			return plugDocs, err, false
+			return plugDocs, false, err
 		}
 		json.Unmarshal(byteValue, &plugDocs)
 		if plugDocs.Name != "" {
-			ui.Output(fmt.Sprintf("You have selected the %s %s plugin.", plugDocs.Name, plugType))
+			ui.Output(fmt.Sprintf("You have selected the %s %s plugin.", plugDocs.Name, plug))
 
 		} else {
 			//TODO: better error here, do we need to check again here?
-			return plugDocs, nil, true
+			return plugDocs, true, nil
 		}
-		return plugDocs, nil, false
+		return plugDocs, false, nil
 	}
-	return plugDocs, nil, false
+	return plugDocs, false, nil
 }
 
 // Gets either a project or app name for an HCL file, pa should be either "project" or "app"
