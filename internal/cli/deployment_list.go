55930ba00e3cb7d798e905b1a95313388d22e0b1
Shirley Xiaolin Xu <xiaolin@hashicorp.com>
2022-09-01 16:40:22 -0400

    deployment list shows pipeline

--- internal/cli/deployment_list.go
+++ internal/cli/deployment_list.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"sort"
+	"strconv"
 	"strings"
 	"time"
 
@@ -247,6 +248,18 @@ func (c *DeploymentListCommand) Run(args []string) int {
 
 					details = append(details, "image:"+img)
 				}
+			}
+
+			j, err := c.project.Client().GetJob(c.Ctx, &pb.GetJobRequest{
+				JobId: deployBundle.Deployment.JobId,
+			})
+			if err != nil {
+				app.UI.Output(clierrors.Humanize(err), terminal.WithErrorStyle())
+				return err
+			}
+			if j.Pipeline != nil {
+				pipeline := "pipeline: " + j.Pipeline.PipelineName + "[run: " + strconv.FormatUint(j.Pipeline.RunSequence, 10) + "]" + "[step: " + j.Pipeline.Step + "]"
+				details = append(details, pipeline)
 			}
 
 			if deployBundle.Artifact != nil {
