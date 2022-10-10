2e76e46b5282783b5b5075c336abe26196dc1e9a
Bryce Kalow <bkalow@hashicorp.com>
2022-10-10 09:47:22 -0500

    updates redirects for developer

--- website/redirects.js
+++ website/redirects.js
@@ -1,33 +1,27 @@
 module.exports = [
-  // This is an example redirect, it can be removed once other redirects have been added
   {
-    source: '/home',
-    destination: '/',
-    permanent: true,
-  },
-  {
-    source: '/docs/kubernetes/:path*',
+    source: '/waypoint/docs/kubernetes/:path*',
     destination: '/docs/platforms/kubernetes/:path*',
     permanent: true,
   },
   {
-    source: '/docs/glossary',
-    destination: '/docs/resources/glossary',
+    source: '/waypoint/docs/glossary',
+    destination: '/waypoint/docs/resources/glossary',
     permanent: true,
   },
   {
-    source: '/docs/roadmap',
-    destination: '/docs/resources/roadmap',
+    source: '/waypoint/docs/roadmap',
+    destination: '/waypoint/docs/resources/roadmap',
     permanent: true,
   },
   {
-    source: '/docs/troubleshooting',
-    destination: '/docs/resources/troubleshooting',
+    source: '/waypoint/docs/troubleshooting',
+    destination: '/waypoint/docs/resources/troubleshooting',
     permanent: true,
   },
   {
-    source: '/docs/internals/:path*',
-    destination: '/docs/resources/internals/:path*',
+    source: '/waypoint/docs/internals/:path*',
+    destination: '/waypoint/docs/resources/internals/:path*',
     permanent: true,
   },
 ]
