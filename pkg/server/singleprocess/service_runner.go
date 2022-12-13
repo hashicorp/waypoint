527f0b21a83fbbce2e7ac33797e6ae94926a885f
Brian Cain <bcain@hashicorp.com>
2022-12-13 14:18:44 -0800

    server: Attempt to complete a job on job event failure
    This commit attempts to complete the original job by jobId in the event
    where we failed to properly look up a job earlier in the event stream.
    This is to prevent any running but "errored" jobs to be stuck in the
    Running state in the database, leaving them hanging and locking out the
    workspace from queuing any more jobs.

--- pkg/server/singleprocess/service_runner.go
+++ pkg/server/singleprocess/service_runner.go
@@ -780,6 +780,29 @@ func (s *Service) RunnerJobStream(
 			return err
 
 		case req := <-eventCh:
+			if job == nil {
+				select {
+				case err := <-errCh:
+					// NOTE(briancain): In this case, it means we've received an event from
+					// the event channel, however `job` was nil, and `err` was not. This
+					// means we received an error trying to retrieve a job by Id and need
+					// to properly handle that.
+					log.Error("Job disappeared and there was an error while processing job event", "error", err, "event", req)
+
+					// Attempt to complete job with original jobId so that the original
+					// job Id isn't left hanging in a Running state. We also send through
+					// the error to give to the job details for why it's Errored.
+					if err := s.state(ctx).JobComplete(jobId, nil, err); err != nil {
+						return hcerr.Externalize(log, err,
+							"failed to complete job while failing to process job event",
+							"id", jobId,
+							"event_request", req)
+					}
+
+					return hcerr.Externalize(log, err, "failed to process job event", "event_request", req)
+				default:
+				}
+			}
 			if err := s.handleJobStreamRequest(log, job, server, req, logStreamWriter); err != nil {
 				log.Error("error handling job stream request", "err", err, "req", req)
 				return err
