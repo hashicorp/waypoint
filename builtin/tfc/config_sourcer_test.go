0069d601cb49d7b68dfe9458a442d8fa528ea14a
Izaak Lauer <8404559+izaaklauer@users.noreply.github.com>
2022-12-22 16:38:43 -0600

    Updating the terraform configsourcer to emit json values

--- builtin/tfc/config_sourcer_test.go
+++ builtin/tfc/config_sourcer_test.go
@@ -2,6 +2,7 @@ package tfc
 
 import (
 	"context"
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -14,13 +15,15 @@ import (
 	pb "github.com/hashicorp/waypoint-plugin-sdk/proto/gen"
 )
 
-func TestConfigSourcer(t *testing.T) {
+// Tests that the TFC configsourcer can return a simple string value
+func TestConfigSourcer_StringValue(t *testing.T) {
 	ctx := context.Background()
 	log := hclog.L()
 	require := require.New(t)
 
 	var (
-		includes string
+		includes                string
+		tfcRespJsonTestdataPath string
 	)
 
 	s := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
@@ -38,7 +41,7 @@ func TestConfigSourcer(t *testing.T) {
 		case "/api/v2/workspaces/ws-abcdef/current-state-version":
 			includes = req.URL.Query().Get("include")
 
-			data, err := ioutil.ReadFile("testdata/state-version.json")
+			data, err := ioutil.ReadFile(tfcRespJsonTestdataPath)
 			if err != nil {
 				panic(err)
 			}
@@ -58,33 +61,154 @@ func TestConfigSourcer(t *testing.T) {
 
 	defer cs.stop()
 
-	// Read
-	result, err := cs.read(ctx, log, []*component.ConfigRequest{
-		{
-			Name: "HOSTNAME",
-			Config: map[string]string{
-				"organization": "foocorp",
-				"workspace":    "databases",
-				"output":       "aws_assume_role_arn",
+	t.Run("can read single string value", func(t *testing.T) {
+		tfcRespJsonTestdataPath = "testdata/state-version.json"
+
+		result, err := cs.read(ctx, log, []*component.ConfigRequest{
+			{
+				Name: "HOSTNAME",
+				Config: map[string]string{
+					"organization": "foocorp",
+					"workspace":    "databases",
+					"output":       "aws_assume_role_arn",
+				},
 			},
-		},
+		})
+		require.NoError(err)
+		require.NotNil(result)
+		require.Len(result, 1)
+
+		v := result[0]
+
+		require.Equal("outputs", includes)
+
+		require.Equal("HOSTNAME", v.Name)
+		ve, ok := v.Result.(*pb.ConfigSource_Value_Error)
+		if ok {
+			require.False(ok, "error reading value: %s", ve.Error.Message)
+		}
+
+		require.Equal(
+			"arn:aws:iam::797645259670:role/waypoint-circleci-release-pipeline",
+			v.Result.(*pb.ConfigSource_Value_Value).Value,
+		)
 	})
-	require.NoError(err)
-	require.NotNil(result)
-	require.Len(result, 1)
 
-	v := result[0]
+	t.Run("can read single complex-typed value", func(t *testing.T) {
+		tfcRespJsonTestdataPath = "testdata/state-version-complex.json"
 
-	require.Equal("outputs", includes)
+		result, err := cs.read(ctx, log, []*component.ConfigRequest{
+			{
+				Name: "HOSTNAME",
+				Config: map[string]string{
+					"organization": "foocorp",
+					"workspace":    "databases",
+					"output":       "ecs_task_subnets",
+				},
+			},
+		})
+		require.NoError(err)
+		require.NotNil(result)
+		require.Len(result, 1)
 
-	require.Equal("HOSTNAME", v.Name)
-	ve, ok := v.Result.(*pb.ConfigSource_Value_Error)
-	if ok {
-		require.False(ok, "error reading value: %s", ve.Error.Message)
-	}
+		v := result[0]
 
-	require.Equal(
-		"arn:aws:iam::797645259670:role/waypoint-circleci-release-pipeline",
-		v.Result.(*pb.ConfigSource_Value_Value).Value,
-	)
+		require.Equal("outputs", includes)
+
+		require.Equal("HOSTNAME", v.Name)
+		ve, ok := v.Result.(*pb.ConfigSource_Value_Error)
+		if ok {
+			require.False(ok, "error reading value: %s", ve.Error.Message)
+		}
+
+		resultJson, ok := v.Result.(*pb.ConfigSource_Value_Json)
+		require.True(ok)
+
+		var resultMarshalled []string
+		require.NoError(json.Unmarshal(resultJson.Json, &resultMarshalled))
+
+		require.True(ok)
+		require.Equal([]string{
+			"subnet-03afefcc38a919083",
+			"subnet-087ea2efd4f009fc5",
+			"subnet-09e02553a59746ba1",
+			"subnet-05a2d1112fbca071c",
+			"subnet-0d5f650b4d0eebc56",
+		}, resultMarshalled)
+	})
+
+	t.Run("Read all outputs", func(t *testing.T) {
+
+		cases := []struct {
+			tfcRespJsonTestdataPath string
+			numOutputs              int
+			spotChecks              map[string]interface{}
+		}{
+			{
+				"testdata/state-version.json",
+				3,
+				map[string]interface{}{
+					"alb_listener_arn": "arn:aws:elasticloadbalancing:us-east-1:797645259670:listener/app/acmeapp1-dev/0ed92920e20ed1dc/07e51901e3cec498",
+				},
+			},
+			{
+				"testdata/state-version-complex.json",
+				14,
+				map[string]interface{}{
+					"region": "us-east-1",
+					"ecs_task_subnets": []string{
+						"subnet-03afefcc38a919083",
+						"subnet-087ea2efd4f009fc5",
+						"subnet-09e02553a59746ba1",
+						"subnet-05a2d1112fbca071c",
+						"subnet-0d5f650b4d0eebc56",
+					},
+				},
+			},
+		}
+
+		for _, tt := range cases {
+			tfcRespJsonTestdataPath = tt.tfcRespJsonTestdataPath
+
+			result, err := cs.read(ctx, log, []*component.ConfigRequest{
+				{
+					Name: "HOSTNAME",
+					Config: map[string]string{
+						"organization": "foocorp",
+						"workspace":    "databases",
+						"all_outputs":  "true",
+					},
+				},
+			})
+			require.NoError(err)
+			require.NotNil(result)
+			require.Len(result, 1)
+
+			v := result[0]
+
+			ve, ok := v.Result.(*pb.ConfigSource_Value_Error)
+			if ok {
+				require.False(ok, "error reading value: %s", ve.Error.Message)
+			}
+
+			jsonResult, ok := v.Result.(*pb.ConfigSource_Value_Json)
+			require.True(ok)
+
+			require.NotEmpty(jsonResult.Json)
+
+			var jsonResults map[string]interface{}
+			require.NoError(json.Unmarshal(jsonResult.Json, &jsonResults))
+			require.Len(jsonResults, tt.numOutputs)
+			for k, v := range tt.spotChecks {
+				actualV, ok := jsonResults[k]
+				require.True(ok)
+				require.Equal(v, actualV)
+			}
+		}
+
+	})
+
+	t.Run("Can read a single var with a complex type", func(t *testing.T) {
+
+	})
 }
