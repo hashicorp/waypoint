ff9360be9e5ebd1817447729100d84f30d56c253
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2023-03-25 16:11:43 -0400

    chore: update should-build script

--- website/scripts/should-build.sh
+++ website/scripts/should-build.sh
@@ -8,11 +8,11 @@
 # This is run during the website build step to determine if we should skip the build or not.
 # More information: https://vercel.com/docs/platform/projects#ignored-build-step
 
-if [[ "$VERCEL_GIT_COMMIT_REF" == "stable-website"  ]] ; then
+if [[ "$VERCEL_GIT_COMMIT_REF" == "stable-website" ]]; then
   # Proceed with the build if the branch is stable-website
   echo "✅ - Build can proceed"
-  exit 1;
+  exit 1
 else
   # Check for differences in the website directory
-  git diff --quiet HEAD^ HEAD ./
+  git diff --quiet HEAD^ HEAD ./ ../pkg/server/gen/server.swagger.json
 fi
