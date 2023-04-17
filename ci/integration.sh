6058b62855d93dd3bb571c5406007345c13e6ceb
Daniel Kimsey <daniel.kimsey@hashicorp.com>
2023-04-17 13:00:59 -0500

    fixme: add push

--- ci/integration.sh
+++ ci/integration.sh
@@ -4,26 +4,39 @@ set -e -u -o pipefail
 
 set -x
 
-echo "Boot up the registry to use:"
+export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 
-docker run -d -p 5000:5000 --restart=always --name registry registry:2
+# Confirm k8s is working
+echo "Confirm kubernetes is working:"
+kubectl cluster-info
+
+
+echo "Boot up the registry to use:"
+docker run -d -p 5000:5000 --restart=always --name registry.localhost registry:2
+#
+#echo "Connect the registry network to the k3d network"
+#docker network connect k3d-k3s-default registry.localhost
 
 WP="$(pwd)/waypoint"
 
 test -e "$WP"
 
-export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
-
 cd ci/sinatra || exit 1
 
+[[ -n "$GITHUB_ACTION" ]] && echo "::group::Waypoint init"
 "$WP" init
 
+[[ -n "$GITHUB_ACTION" ]] && echo "::group::Waypoint build"
 "$WP" build
 
-"$WP" deploy
+[[ -n "$GITHUB_ACTION" ]] && echo "::group::Waypoint deploy"
+# If the registry isn't working and the pods are therefore unable to pull, we get stuck in an infinite wait
+timeout 1m "$WP" deploy
 
+[[ -n "$GITHUB_ACTION" ]] && echo "::group::Waypoint release"
 "$WP" release
 
+[[ -n "$GITHUB_ACTION" ]] && echo "::group::Waypoint deployment list"
 # Smoke test list methods
 "$WP" deployment list
 "$WP" deployment list -V
@@ -31,6 +44,9 @@ cd ci/sinatra || exit 1
 
 ## Let things get going.
 sleep 10
+
+[[ -n "$GITHUB_ACTION" ]] && echo "::group::Check deployed sinatra service"
+kubectl get service sinatra -o 'jsonpath={}' | jq
 
 PORT=$(kubectl get service sinatra -o jsonpath="{.spec.ports[0].nodePort}")
 
