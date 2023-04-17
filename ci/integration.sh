c2d25d6d64edc2e71f5534eecd67724712dcded7
Scott Macfarlane <smacfarlane@hashicorp.com>
2023-04-17 15:37:09 -0700

    Use registry.localhost as name for registry
    Signed-off-by: Scott Macfarlane <smacfarlane@hashicorp.com>

--- ci/integration.sh
+++ ci/integration.sh
@@ -13,10 +13,11 @@ echo "Confirm kubernetes is working:"
 kubectl cluster-info dump
 
 
-if [ -z "$GITHUB_ACTION" ]; then
-  echo "Boot up the registry to use:"
-  docker run -d -p 5000:5000 --restart=always --name registry registry:2
-fi
+echo "Boot up the registry to use:"
+docker run -d -p 5000:5000 --restart=always --name registry.localhost registry:2
+
+echo "Connect the registry network to the k3d network"
+docker network connect k3d-k3s-default registry.localhost
 
 WP="$(pwd)/waypoint"
 
