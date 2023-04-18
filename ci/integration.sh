42178d297cc91cf2ed3f611d0a441fe9fc629e64
Daniel Kimsey <daniel.kimsey@hashicorp.com>
2023-04-18 14:12:46 -0500

    Remove dead code

--- ci/integration.sh
+++ ci/integration.sh
@@ -15,9 +15,6 @@ kubectl cluster-info
 
 echo "Boot up the registry to use:"
 docker run -d -p 5000:5000 --restart=always --name registry.localhost registry:2
-#
-#echo "Connect the registry network to the k3d network"
-#docker network connect k3d-k3s-default registry.localhost
 
 WP="$(pwd)/waypoint"
 
