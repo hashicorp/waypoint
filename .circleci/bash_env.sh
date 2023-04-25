451e0cf9da3cb5565b940c8f3f317b7731061e38
catsby <clint@ctshryock.com>
2023-04-25 15:00:11 +0000

    backport of commit 4b9ed8a313b5a9d1bb8b64df9912bc08e0766eb1

--- .circleci/bash_env.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export GIT_COMMIT=$(git rev-parse --short HEAD)
-export GIT_DIRTY=$(test -n "`git status --porcelain`" && echo "+CHANGES" || true)
-export GIT_DESCRIBE=$(git describe --tags --always --match "v*")
-export GIT_IMPORT=github.com/hashicorp/waypoint/internal/version
-export GOLDFLAGS="-X ${GIT_IMPORT}.GitCommit=${GIT_COMMIT}${GIT_DIRTY} -X ${GIT_IMPORT}.GitDescribe=${GIT_DESCRIBE}"
-export CGO_ENABLED=0
