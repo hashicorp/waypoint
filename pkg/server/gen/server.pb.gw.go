b1925e822bf53e81555bbca5e48106a76f05a4be
Jamie White <jamie@jgwhite.co.uk>
2023-05-03 16:23:05 +0200

    Add `body: *` to CreateProjectFromTemplate grpc-gateway mapping
    Without this, we can’t pass any of the attributes over HTTP :)
    
    Co-authored-by: Amy Lam <1372946+amyrlam@users.noreply.github.com>

--- pkg/server/gen/server.pb.gw.go
+++ pkg/server/gen/server.pb.gw.go
@@ -9096,18 +9096,15 @@ func local_request_Waypoint_ListProjectTemplates_0(ctx context.Context, marshale
 
 }
 
-var (
-	filter_Waypoint_CreateProjectFromTemplate_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
-)
-
 func request_Waypoint_CreateProjectFromTemplate_0(ctx context.Context, marshaler runtime.Marshaler, client WaypointClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq CreateProjectFromTemplateRequest
 	var metadata runtime.ServerMetadata
 
-	if err := req.ParseForm(); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
 	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Waypoint_CreateProjectFromTemplate_0); err != nil {
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
@@ -9120,10 +9117,11 @@ func local_request_Waypoint_CreateProjectFromTemplate_0(ctx context.Context, mar
 	var protoReq CreateProjectFromTemplateRequest
 	var metadata runtime.ServerMetadata
 
-	if err := req.ParseForm(); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
 	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Waypoint_CreateProjectFromTemplate_0); err != nil {
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
